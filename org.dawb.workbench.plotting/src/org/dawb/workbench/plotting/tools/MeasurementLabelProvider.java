f8b8190ef2bfc5d3ceba2ddd5fbd2bf6f51811a2
matthew.gerring <matthew.gerring@gmail.com>
2012-04-19 11:02:44 +0100

    Added try/catch to avoid user seeing funny messages.

--- org.dawb.workbench.plotting/src/org/dawb/workbench/plotting/tools/MeasurementLabelProvider.java
+++ org.dawb.workbench.plotting/src/org/dawb/workbench/plotting/tools/MeasurementLabelProvider.java
@@ -5,9 +5,12 @@ import java.text.DecimalFormat;
 import org.dawb.common.ui.plot.region.IRegion;
 import org.dawb.common.ui.plot.region.IRegion.RegionType;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MeasurementLabelProvider extends ColumnLabelProvider {
 
+	private static final Logger logger = LoggerFactory.getLogger(MeasurementLabelProvider.class);
 	
 	private int column;
 	private MeasurementTool tool;
@@ -25,35 +28,43 @@ public class MeasurementLabelProvider extends ColumnLabelProvider {
 		if (!(element instanceof IRegion)) return null;
 		final IRegion    region = (IRegion)element;
 		
-		switch(column) {
-		case 0:
-			return region.getName();
-		case 1:
-			return region.getRegionType().getName();
-		case 2: // dx
-			if (region.getRegionType()==RegionType.RING) return "-";
-			return format.format(tool.getBounds(region).getDx());
-		case 3: // dy
-			if (region.getRegionType()==RegionType.RING) return "-";
-			return format.format(tool.getBounds(region).getDy());
-		case 4: // length
-			if (region.getRegionType()==RegionType.RING) return "-";
-			return format.format(tool.getBounds(region).getLength());
-		case 5: // max
-			final double max = tool.getMaxIntensity(region);
-		    if (Double.isNaN(max)) return "-";
-			return format.format(max);
-		case 6: // in rad
-			if (region.getRegionType()!=RegionType.RING) return "-";
-			return format.format(tool.getBounds(region).getInner());
-		case 7: // out rad
-			if (region.getRegionType()!=RegionType.RING) return "-";
-			return format.format(tool.getBounds(region).getOuter());
-		case 8: // region
-			return tool.getBounds(region).toString();
-
-		default:
-			return "Not found";
+		try {
+			switch(column) {
+			case 0:
+				return region.getName();
+			case 1:
+				return region.getRegionType().getName();
+			case 2: // dx
+				if (region.getRegionType()==RegionType.RING) return "-";
+				return format.format(tool.getBounds(region).getDx());
+			case 3: // dy
+				if (region.getRegionType()==RegionType.RING) return "-";
+				return format.format(tool.getBounds(region).getDy());
+			case 4: // length
+				if (region.getRegionType()==RegionType.RING) return "-";
+				return format.format(tool.getBounds(region).getLength());
+			case 5: // max
+				final double max = tool.getMaxIntensity(region);
+			    if (Double.isNaN(max)) return "-";
+				return format.format(max);
+			case 6: // in rad
+				if (region.getRegionType()!=RegionType.RING) return "-";
+				return format.format(tool.getBounds(region).getInner());
+			case 7: // out rad
+				if (region.getRegionType()!=RegionType.RING) return "-";
+				return format.format(tool.getBounds(region).getOuter());
+			case 8: // region
+				return tool.getBounds(region).toString();
+	
+			default:
+				return "Not found";
+			}
+		} catch (Throwable ne) {
+			// One must not throw RuntimeExceptions like null pointers from this
+			// methd becuase the user gets an eclipse dialog confusing them with 
+			// the error
+			logger.error("Cannot get value in info table", ne);
+			return "";
 		}
 	}
 	
