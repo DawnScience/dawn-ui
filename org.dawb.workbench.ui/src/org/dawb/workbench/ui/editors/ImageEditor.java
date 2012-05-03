538220693f7398e727e52463a8104878285f4c0c
Gábor Náray <gnaray@embl.fr>
2012-05-03 21:41:14 +0200

    Merged again.

--- org.dawb.workbench.ui/src/org/dawb/workbench/ui/editors/ImageEditor.java
+++ org.dawb.workbench.ui/src/org/dawb/workbench/ui/editors/ImageEditor.java
@@ -59,7 +59,7 @@ public class ImageEditor extends MultiPageEditorPart implements IReusableEditor,
 			int index = 0;
 			
 			try {
-				Collection<IEditorPart> extensions = EditorExtensionFactory.getEditors(getEditorInput());
+				Collection<IEditorPart> extensions = EditorExtensionFactory.getEditors(this);
 				if (extensions!=null && extensions.size()>0) {
 					for (IEditorPart iEditorPart : extensions) {
 						addPage(index, iEditorPart,  getEditorInput());
