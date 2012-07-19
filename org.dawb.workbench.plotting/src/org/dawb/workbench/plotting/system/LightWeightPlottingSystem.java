4883a25f8483223e85d9296f4f46c52cd1e8ae45
matthew.gerring <matthew.gerring@gmail.com>
2012-07-19 13:41:09 +0100

    Axes should clear label data sets when plotting 1D!

--- org.dawb.workbench.plotting/src/org/dawb/workbench/plotting/system/LightWeightPlottingSystem.java
+++ org.dawb.workbench.plotting/src/org/dawb/workbench/plotting/system/LightWeightPlottingSystem.java
@@ -627,8 +627,10 @@ public class LightWeightPlottingSystem extends AbstractPlottingSystem {
 	
 		if (xyGraph==null) return null;
 		
-		final Axis xAxis = (Axis)getSelectedXAxis();
-		final Axis yAxis = (Axis)getSelectedYAxis();
+		final AspectAxis xAxis = (AspectAxis)getSelectedXAxis();
+		xAxis.setLabelData(null);
+		final AspectAxis yAxis = (AspectAxis)getSelectedYAxis();
+		yAxis.setLabelData(null);
 
 		xAxis.setVisible(true);
 		yAxis.setVisible(true);
