9463c2d62ed4fbabd1e434ba7cb3389b934c5fb1
Mark Basham <mark.basham@diamond.ac.uk>
2012-09-03 16:21:41 +0100

    update to the Region Normalise Actor

--- org.dawb.passerelle.actors.dawn/src/org/dawb/passerelle/actors/dawn/RegionNormaliseActor.java
+++ org.dawb.passerelle.actors.dawn/src/org/dawb/passerelle/actors/dawn/RegionNormaliseActor.java
@@ -1,6 +1,9 @@
 package org.dawb.passerelle.actors.dawn;
 
+import java.beans.XMLEncoder;
+import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 import org.dawb.passerelle.common.actors.AbstractDataMessageTransformer;
 import org.dawb.passerelle.common.actors.ActorUtils;
@@ -24,43 +27,67 @@ import com.isencia.passerelle.actor.ProcessingException;
 
 public class RegionNormaliseActor extends AbstractDataMessageTransformer {
 
-	public ROIParameter NormalisationROI;
+	public ROIParameter normalisationROI;
+	public StringParameter roiName;
+	public StringParameter datasetName;
 
 	public RegionNormaliseActor(CompositeEntity container, String name)
 			throws NameDuplicationException, IllegalActionException {
 		super(container, name);
 
-		NormalisationROI = new ROIParameter(this, "NormalisationROI");
-		registerConfigurableParameter(NormalisationROI);
+		normalisationROI = new ROIParameter(this, "NormalisationROI");
+		registerConfigurableParameter(normalisationROI);
+		roiName = new StringParameter(this, "roiName");
+		registerConfigurableParameter(roiName);
+		datasetName = new StringParameter(this, "datasetName");
+		registerConfigurableParameter(datasetName);
 	}
 
 	@Override
 	protected DataMessageComponent getTransformedMessage(
 			List<DataMessageComponent> cache) throws ProcessingException {
 		// get the data out of the message, name of the item should be specified
-		List<IDataset> datasets = MessageUtils.getDatasets(cache);
+		final Map<String, Serializable>  data = MessageUtils.getList(cache);
 		
 		// get the roi out of the message, name of the roi should be specified
-		RectangularROI roi = (RectangularROI) NormalisationROI.getRoi();
+		RectangularROI roi = (RectangularROI) normalisationROI.getRoi();
+
+		// check the roi list, and see if one exists
+		try {
+			Map<String, ROIBase> rois = MessageUtils.getROIs(cache);
+
+			if(rois.containsKey(roiName.getExpression())) {
+				if (rois.get(roiName.getExpression()) instanceof RectangularROI) {
+					roi = (RectangularROI) rois.get(roiName.getExpression());
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 		// prepare the output message
 		DataMessageComponent result = new DataMessageComponent();
 		
-		// for each dataset process the output
-		for (IDataset dataset : datasets) {
-			AbstractDataset ds = DatasetUtils.convertToAbstractDataset(dataset.clone());
-			//ds = ds.cast(AbstractDataset.ARRAYFLOAT32);
+		// put all the datasets in for reprocessing
+		for (String key : data.keySet()) {
+			result.addList(key, (AbstractDataset) data.get(key));
+		}
+
+		// Normalise the specified dataset
+		String name = datasetName.getExpression();
+		if (data.containsKey(name)) {
+			AbstractDataset ds = ((AbstractDataset)data.get(name)).clone();
 			AbstractDataset[] profiles = ROIProfile.box(ds, roi);
 			AbstractDataset tile = profiles[1].reshape(profiles[1].getShape()[0],1);
 			double width = roi.getLengths()[0];
 			tile.idivide(width);
 			AbstractDataset correction = DatasetUtils.tile(tile, ds.getShape()[1]);
 			
-			
-			result.addList(dataset.getName(), ds.idivide(correction));
-			result.addList("Correction", correction);
-			
-			
+			result.addList(ds.getName()+"_norm", ds.idivide(correction));
+			result.addList(ds.getName()+"_correction_map", correction);
 		}
 		
 		// do the correction and put that into the pipeline., with a name that should be specified.
