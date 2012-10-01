28f17260f7aac1a444b999cca128de0136f9436d
matthew.gerring <matthew.gerring@gmail.com>
2012-10-01 10:55:49 +0100

    Removed always showing value view, it may be hidden.
    Conflicts:
    	org.dawb.workbench.ui/src/org/dawb/workbench/ui/editors/H5MultiEditor.java

--- org.dawb.workbench.ui/src/org/dawb/workbench/ui/editors/H5MultiEditor.java
+++ org.dawb.workbench.ui/src/org/dawb/workbench/ui/editors/H5MultiEditor.java
@@ -30,6 +30,7 @@ import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IReusableEditor;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.MultiPageEditorPart;
 import org.eclipse.ui.part.Page;
@@ -109,11 +110,17 @@ public class H5MultiEditor extends MultiPageEditorPart  implements ISlicablePlot
 			logger.error("Cannot initiate "+getClass().getName()+"!", e);
 		}
 		
-		try {
-			EclipseUtils.getActivePage().showView("org.dawb.passerelle.views.ValueView");
 
-		} catch (Throwable ignored) {
-			// Nowt
+		IWorkbenchPage page = EclipseUtils.getActivePage();
+		if (page != null) {
+			try {
+				if (page.findView("org.dawb.passerelle.views.ValueView")==null) {
+					page.showView("org.dawb.passerelle.views.ValueView");
+				}
+			} catch (PartInitException e) {
+				// do nothing
+			}
+
 		}
 		
 		final int lastIndex = index;
