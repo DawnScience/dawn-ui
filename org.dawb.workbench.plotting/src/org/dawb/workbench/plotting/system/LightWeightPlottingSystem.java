526314388f40469c53e7421ea21c94a54a0b6d6f
matthew.gerring <matthew.gerring@gmail.com>
2012-10-10 15:02:40 +0100

    Edit data reduction calls to allow for dimension.

--- org.dawb.workbench.plotting/src/org/dawb/workbench/plotting/system/LightWeightPlottingSystem.java
+++ org.dawb.workbench.plotting/src/org/dawb/workbench/plotting/system/LightWeightPlottingSystem.java
@@ -514,7 +514,7 @@ public class LightWeightPlottingSystem extends AbstractPlottingSystem {
 			if (shape!=null && Arrays.equals(shape, data.getShape())) {
 				if (getDisplay().getThread()==Thread.currentThread()) {
 					if (data.getName()!=null) xyGraph.setTitle(data.getName());
-					image.setData(data, axes==null?image.getAxes():axes, false);
+					image.setData(data, axes, false);
 					fireTraceUpdated(new TraceEvent(image));
 				} else {
 					Display.getDefault().syncExec(new Runnable() {
@@ -522,7 +522,7 @@ public class LightWeightPlottingSystem extends AbstractPlottingSystem {
 							// This will keep the previous zoom level if there was one
 							// and will be faster than createPlot2D(...) which autoscales.
 							if (data.getName()!=null) xyGraph.setTitle(data.getName());
-							image.setData(data, axes==null?image.getAxes():axes, false);
+							image.setData(data, axes, false);
 							fireTraceUpdated(new TraceEvent(image));
 						}
 					});
