b014ca94e9985919494058ac6fa017b8b5aad3c1
Jacob Filik <jacob.filik@diamond.ac.uk>
2012-11-12 16:31:33 +0000

    Added DiffractionTool tree view to Diffraction Defaults preference page

--- org.dawb.workbench.plotting/src/org/dawb/workbench/plotting/tools/diffraction/DiffractionDefaultMetadata.java
+++ org.dawb.workbench.plotting/src/org/dawb/workbench/plotting/tools/diffraction/DiffractionDefaultMetadata.java
@@ -11,8 +11,9 @@ import uk.ac.diamond.scisoft.analysis.io.DiffractionMetaDataAdapter;
 import uk.ac.diamond.scisoft.analysis.io.IDiffractionMetadata;
 
 public class DiffractionDefaultMetadata {
+	
 	/**
-	 * Static method to produce a Detector properties object populated with default values
+	 * Static method to produce a Detector properties object populated with persisted values
 	 * from the preferences store
 	 * 
 	 * @param shape
@@ -20,7 +21,7 @@ public class DiffractionDefaultMetadata {
 	 *            Used to produce the initial detector origin
 	 *            
 	 */
-	public static DetectorProperties getDefaultDetectorProperties(int[] shape) {
+	public static DetectorProperties getPersistedDetectorProperties(int[] shape) {
 		
 		int heightInPixels = shape[0];
 		int widthInPixels = shape[1];
@@ -43,10 +44,10 @@ public class DiffractionDefaultMetadata {
 	}
 	
 	/**
-	 * Static method to produce a DiffractionCrystalEnvironment properties object populated with default values
+	 * Static method to produce a DiffractionCrystalEnvironment properties object populated with persisted values
 	 * from the preferences store
 	 */
-	public static DiffractionCrystalEnvironment getDefaultDiffractionCrystalEnvironment() {
+	public static DiffractionCrystalEnvironment getPersistedDiffractionCrystalEnvironment() {
 		double lambda = Activator.getDefault().getPreferenceStore().getDouble(DiffractionToolConstants.LAMBDA);
 		double startOmega = Activator.getDefault().getPreferenceStore().getDouble(DiffractionToolConstants.START_OMEGA);
 		double rangeOmega = Activator.getDefault().getPreferenceStore().getDouble(DiffractionToolConstants.RANGE_OMEGA);
@@ -55,14 +56,83 @@ public class DiffractionDefaultMetadata {
 		return new DiffractionCrystalEnvironment(lambda, startOmega, rangeOmega, exposureTime);
 	}
 	
+	
+	/**
+	 * Static method to produce a Detector properties object populated with default values
+	 * from the preferences store
+	 * 
+	 * @param shape
+	 *            shape from the AbstractDataset the detector properties are created for
+	 *            Used to produce the initial detector origin
+	 *            
+	 */
+	public static DetectorProperties getDefaultDetectorProperties(int[] shape) {
+		
+		int heightInPixels = shape[0];
+		int widthInPixels = shape[1];
+		
+		// Get the default values from the preference store
+		double pixelSizeX  = Activator.getDefault().getPreferenceStore().getDefaultDouble(DiffractionToolConstants.PIXEL_SIZE_X);
+		double pixelSizeY  = Activator.getDefault().getPreferenceStore().getDefaultDouble(DiffractionToolConstants.PIXEL_SIZE_Y);
+		double distance = Activator.getDefault().getPreferenceStore().getDefaultDouble(DiffractionToolConstants.DISTANCE);
+		
+		// Create the detector origin vector based on the above
+		double[] detectorOrigin = { (widthInPixels - widthInPixels/2d) * pixelSizeX, (heightInPixels - heightInPixels/2d) * pixelSizeY, distance };
+		
+		// The rotation of the detector relative to the reference frame - assume no rotation
+		double detectorRotationX = Activator.getDefault().getPreferenceStore().getDefaultDouble(DiffractionToolConstants.DETECTOR_ROTATION_X);
+		double detectorRotationY = Activator.getDefault().getPreferenceStore().getDefaultDouble(DiffractionToolConstants.DETECTOR_ROTATION_Y);
+		double detectorRotationZ = Activator.getDefault().getPreferenceStore().getDefaultDouble(DiffractionToolConstants.DETECTOR_ROTATION_Z);
+
+		return new DetectorProperties(new Vector3d(detectorOrigin), heightInPixels, widthInPixels, 
+				pixelSizeX, pixelSizeY, detectorRotationX, detectorRotationY, detectorRotationZ);
+	}
+	
+	/**
+	 * Static method to produce a DiffractionCrystalEnvironment properties object populated with default values
+	 * from the preferences store
+	 */
+	public static DiffractionCrystalEnvironment getDefaultDiffractionCrystalEnvironment() {
+		double lambda = Activator.getDefault().getPreferenceStore().getDefaultDouble(DiffractionToolConstants.LAMBDA);
+		double startOmega = Activator.getDefault().getPreferenceStore().getDefaultDouble(DiffractionToolConstants.START_OMEGA);
+		double rangeOmega = Activator.getDefault().getPreferenceStore().getDefaultDouble(DiffractionToolConstants.RANGE_OMEGA);
+		double exposureTime = Activator.getDefault().getPreferenceStore().getDefaultDouble(DiffractionToolConstants.EXPOSURE_TIME);
+		
+		return new DiffractionCrystalEnvironment(lambda, startOmega, rangeOmega, exposureTime);
+	}
+	
+	/**
+	 * Static method to set the default DiffractionCrystalEnvironment values in the 
+	 * from the preferences store
+	 */
+	public static void setPersistedDiffractionCrystalEnvironmentValues(DiffractionCrystalEnvironment dce){
+		Activator.getDefault().getPreferenceStore().setValue(DiffractionToolConstants.LAMBDA, dce.getWavelength());
+		Activator.getDefault().getPreferenceStore().setValue(DiffractionToolConstants.START_OMEGA, dce.getPhiStart());
+		Activator.getDefault().getPreferenceStore().setValue(DiffractionToolConstants.RANGE_OMEGA, dce.getPhiRange());
+	}
+	
+	/**
+	 * Static method to set the default DetectorProperties values in the 
+	 * from the preferences store
+	 */
+	public static void setPersistedDetectorPropertieValues(DetectorProperties detprop) {
+		Activator.getDefault().getPreferenceStore().setValue(DiffractionToolConstants.PIXEL_SIZE_X, detprop.getVPxSize());
+		Activator.getDefault().getPreferenceStore().setValue(DiffractionToolConstants.PIXEL_SIZE_Y, detprop.getHPxSize());
+		Activator.getDefault().getPreferenceStore().setValue(DiffractionToolConstants.DISTANCE, detprop.getOrigin().z);
+		
+	}
+	
 	/**
 	 * Static method to obtain a DiffractionMetaDataAdapter populated with default values
 	 * from the preferences store to act as a starting point for images without metadata
 	 */
 	public static IDiffractionMetadata getDiffractionMetadata(int[] shape) {
 		
-		final DetectorProperties detprop = getDefaultDetectorProperties(shape);
-		final DiffractionCrystalEnvironment diffenv = getDefaultDiffractionCrystalEnvironment();
+		final DetectorProperties detprop = getPersistedDetectorProperties(shape);
+		final DiffractionCrystalEnvironment diffenv = getPersistedDiffractionCrystalEnvironment();
+		
+		final DetectorProperties detpropOrig = getDefaultDetectorProperties(shape);
+		final DiffractionCrystalEnvironment diffenvOrig = getDefaultDiffractionCrystalEnvironment();
 		
 		return new DiffractionMetaDataAdapter() {
 			private static final long serialVersionUID = DiffractionMetaDataAdapter.serialVersionUID;
@@ -79,12 +149,12 @@ public class DiffractionDefaultMetadata {
 			
 			@Override
 			public DetectorProperties getOriginalDetector2DProperties() {
-				return detprop;
+				return detpropOrig;
 			}
 
 			@Override
 			public DiffractionCrystalEnvironment getOriginalDiffractionCrystalEnvironment() {
-				return diffenv;
+				return diffenvOrig;
 			}
 
 			@Override
