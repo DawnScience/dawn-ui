d54cb6be35c4395cc414c65e716c01201d1944bc
Mark Basham <mark.basham@diamond.ac.uk>
2013-04-12 11:22:04 +0100

    Update for windows to make newline separator generic

--- org.dawnsci.plotting/src/org/dawnsci/plotting/tools/fitting/FunctionFittingTool.java
+++ org.dawnsci.plotting/src/org/dawnsci/plotting/tools/fitting/FunctionFittingTool.java
@@ -71,7 +71,6 @@ import uk.ac.diamond.scisoft.analysis.fitting.functions.StraightLine;
 import uk.ac.diamond.scisoft.analysis.optimize.ApacheNelderMead;
 import uk.ac.diamond.scisoft.analysis.optimize.GeneticAlg;
 import uk.ac.diamond.scisoft.analysis.optimize.IOptimizer;
-import uk.ac.diamond.scisoft.analysis.optimize.NelderMead;
 import uk.ac.diamond.scisoft.analysis.roi.RectangularROI;
 import uk.ac.gda.common.rcp.util.GridUtils;
 
@@ -621,7 +620,7 @@ public class FunctionFittingTool extends AbstractToolPage implements IFunctionSe
 				try {
 					result = Double.toString(compFunction.getFunction(index).getParameterValue(0));
 					for (int i = 1; i < compFunction.getFunction(index).getNoOfParameters(); i++) {
-						result += "\n";
+						result += System.getProperty("line.separator");
 						result += compFunction.getFunction(index).getParameterValue(i);
 					}
 				} catch (Exception e) {
@@ -632,7 +631,7 @@ public class FunctionFittingTool extends AbstractToolPage implements IFunctionSe
 				try {
 					result = Double.toString(resultFunction.getFunction(index).getParameterValue(0));
 					for (int i = 1; i < resultFunction.getFunction(index).getNoOfParameters(); i++) {
-						result += "\n";
+						result += System.getProperty("line.separator");
 						result += resultFunction.getFunction(index).getParameterValue(i);
 					}
 				} catch (Exception e) {
