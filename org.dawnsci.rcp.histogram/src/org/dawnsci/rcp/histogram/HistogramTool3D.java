1963f6afd460735c09df89a271fce9bc0f691ccc
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2013-04-22 14:58:24 +0100

    Make histogram with 2d surface: add new class

---
+++ org.dawnsci.rcp.histogram/src/org/dawnsci/rcp/histogram/HistogramTool3D.java
@@ -0,0 +1,14 @@
+package org.dawnsci.rcp.histogram;
+
+/**
+ * Histogram toolpage for 3D
+ * @author wqk87977
+ *
+ */
+public class HistogramTool3D extends HistogramToolPage {
+
+	@Override
+	public ToolPageRole getToolPageRole() {
+		return ToolPageRole.ROLE_3D;
+	}
+}
