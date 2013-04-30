a4ce129caac858796738ab2ebd602930698ccc95
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2013-04-30 17:03:09 +0100

    Image processing tool: *add auxiliary dataset processing *add input field to load the auxiliary data

--- org.dawnsci.plotting/src/org/dawnsci/plotting/tools/processing/ImageNormalisationProcessTool.java
+++ org.dawnsci.plotting/src/org/dawnsci/plotting/tools/processing/ImageNormalisationProcessTool.java
@@ -1,27 +1,48 @@
 package org.dawnsci.plotting.tools.processing;
 
+import java.io.File;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map.Entry;
 
+import org.dawb.common.ui.monitor.ProgressMonitorWrapper;
 import org.dawb.common.ui.plot.AbstractPlottingSystem;
+import org.dawb.common.util.io.IOUtils;
 import org.dawnsci.plotting.api.region.IRegion;
 import org.dawnsci.plotting.api.region.IRegion.RegionType;
 import org.dawnsci.plotting.api.trace.IImageTrace;
 import org.dawnsci.plotting.api.trace.ITrace;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.fieldassist.ContentProposalAdapter;
+import org.eclipse.jface.fieldassist.TextContentAdapter;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.DropTarget;
+import org.eclipse.swt.dnd.DropTargetAdapter;
+import org.eclipse.swt.dnd.DropTargetEvent;
+import org.eclipse.swt.dnd.FileTransfer;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Spinner;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.part.ResourceTransfer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,10 +50,13 @@ import uk.ac.diamond.scisoft.analysis.dataset.AbstractDataset;
 import uk.ac.diamond.scisoft.analysis.dataset.DatasetUtils;
 import uk.ac.diamond.scisoft.analysis.dataset.IDataset;
 import uk.ac.diamond.scisoft.analysis.dataset.IntegerDataset;
+import uk.ac.diamond.scisoft.analysis.io.LoaderFactory;
+import uk.ac.diamond.scisoft.analysis.monitor.IMonitor;
 import uk.ac.diamond.scisoft.analysis.optimize.ApachePolynomial;
 import uk.ac.diamond.scisoft.analysis.roi.IROI;
 import uk.ac.diamond.scisoft.analysis.roi.ROIProfile;
 import uk.ac.diamond.scisoft.analysis.roi.RectangularROI;
+import uk.ac.gda.ui.content.FileContentProposalProvider;
 
 /**
  * Tool to normalize an image with given specific parameters
@@ -43,7 +67,7 @@ public class ImageNormalisationProcessTool extends ImageProcessingTool {
 
 	private final Logger logger = LoggerFactory.getLogger(ImageNormalisationProcessTool.class);
 	private AbstractDataset profile;
-	
+
 	private boolean divideNormalization = false;
 	private Spinner smoothingSpinner;
 	private int smoothLevel = 1;
@@ -54,8 +78,14 @@ public class ImageNormalisationProcessTool extends ImageProcessingTool {
 
 	private NormaliseType type = NormaliseType.NONE;
 
+	private Text inputLocation;
+	private String inputFile;
+	private Button inputBrowse;
+	private Composite auxComp;
+
+	private IDataset currentAuxData;
+
 	public ImageNormalisationProcessTool() {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -78,6 +108,12 @@ public class ImageNormalisationProcessTool extends ImageProcessingTool {
 			radioGroupNorm.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 			radioGroupNorm.setText("Normalisation type");
 			createRadioControls(radioGroupNorm, createNormActions());
+
+			auxComp = new Composite(radioGroupNorm, SWT.NONE);
+			auxComp.setLayout(new GridLayout(1, false));
+			auxComp.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+			createInputField(auxComp);
+			auxComp.setVisible(false);
 
 			Group radioGroupOperation = new Group(parent, SWT.NONE);
 			radioGroupOperation.setLayout(new GridLayout(1, false));
@@ -116,6 +152,7 @@ public class ImageNormalisationProcessTool extends ImageProcessingTool {
 				public void run() {
 					type = NormaliseType.NONE;
 					updateProfiles();
+					auxComp.setVisible(false);
 				}
 			}
 		);
@@ -125,6 +162,7 @@ public class ImageNormalisationProcessTool extends ImageProcessingTool {
 					public void run() {
 						type = NormaliseType.ROI;
 						updateProfiles();
+						auxComp.setVisible(false);
 					}
 				}
 			);
@@ -133,7 +171,8 @@ public class ImageNormalisationProcessTool extends ImageProcessingTool {
 					@Override
 					public void run() {
 						type = NormaliseType.AUX;
-//						updateProfiles();
+						updateProfiles();
+						auxComp.setVisible(true);
 						System.out.println("Auxiliary normalisation");
 					}
 				}
@@ -142,6 +181,40 @@ public class ImageNormalisationProcessTool extends ImageProcessingTool {
 		radioActions.add(roiNormalisation);
 		radioActions.add(auxNormalisation);
 		return radioActions;
+	}
+
+	private void createInputField(Composite parent){
+		Label lb = new Label(parent, SWT.NONE);
+		lb.setText("Choose Auxiliary Data Input:");
+
+		Composite comp = new Composite(parent, SWT.NONE);
+		comp.setLayout(new GridLayout(2, false));
+		comp.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+		inputLocation = new Text(comp, SWT.BORDER);
+		if (inputFile != null)
+			inputLocation.setText(inputFile);
+		inputLocation.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		FileContentProposalProvider prov = new FileContentProposalProvider();
+		ContentProposalAdapter ad = new ContentProposalAdapter(inputLocation, 
+				new TextContentAdapter(), prov, null, null);
+		ad.setProposalAcceptanceStyle(ContentProposalAdapter.PROPOSAL_REPLACE);
+		inputLocation.setToolTipText("Input file location");
+		inputLocation.addSelectionListener(getInputLocationListener());
+		//add drop support
+		DropTarget dt = new DropTarget(inputLocation, DND.DROP_MOVE
+				| DND.DROP_DEFAULT | DND.DROP_COPY);
+		dt.setTransfer(new Transfer[] { TextTransfer.getInstance(),
+				FileTransfer.getInstance(), ResourceTransfer.getInstance() });
+		dt.addDropListener(getDropTargetEvent());
+//		GridData gd = new GridData(GridData.FILL_BOTH);
+////		gd.widthHint = 250;
+//		inputLocation.setLayoutData(gd);
+
+		inputBrowse = new Button(comp, SWT.NONE);
+		inputBrowse.setText("...");
+		inputBrowse.setToolTipText("Choose Auxiliary Data Input");
+		inputBrowse.addSelectionListener(getInputBrowseListener());
 	}
 
 	private List<Entry<String, Action>> createOperationActions(){
@@ -171,21 +244,96 @@ public class ImageNormalisationProcessTool extends ImageProcessingTool {
 		return radioActions;
 	}
 
+	private SelectionListener getInputLocationListener(){
+		return new SelectionAdapter() {
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				final String path = inputLocation.getText();
+				if (IOUtils.checkFile(path, true)) {
+					inputFile = path;
+					currentAuxData = loadDataset(path);
+					if(currentAuxData == null) return;
+					if(!auxiliaryDatasets.containsKey(currentAuxData.getName()))
+						addDataset(currentAuxData);
+					else
+						currentAuxData = auxiliaryDatasets.get(currentAuxData.getName());
+					updateProfiles();
+				} else {
+					inputLocation.setText(inputFile);
+				}
+			}
+		};
+	}
+
+	private SelectionListener getInputBrowseListener(){
+		return new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				FileDialog fChooser = new FileDialog(getSite().getShell());
+				fChooser.setText("Choose File to load from");
+				fChooser.setFilterPath(inputFile);
+				final String path = fChooser.open();
+				if (IOUtils.checkFile(path, true)) {
+					inputFile = path;
+					currentAuxData = loadDataset(path);
+					if(currentAuxData == null) return;
+					if(!auxiliaryDatasets.containsKey(currentAuxData.getName()))
+						addDataset(currentAuxData);
+					else
+						currentAuxData = auxiliaryDatasets.get(currentAuxData.getName());
+					updateProfiles();
+				}
+			}
+		};
+	}
+	
+	private DropTargetAdapter getDropTargetEvent(){
+		return new DropTargetAdapter() {
+			@Override
+			public void drop(DropTargetEvent event) {
+				String path = null;
+				Object data = event.data;
+				if (data instanceof IResource[]) {
+					IResource[] res = (IResource[]) data;
+					path = res[0].getRawLocation().toOSString();
+				} else if (data instanceof File[]) {
+					path = ((File[]) data)[0].getAbsolutePath();
+				}
+				if (path != null) {
+					inputLocation.setText(path);
+					if (IOUtils.checkFile(path, true)) {
+						inputFile = path;
+						currentAuxData = loadDataset(path);
+						if(currentAuxData == null) return;
+						if (!auxiliaryDatasets.containsKey(currentAuxData.getName()))
+							addDataset(currentAuxData);
+						else
+							currentAuxData = auxiliaryDatasets
+									.get(currentAuxData.getName());
+						updateProfiles();
+					}
+				}
+			}
+		};
+	}
+
 	@Override
 	protected void createNormProfile(IImageTrace image, IRegion region, IROI roi,
 			boolean tryUpdate, boolean isDrag, IProgressMonitor monitor) {
-			
+			AbstractDataset data = (AbstractDataset)image.getData();
+			AbstractDataset ds = data.clone();
+			AbstractDataset tmpProfile = profile.clone();
+			AbstractDataset tile = tmpProfile.reshape(tmpProfile.getShape()[0],1);
+			double width = 0;
+
 			switch (type) {
 			case NONE:
-				normProfilePlotSystem.updatePlot2D((AbstractDataset)image.getData(), image.getAxes(), monitor);
+				normProfilePlotSystem.updatePlot2D(data, image.getAxes(), monitor);
 				break;
 			case ROI:
-				AbstractDataset ds = ((AbstractDataset)image.getData()).clone();
-				AbstractDataset tmpProfile =  profile.clone();
-				AbstractDataset tile = tmpProfile.reshape(tmpProfile.getShape()[0],1);
 				if(roi == null) return;
 				
-				double width = ((RectangularROI)roi).getLengths()[0];
+				width = ((RectangularROI)roi).getLengths()[0];
 				tile.idivide(width);
 				AbstractDataset correction = DatasetUtils.tile(tile, ds.getShape()[1]);
 				if(divideNormalization){
@@ -196,7 +344,16 @@ public class ImageNormalisationProcessTool extends ImageProcessingTool {
 				normProfilePlotSystem.updatePlot2D(ds, image.getAxes(), monitor);
 				break;
 			case AUX:
-				normProfilePlotSystem.clear();
+				if(currentAuxData == null) return;
+				
+				width = currentAuxData.getShape()[0];
+				tile.idivide(width);
+				if(divideNormalization){
+					ds.idivide(DatasetUtils.tile(tile, ds.getShape()[1]));
+				} else {
+					ds.isubtract(DatasetUtils.tile(tile, ds.getShape()[1]));
+				}
+				normProfilePlotSystem.updatePlot2D(ds, image.getAxes(), monitor);
 				break;
 			default:
 				break;
@@ -209,7 +366,7 @@ public class ImageNormalisationProcessTool extends ImageProcessingTool {
 			IProgressMonitor monitor) {
 		List<IDataset> data = new ArrayList<IDataset>();
 		Collection<ITrace> plotted = null;
-
+		AbstractDataset ds = ((AbstractDataset)image.getData()).clone();
 		switch (type) {
 		case NONE:
 			data.add(new IntegerDataset(image.getData().getShape()[0]));
@@ -219,7 +376,6 @@ public class ImageNormalisationProcessTool extends ImageProcessingTool {
 			registerTraces(region, plotted);
 			break;
 		case ROI:
-			AbstractDataset ds = ((AbstractDataset)image.getData()).clone();
 			if(roi == null)return;
 			
 			AbstractDataset[] profiles = ROIProfile.box(ds, (RectangularROI)roi);
@@ -238,7 +394,32 @@ public class ImageNormalisationProcessTool extends ImageProcessingTool {
 			registerTraces(region, plotted);
 			break;
 		case AUX:
-			normProfilePlotSystem.clear();
+			
+			if(currentAuxData == null)return;
+			double width = 0, height = 0;
+			if(currentAuxData.getShape().length>2){
+				width = currentAuxData.getShape()[1];
+				height = currentAuxData.getShape()[2];
+			} else {
+				width = currentAuxData.getShape()[0];
+				height = currentAuxData.getShape()[1];
+			}
+			
+			RectangularROI auxROI = new RectangularROI(0, 0, width, height, 0);
+			
+			profile = ROIProfile.box(ds, (RectangularROI)auxROI)[1];
+			if(smoothLevel > 1){
+				try {
+					profile = ApachePolynomial.getPolynomialSmoothed(((AbstractDataset)image.getAxes().get(1)), profile, smoothLevel, 3);
+				} catch (Exception e) {
+					logger.error("Could not smooth the plot:"+e);
+				}
+			}
+			data.add(profile);
+			data.get(0).setName("Norm");
+			plotted = preNormProfilePlotSystem.updatePlot1D(image.getAxes().get(1), data, monitor);
+//			if(plotted == null) return;
+//			registerTraces(region, plotted);
 			break;
 		default:
 			break;
@@ -248,5 +429,26 @@ public class ImageNormalisationProcessTool extends ImageProcessingTool {
 	@Override
 	protected RegionType getCreateRegionType() {
 		return RegionType.XAXIS;
+	}
+
+	private IDataset loadDataset(final String path) {
+		Job loadFileJob = new Job("Loading File") {
+			
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				final IMonitor mon = new ProgressMonitorWrapper(monitor);
+				try {
+					currentAuxData = LoaderFactory.getDataSet(path, "/entry1/analyser/data", mon);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return Status.CANCEL_STATUS;
+				}
+				return Status.OK_STATUS;
+			}
+		};
+		loadFileJob.schedule();
+		
+		return null;
 	}
 }
