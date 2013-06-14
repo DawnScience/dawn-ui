9411408ebf04974200f70d423135c96b4c7bfa83
matthew.gerring <matthew.gerring@gmail.com>
2013-06-14 10:31:41 +0100

    Added comment about using getAdpater

--- org.dawnsci.plotting.api/src/org/dawnsci/plotting/api/IPlottingContainer.java
+++ org.dawnsci.plotting.api/src/org/dawnsci/plotting/api/IPlottingContainer.java
@@ -3,7 +3,11 @@ package org.dawnsci.plotting.api;
 
 /**
  * Any editor, view or control can implement this interface to allow access to its
- * embedded plotting system 
+ * embedded plotting system.
+ * 
+ * We should replace this with 
+ * <code>IPlottingSystem system = (IPlottingSystem)part.getAdpater(IPlottingSystem.class)</code> 
+ * instead but this requires the getAdpater(...) method to be implemented in several places.
  */
 public interface IPlottingContainer {
 	/**
