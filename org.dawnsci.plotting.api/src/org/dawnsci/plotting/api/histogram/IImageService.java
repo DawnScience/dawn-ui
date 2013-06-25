dbc3bfdc3681369d351c1621ef25f4ba511bfaa8
Gábor Náray <gnaray@embl.fr>
2013-06-25 14:41:02 +0200

    Updated getFastStatistics method to return real mean and max.

--- org.dawnsci.plotting.api/src/org/dawnsci/plotting/api/histogram/IImageService.java
+++ org.dawnsci.plotting.api/src/org/dawnsci/plotting/api/histogram/IImageService.java
@@ -52,14 +52,18 @@ public interface IImageService {
 	public Image getImage(ImageServiceBean bean) throws Exception;
 	
 	/**
-	 * Call to calculate max, min to be used in the histogram. These are the
-	 * max and min inside the bounds which are to be part of the main histogram.
+	 * Call to calculate min, max to be used in the histogram. These are the
+	 * min and max inside the bounds which are to be part of the main histogram.
+	 * The mean and real max values are also calculated.
 	 * 
 	 * The AbstractDataset and the histogram type must be set in the bean.
-	 * If the cut 
+	 * 
+	 * The return array[2] was added in "Updated for Diffraction Tool." commit,
+	 * but no trace of such usage. However it should not be removed, because
+	 * it is useful as return array[3].
 	 * 
 	 * @param bean
-	 * @return
+	 * @return [0] = min [1] = max(=mean*constant) [2] = mean [3] max
 	 */
 	public float[] getFastStatistics(ImageServiceBean bean);
 	
