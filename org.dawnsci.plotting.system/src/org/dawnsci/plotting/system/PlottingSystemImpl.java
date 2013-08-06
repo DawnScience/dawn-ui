65458ab26380b8a6320990a790e5ff7a3d0aff6b
Jacob Filik <jacob.filik@diamond.ac.uk>
2013-08-06 13:31:10 +0100

    Changed getTraces to return LinkedHashSet so order is remembered

--- org.dawnsci.plotting.system/src/org/dawnsci/plotting/system/PlottingSystemImpl.java
+++ org.dawnsci.plotting.system/src/org/dawnsci/plotting/system/PlottingSystemImpl.java
@@ -14,9 +14,9 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -644,7 +644,7 @@ public class PlottingSystemImpl extends AbstractPlottingSystem {
 
 	public Collection<ITrace> getTraces() {
 		if (traceMap==null) return Collections.emptyList();
-		return new HashSet<ITrace>(traceMap.values());
+		return new LinkedHashSet<ITrace>(traceMap.values());
 	}
 
 	@Override
