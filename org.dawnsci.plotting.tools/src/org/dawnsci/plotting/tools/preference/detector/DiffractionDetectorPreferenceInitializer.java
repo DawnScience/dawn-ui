1c7eddef207cb39396cbdaa94037225411b46207
Peter Chang <Peter.Chang@diamond.ac.uk>
2013-08-08 14:45:31 +0100

    Added I18's CCD detector

--- org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/preference/detector/DiffractionDetectorPreferenceInitializer.java
+++ org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/preference/detector/DiffractionDetectorPreferenceInitializer.java
@@ -18,7 +18,7 @@ public class DiffractionDetectorPreferenceInitializer extends AbstractPreference
 		
 		DiffractionDetectors dds = new DiffractionDetectors();
 		
-		//Pilatus 2M
+		// Pilatus 2M
 		DiffractionDetector dd = new DiffractionDetector();
 		dd.setDetectorName("Pilatus2m");
 		dd.setxPixelSize(Amount.valueOf(0.172, SI.MILLIMETRE));
@@ -28,7 +28,7 @@ public class DiffractionDetectorPreferenceInitializer extends AbstractPreference
 		dds.addDiffractionDetector(dd);
 		dds.setDiffractionDetector(dd);
 		
-		//Pilatus 2M
+		// Pilatus 2M
 		DiffractionDetector dd2 = new DiffractionDetector();
 		dd2.setDetectorName("Pilatus6m");
 		dd2.setxPixelSize(Amount.valueOf(0.172, SI.MILLIMETRE));
@@ -37,7 +37,7 @@ public class DiffractionDetectorPreferenceInitializer extends AbstractPreference
 		dd2.setNumberOfPixelsY(2463);
 		dds.addDiffractionDetector(dd2);
 		
-		//Pilatus100k
+		// Pilatus100k
 		DiffractionDetector dd3 = new DiffractionDetector();
 		dd3.setDetectorName("Pilatus100k");
 		dd3.setxPixelSize(Amount.valueOf(0.172, SI.MILLIMETRE));
@@ -46,7 +46,7 @@ public class DiffractionDetectorPreferenceInitializer extends AbstractPreference
 		dd3.setNumberOfPixelsY(487);
 		dds.addDiffractionDetector(dd3);
 		
-		//Pilatus100k
+		// Pilatus100k
 		DiffractionDetector dd4 = new DiffractionDetector();
 		dd4.setDetectorName("Pilatus300k");
 		dd4.setxPixelSize(Amount.valueOf(0.172, SI.MILLIMETRE));
@@ -55,7 +55,7 @@ public class DiffractionDetectorPreferenceInitializer extends AbstractPreference
 		dd4.setNumberOfPixelsY(487);
 		dds.addDiffractionDetector(dd4);
 		
-		//Pixium RF4343
+		// Pixium RF4343
 		DiffractionDetector dd5 = new DiffractionDetector();
 		dd5.setDetectorName("Pixium RF4343");
 		dd5.setxPixelSize(Amount.valueOf(0.148, SI.MILLIMETRE));
@@ -64,7 +64,7 @@ public class DiffractionDetectorPreferenceInitializer extends AbstractPreference
 		dd5.setNumberOfPixelsY(2881);
 		dds.addDiffractionDetector(dd5);
 		
-		//Perkin Elmer 1621 EN
+		// Perkin Elmer 1621 EN
 		DiffractionDetector dd6 = new DiffractionDetector();
 		dd6.setDetectorName("Perkin Elmer 1621 EN");
 		dd6.setxPixelSize(Amount.valueOf(0.2, SI.MILLIMETRE));
@@ -73,7 +73,7 @@ public class DiffractionDetectorPreferenceInitializer extends AbstractPreference
 		dd6.setNumberOfPixelsY(2048);
 		dds.addDiffractionDetector(dd6);
 		
-		//ADSC 	Q315r 
+		// ADSC Q315r 
 		DiffractionDetector dd7 = new DiffractionDetector();
 		dd7.setDetectorName("ADSC Q315r");
 		dd7.setxPixelSize(Amount.valueOf(0.1026, SI.MILLIMETRE));
@@ -82,7 +82,7 @@ public class DiffractionDetectorPreferenceInitializer extends AbstractPreference
 		dd7.setNumberOfPixelsY(3072);
 		dds.addDiffractionDetector(dd7);
 		
-		//MAR 345 image plate
+		// MAR 345 image plate
 		DiffractionDetector dd8 = new DiffractionDetector();
 		dd8.setDetectorName("MAR 345 image plate");
 		dd8.setxPixelSize(Amount.valueOf(0.1000, SI.MILLIMETRE));
@@ -91,6 +91,15 @@ public class DiffractionDetectorPreferenceInitializer extends AbstractPreference
 		dd8.setNumberOfPixelsY(3450);
 		dds.addDiffractionDetector(dd8);
 		
+		// Photonic Science X-ray FDI-VHR 125mm
+		DiffractionDetector dd9 = new DiffractionDetector();
+		dd9.setDetectorName("Photonic Science X-ray FDI-VHR 125mm");
+		dd9.setxPixelSize(Amount.valueOf(26.0, SI.MICRO(SI.METRE)));
+		dd9.setyPixelSize(Amount.valueOf(26.0, SI.MICRO(SI.METRE)));
+		dd9.setNumberOfPixelsX(4008);
+		dd9.setNumberOfPixelsY(2663);
+		dds.addDiffractionDetector(dd9);
+
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		XMLEncoder xmlEncoder = new XMLEncoder(baos);
 		xmlEncoder.writeObject(dds);
