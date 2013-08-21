2981a157c9e1ae5a6982350cdc956d98a585c6c1
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2013-08-21 11:28:45 +0100

    Fix class casting error and remove a duplicate line from beam center refinement instructions

--- org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/diffraction/DiffractionTool.java
+++ org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/diffraction/DiffractionTool.java
@@ -778,7 +778,7 @@ public class DiffractionTool extends AbstractToolPage implements CalibrantSelect
 					if (peaks==null) throw new Exception("Cannot find peaks!");
 					beamOffset.setInitPeaks(peaks);
 					
-					beamOffset.optimize(((IDiffractionMetadata) dataset.getMetadata()).getDetector2DProperties().getBeamCentreCoords());
+					beamOffset.optimize(getDiffractionMetaData().getDetector2DProperties().getBeamCentreCoords());
 				} catch (Throwable ne) {
 					
 					/**
@@ -793,11 +793,10 @@ public class DiffractionTool extends AbstractToolPage implements CalibrantSelect
 							"2. Open the 'Radial Profile' tool (from the plot containing the image).\n" +
 							"3. Select a sector which bisects the rings wanted.\n"+
 							"4. In the 'Radial Profile' tool press 'Lock to Metadata' button.\n"+
-							"5. In the 'Radial Profile' tool press 'Lock to Metadata' button.\n"+
-							"6. Select 'q' from the 'Select x axis values' list in the 'Radial Profile' toolbar.\n"+
-							"7. In the 'Radial Profile' tool select peak fitting.\n"+
-							"8. Set up a peak fit on all the rings which the redial profile found.\n"+
-							"9. Now run the refine action in the diffraction tool again.\n\n"+
+							"5. Select 'q' from the 'Select x axis values' list in the 'Radial Profile' toolbar.\n"+
+							"6. In the 'Radial Profile' tool select peak fitting.\n"+
+							"7. Set up a peak fit on all the rings which the redial profile found.\n"+
+							"8. Now run the refine action in the diffraction tool again.\n\n"+
 							"Please note that the algorithm may not converge. A job is run for the refinement which may be stopped.\n"+
 							"Please contact your support representative for more training/help with refinement.\n\n"+
 							"(NOTE This dialog can be kept open as a guide while doing the proceedure.)",
