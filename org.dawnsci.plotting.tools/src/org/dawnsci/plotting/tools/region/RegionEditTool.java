c106126ed3c6448c389e54f39f85d946bbf8c3e8
matthew.gerring <matthew.gerring@gmail.com>
2013-09-12 10:43:02 +0100

    Fix to http://jira.diamond.ac.uk/browse/SCI-1369

--- org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/region/RegionEditTool.java
+++ org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/region/RegionEditTool.java
@@ -49,77 +49,84 @@ public class RegionEditTool extends AbstractRegionTableTool {
 		
 		ColumnViewerToolTipSupport.enableFor(viewer,ToolTip.NO_RECREATE);
 
-		TableViewerColumn var = new TableViewerColumn(viewer, SWT.LEFT, 0);
+		int count = 0;
+		TableViewerColumn var = new TableViewerColumn(viewer, SWT.LEFT, count);
+		var.getColumn().setText("");
+		var.getColumn().setWidth(22);
+		var.setLabelProvider(new MeasurementLabelProvider(this, LabelType.VISIBLE));
+		RegionEditingSupport regionEditor = new RegionEditingSupport(this,viewer, count);
+		var.setEditingSupport(regionEditor);
+
+		++count;
+		var = new TableViewerColumn(viewer, SWT.LEFT, count);
 		var.getColumn().setText("Name");
 		var.getColumn().setWidth(120);
 		var.setLabelProvider(new MeasurementLabelProvider(this, LabelType.ROINAME));
-		RegionEditingSupport regionEditor = new RegionEditingSupport(viewer, 0);
+		regionEditor = new RegionEditingSupport(this,viewer, count);
 		var.setEditingSupport(regionEditor);
 
-		var = new TableViewerColumn(viewer, SWT.CENTER, 1);
+		++count;
+		var = new TableViewerColumn(viewer, SWT.CENTER, count);
 		var.getColumn().setText("Start point x");
 		var.getColumn().setWidth(100);
 		var.setLabelProvider(new MeasurementLabelProvider(this, LabelType.STARTX));
-		regionEditor = new RegionEditingSupport(viewer, 1);
+		regionEditor = new RegionEditingSupport(this,viewer, count);
 		var.setEditingSupport(regionEditor);
 
-		var = new TableViewerColumn(viewer, SWT.LEFT, 2);
+		++count;
+		var = new TableViewerColumn(viewer, SWT.LEFT, count);
 		var.getColumn().setText("Start point y");
 		var.getColumn().setWidth(100);
 		var.setLabelProvider(new MeasurementLabelProvider(this, LabelType.STARTY));
-		regionEditor = new RegionEditingSupport(viewer, 2);
+		regionEditor = new RegionEditingSupport(this,viewer, count);
 		var.setEditingSupport(regionEditor);
 
-		var = new TableViewerColumn(viewer, SWT.LEFT, 3);
+		++count;
+		var = new TableViewerColumn(viewer, SWT.LEFT, count);
 		var.getColumn().setText("End Point x");
 		var.getColumn().setWidth(100);
 		var.setLabelProvider(new MeasurementLabelProvider(this, LabelType.ENDX));
-		regionEditor = new RegionEditingSupport(viewer, 3);
+		regionEditor = new RegionEditingSupport(this,viewer,count);
 		var.setEditingSupport(regionEditor);
 
-		var = new TableViewerColumn(viewer, SWT.LEFT, 4);
+		++count;
+		var = new TableViewerColumn(viewer, SWT.LEFT, count);
 		var.getColumn().setText("End point y");
 		var.getColumn().setWidth(100);
 		var.setLabelProvider(new MeasurementLabelProvider(this, LabelType.ENDY));
-		regionEditor = new RegionEditingSupport(viewer, 4);
+		regionEditor = new RegionEditingSupport(this,viewer, count);
 		var.setEditingSupport(regionEditor);
 
-		var = new TableViewerColumn(viewer, SWT.LEFT, 5);
+		++count;
+		var = new TableViewerColumn(viewer, SWT.LEFT, count);
 		var.getColumn().setText("Max Intensity");
 		var.getColumn().setWidth(100);
 		var.setLabelProvider(new MeasurementLabelProvider(this, LabelType.MAX));
-		regionEditor = new RegionEditingSupport(viewer, 5);
+		regionEditor = new RegionEditingSupport(this,viewer,count);
 		var.setEditingSupport(regionEditor);
 
-		var = new TableViewerColumn(viewer, SWT.LEFT, 6);
+		++count;
+		var = new TableViewerColumn(viewer, SWT.LEFT, count);
 		var.getColumn().setText("Sum");
 		var.getColumn().setWidth(100);
 		var.setLabelProvider(new MeasurementLabelProvider(this, LabelType.SUM));
-		regionEditor = new RegionEditingSupport(viewer, 6);
+		regionEditor = new RegionEditingSupport(this,viewer, count);
 		var.setEditingSupport(regionEditor);
 
-		var = new TableViewerColumn(viewer, SWT.LEFT, 7);
+		++count;
+		var = new TableViewerColumn(viewer, SWT.LEFT, count);
 		var.getColumn().setText("Active");
 		var.getColumn().setWidth(100);
 		var.setLabelProvider(new MeasurementLabelProvider(this, LabelType.ACTIVE));
-		regionEditor = new RegionEditingSupport(viewer, 7);
+		regionEditor = new RegionEditingSupport(this,viewer, count);
 		var.setEditingSupport(regionEditor);
 	}
-	
-	private final static Collection<RegionType> EDITABLE_REGIONS;
-	static {
-		Collection<RegionType> tmp = new HashSet<IRegion.RegionType>();
-		tmp.add(RegionType.BOX);
-		tmp.add(RegionType.LINE);
-		tmp.add(RegionType.GRID);
-		tmp.add(RegionType.XAXIS);
-		tmp.add(RegionType.XAXIS_LINE);
-		tmp.add(RegionType.YAXIS);
-		tmp.add(RegionType.YAXIS_LINE);
-		tmp.add(RegionType.PERIMETERBOX);
-		EDITABLE_REGIONS = Collections.unmodifiableCollection(tmp);
+		
+	@Override
+	protected boolean isRegionOk(IRegion iRegion) {
+		return iRegion.isUserRegion();
 	}
-	
+
 	protected void createActions() {
 		getSite().getActionBars().getToolBarManager().add(getReselectAction());
 		getSite().getActionBars().getToolBarManager().add(new Separator());
@@ -138,201 +145,4 @@ public class RegionEditTool extends AbstractRegionTableTool {
 	 * EditingSupport Class
 	 *
 	 */
-	private class RegionEditingSupport extends EditingSupport {
-
-		private int column;
-
-		public RegionEditingSupport(ColumnViewer viewer, int col) {
-			super(viewer);
-			this.column = col;
-		}
-		@Override
-		protected CellEditor getCellEditor(final Object element) {
-			CellEditor ed = null;
-			
-			if(column > 0 && column < 7){
-				IPreferenceStore store = Activator.getPlottingPreferenceStore();
-				DecimalFormat pointFormat = new DecimalFormat(store.getString(RegionEditorConstants.POINT_FORMAT));
-				
-				final FloatSpinnerCellEditor fse = new FloatSpinnerCellEditor((Composite)getViewer().getControl(), SWT.RIGHT);
-				fse.setFormat(pointFormat.getMaximumIntegerDigits(), pointFormat.getMaximumFractionDigits());
-				fse.setMaximum(Double.MAX_VALUE);
-				fse.setMinimum(-Double.MAX_VALUE);
-				fse.addSelectionListener(new SelectionAdapter() {
-					@Override
-					public void widgetSelected(SelectionEvent e) {
-						try {
-							setValue(element, fse.getValue(), false);
-						} catch (Exception e1) {
-							logger.debug("Error while setting table value");
-							e1.printStackTrace();
-						}
-					}
-				});
-				return fse;
-			} else if(column == 0){
-				ed = new TextCellEditor(((TableViewer)getViewer()).getTable(), SWT.RIGHT);
-				return ed;
-			} else if(column == 7){
-				ed = new CheckboxCellEditor(((TableViewer)getViewer()).getTable(), SWT.RIGHT);
-				return ed;
-			}
-			return null;
-		}
-
-		@Override
-		protected boolean canEdit(Object element) {
-			
-			if (!(element instanceof IRegion)) return false;
-			final IRegion region = (IRegion)element;
-			if (!EDITABLE_REGIONS.contains(region.getRegionType())) return false;
-			if (column == 5 || column == 6) return false;
-			else return true;
-		}
-
-		@Override
-		protected Object getValue(Object element) {
-			final IRegion region = (IRegion)element;
-			IROI roi = region.getROI();
-			ICoordinateSystem coords = region.getCoordinateSystem();
-			double[] startPoint = getAxisPoint(coords, roi.getPoint());
-			double[] endPoint = {0, 0};
-			if(roi instanceof RectangularROI){
-				endPoint = getAxisPoint(coords, ((RectangularROI)roi).getEndPoint());
-			} else if (roi instanceof LinearROI){
-				endPoint = getAxisPoint(coords, ((LinearROI)roi).getEndPoint());
-			}
-			switch (column){
-			case 0:
-				return region.getLabel();
-			case 1:
-				return startPoint[0];
-			case 2:
-				return startPoint[1];
-			case 3:
-				return endPoint[0];
-			case 4:
-				return endPoint[1];
-			case 5: //max intensity
-				return null;
-			case 6: //sum
-				return null;
-			case 7: //isPlot
-				return region.isActive();
-//				return roi.isPlot();
-			default:
-				return null;
-			}
-			
-		}
-
-		@Override
-		protected void setValue(Object element, Object value) {
-			try {
-				this.setValue(element, value, true);
-			} catch (Exception e) {
-				logger.debug("Error while setting table value");
-				e.printStackTrace();
-			}
-		}
-		
-		private void setValue(Object element, Object value, boolean tableRefresh) throws Exception {
-
-			final IRegion region = (IRegion) element;
-			IROI myRoi = region.getROI();
-			ICoordinateSystem coords = region.getCoordinateSystem();
-			double[] roiStartPoint = getAxisPoint(coords, myRoi.getPoint());
-			double[] roiEndPoint = {0, 0};
-			if(myRoi instanceof RectangularROI){
-				roiEndPoint = getAxisPoint(coords, ((RectangularROI)myRoi).getEndPoint());
-			} else if (myRoi instanceof LinearROI){
-				roiEndPoint = getAxisPoint(coords, ((LinearROI)myRoi).getEndPoint());
-			}
-			switch (column){
-			case 0:
-				// takes care of renaming the region (label and key value in hash table)
-				if(!region.getName().equals((String)value))
-					getPlottingSystem().renameRegion(region, (String)value);
-				break;
-			case 1:
-				if(myRoi instanceof LinearROI){
-					LinearROI lroi = (LinearROI)myRoi;
-					double[] endPoint = lroi.getEndPoint();
-					double[] startPoint = getImagePoint(coords, new double[]{(Double)value, roiStartPoint[1]});
-					lroi.setPoint(startPoint);
-					lroi.setPoint((Double)value, lroi.getPointY());
-					lroi.setEndPoint(endPoint);
-					myRoi = lroi;
-				} else if(myRoi instanceof RectangularROI){
-					RectangularROI rroi = (RectangularROI)myRoi;
-					double[] endPoint = rroi.getEndPoint();
-					double[] startPoint = getImagePoint(coords, new double[]{(Double)value, roiStartPoint[1]});
-					rroi.setPoint(startPoint);
-					rroi.setLengths(endPoint[0] - startPoint[0], rroi.getLengths()[1]); //set new endpoint
-					myRoi = rroi;
-				}
-				break;
-			case 2:
-				if(myRoi instanceof LinearROI){
-					LinearROI lroi = (LinearROI)myRoi;
-					double[] endPoint = lroi.getEndPoint();
-					double[] startPoint = getImagePoint(coords, new double[]{roiStartPoint[0], (Double)value});
-					lroi.setPoint(startPoint);
-					lroi.setEndPoint(endPoint);
-					myRoi = lroi;
-				} else if(myRoi instanceof RectangularROI){
-					RectangularROI rroi = (RectangularROI)myRoi;
-					double[] endPoint = rroi.getEndPoint();
-					double[] startPoint = getImagePoint(coords, new double[]{roiStartPoint[0], (Double)value});
-					rroi.setPoint(startPoint);
-					rroi.setLengths(rroi.getLengths()[0], endPoint[1] - startPoint[1]); //set new endpoint
-					myRoi = rroi;
-				}
-				break;
-			case 3:
-				if(myRoi instanceof LinearROI){
-					double[] endPoint = getImagePoint(coords, 
-							new double[]{(Double)value, roiEndPoint[1]});
-					((LinearROI)myRoi).setEndPoint(endPoint);
-				}
-				else if (myRoi instanceof RectangularROI){
-					double[] endPoint = getImagePoint(coords, 
-							new double[]{(Double)value, roiEndPoint[1]});
-					((RectangularROI)myRoi).setEndPoint(endPoint);
-				}
-				break;
-			case 4:
-				if(myRoi instanceof LinearROI){
-					double[] endPoint = getImagePoint(coords, 
-							new double[]{roiEndPoint[0], (Double)value});
-					((LinearROI)myRoi).setEndPoint(endPoint);
-				}
-				else if (myRoi instanceof RectangularROI){
-					double[] endPoint = getImagePoint(coords, 
-							new double[]{roiEndPoint[0], (Double)value});
-					((RectangularROI)myRoi).setEndPoint(endPoint);
-				}
-				break;
-			case 5: //intensity
-				break;
-			case 6: //sum
-				break;
-			case 7: //isPlot
-				myRoi.setPlot((Boolean)value);
-				region.setActive((Boolean)value);
-				break;
-			default:
-				break;
-			}
-
-			if (tableRefresh) {
-				getViewer().refresh();
-			}
-
-			roi = myRoi;
-			IRegion myregion = getPlottingSystem().getRegion(region.getName());
-			if(myregion!= null)
-				myregion.setROI(myRoi);
-		}
-	}
 }
