96e72d5754a6303c5dceafda2665d1d949cb4a98
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2013-11-12 11:12:39 +0000

    Set alpha channel of perimeter box selection to 0

--- org.dawnsci.plotting.draw2d/src/org/dawnsci/plotting/draw2d/swtxy/selection/PerimeterBoxSelection.java
+++ org.dawnsci.plotting.draw2d/src/org/dawnsci/plotting/draw2d/swtxy/selection/PerimeterBoxSelection.java
@@ -43,7 +43,7 @@ public class PerimeterBoxSelection extends BoxSelection {
 	PerimeterBoxSelection(String name, ICoordinateSystem coords) {
 		super(name, coords);
 		setRegionColor(IRegion.RegionType.PERIMETERBOX.getDefaultColor());	
-		setAlpha(80);
+		setAlpha(0);
 	}
 
 	@Override
