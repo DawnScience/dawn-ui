801cf85708130c160713260e67685a914a9de0a3
Peter Chang <Peter.Chang@diamond.ac.uk>
2013-11-28 11:49:52 +0000

    Added new methods

--- org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/fitting/NullFunction.java
+++ org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/fitting/NullFunction.java
@@ -5,139 +5,126 @@ import uk.ac.diamond.scisoft.analysis.dataset.IDataset;
 import uk.ac.diamond.scisoft.analysis.fitting.functions.IFunction;
 import uk.ac.diamond.scisoft.analysis.fitting.functions.IParameter;
 import uk.ac.diamond.scisoft.analysis.fitting.functions.IPeak;
+import uk.ac.diamond.scisoft.analysis.monitor.IMonitor;
 
 public class NullFunction implements IPeak {
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void setName(String newName) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public double val(double... values) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public IParameter getParameter(int index) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public IParameter[] getParameters() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public int getNoOfParameters() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int getNoOfFunctions() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public IFunction getFunction(int index) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public double getParameterValue(int index) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public double[] getParameterValues() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void setParameterValues(double... params) {
-		// TODO Auto-generated method stub
+	public void setParameter(int index, IParameter parameter) {
+	}
 
+	@Override
+	public void setParameterValues(double... params) {
 	}
 
 	@Override
 	public double partialDeriv(int Parameter, double... position) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public DoubleDataset makeDataset(IDataset... values) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public double residual(boolean allValues, IDataset data, IDataset... values) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public double getPosition() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public double getFWHM() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public double getArea() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public double getHeight() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void setDescription(String newDescription) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public String getParameterName(int index) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void setParameterName(String name, int index) {
-		// TODO Auto-generated method stub
-		
 	}
 
+	@Override
+	public void setMonitor(IMonitor monitor) {
+	}
+
+	@Override
+	public IMonitor getMonitor() {
+		return null;
+	}
 }
