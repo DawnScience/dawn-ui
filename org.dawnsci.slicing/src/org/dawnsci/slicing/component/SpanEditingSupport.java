fca8a029709c7206577bbd973007a24f5ff8feca
matthew.gerring <matthew.gerring@gmail.com>
2014-01-07 09:20:55 +0000

    Corrected max size by dividing by 4 after PC fixed a bug in my other code which then increased max by 16.

--- org.dawnsci.slicing/src/org/dawnsci/slicing/component/SpanEditingSupport.java
+++ org.dawnsci.slicing/src/org/dawnsci/slicing/component/SpanEditingSupport.java
@@ -32,7 +32,7 @@ public class SpanEditingSupport extends EditingSupport {
 			final DimsData data = (DimsData)element;
 			int dimension = data.getDimension();
 			ret.setMinimum(1);
-			final int max = LazyDataset.getMaxSliceLength(system.getData().getLazySet(), dimension);
+			final int max = (int)Math.round(LazyDataset.getMaxSliceLength(system.getData().getLazySet(), dimension)/4d);
 			ret.setMaximum(max);
 			ret.getSpinner().setToolTipText("The maximum value of a slice of dimension '"+(dimension+1)+"' is '"+max+"',\nbased on available memory.");
 			
