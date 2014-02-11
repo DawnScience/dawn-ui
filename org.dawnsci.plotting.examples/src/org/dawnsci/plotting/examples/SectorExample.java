cd893ab2f3b5e36f848b323e2d89a1a1e0d56120
matthew.gerring <matthew.gerring@gmail.com>
2014-02-11 10:16:50 +0000

    Updated exmaple to use regions now that they are available with few dependencies.

--- org.dawnsci.plotting.examples/src/org/dawnsci/plotting/examples/SectorExample.java
+++ org.dawnsci.plotting.examples/src/org/dawnsci/plotting/examples/SectorExample.java
@@ -6,6 +6,8 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 
+import uk.ac.diamond.scisoft.analysis.roi.SectorROI;
+
 /**
  * View which creates a sector selection region
  * and listens to that region moving.
@@ -27,11 +29,15 @@ public class SectorExample extends ImageExample {
 		try {
 			final IRegion sector = system.createRegion("Sector 1", RegionType.SECTOR);
 			sector.setRegionColor(Display.getDefault().getSystemColor(SWT.COLOR_RED));
+			sector.setROI(new SectorROI(1024, 1024, 0, 500, 0, Math.PI/3));
+			sector.setRegionColor(Display.getDefault().getSystemColor(SWT.COLOR_YELLOW));
+			system.addRegion(sector);
 			
-
-			// TODO Need way in API of creating regions
+			// NOTE there are clases (like ROIProfile.sector(...)) which can do maths based
+			// on where a user moves a sector.
 			
 			system.setTitle("Sector Example");
+			
 		} catch (Exception e) {
   		    e.printStackTrace(); // Or your favourite logging
 		}
--- org.dawnsci.plotting.examples/src/org/dawnsci/plotting/examples/SurfaceExample.java
+++ org.dawnsci.plotting.examples/src/org/dawnsci/plotting/examples/SurfaceExample.java
@@ -3,12 +3,15 @@ package org.dawnsci.plotting.examples;
 import java.io.File;
 
 import org.dawnsci.plotting.api.PlotType;
+import org.dawnsci.plotting.api.histogram.IPaletteService;
 import org.dawnsci.plotting.api.trace.ISurfaceTrace;
 import org.dawnsci.plotting.examples.util.BundleUtils;
+import org.eclipse.swt.graphics.PaletteData;
 import org.eclipse.swt.widgets.Composite;
 
 import uk.ac.diamond.scisoft.analysis.dataset.IDataset;
 import uk.ac.diamond.scisoft.analysis.monitor.IMonitor;
+import uk.ac.diamond.scisoft.analysis.roi.RectangularROI;
 
 public class SurfaceExample extends PlotExample {
 
@@ -23,11 +26,23 @@ public class SurfaceExample extends PlotExample {
 			// NOTE IMonitor is an alternative to IProgressMonitor which cannot be seen in the data layer.
 			
 			// We plot the image
+			system.setPlotType(PlotType.SURFACE); // Set to a 3D plot type.
+			
 			ISurfaceTrace surface = system.createSurfaceTrace("Example surface");
 			surface.setData(image, null);
-			//surface.setWindow(new SurfacePlotROI());
-			// TODO need ROIs in public interface...
+			// NOTE We are viewing a window of the data. The user can 
+			// open a tool from the toolbar to move around this window.
+			surface.setWindow(new RectangularROI(300,300,600,600,0));
+			
+			// Let's make it something colorful!
+			final IPaletteService pservice = (IPaletteService)Activator.getService(IPaletteService.class);
+			final PaletteData     pData    = pservice.getDirectPaletteData("NCD");
+			surface.setPaletteData(pData); 
+			surface.setMax(10);
+			surface.setMin(0);
+			
 			system.addTrace(surface);
+			
 			
 		} catch (Throwable ne) {
 			ne.printStackTrace(); // Or your favourite logging.
