722c2400bd53f0294070c2e9b00d154940920fe6
matthew.gerring <matthew.gerring@gmail.com>
2014-03-12 13:15:32 +0000

    Fix to http://jira.diamond.ac.uk/browse/DAWNSCI-852

--- org.dawnsci.slicing.tools/src/org/dawnsci/slicing/tools/plot/SurfaceSlicingTool.java
+++ org.dawnsci.slicing.tools/src/org/dawnsci/slicing/tools/plot/SurfaceSlicingTool.java
@@ -1,9 +1,13 @@
 package org.dawnsci.slicing.tools.plot;
 
 import org.dawnsci.plotting.api.PlotType;
+import org.dawnsci.plotting.api.tool.IToolPageSystem;
+import org.dawnsci.plotting.api.tool.IToolPage.ToolPageRole;
 import org.dawnsci.slicing.api.system.DimsDataList;
 import org.dawnsci.slicing.api.system.AxisType;
 import org.dawnsci.slicing.api.tool.AbstractSlicingTool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This is a simple type of slice tool based on the available plot
@@ -14,6 +18,8 @@ import org.dawnsci.slicing.api.tool.AbstractSlicingTool;
  *
  */
 public class SurfaceSlicingTool extends AbstractSlicingTool {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SurfaceSlicingTool.class);
 
 	@Override
 	public void militarize() {
@@ -24,6 +30,18 @@ public class SurfaceSlicingTool extends AbstractSlicingTool {
 		if (dimsDataList!=null) dimsDataList.setTwoAxesOnly(AxisType.X, AxisType.Y);   
 		getSlicingSystem().refresh();
 		getSlicingSystem().update(true);
+		
+		showWindowTool();
+		
+	}
+	protected void showWindowTool() {
+		try {
+			final IToolPageSystem system = (IToolPageSystem)getSlicingSystem().getPlottingSystem().getAdapter(IToolPageSystem.class);
+			system.setToolVisible("org.dawb.workbench.plotting.tools.windowTool", ToolPageRole.ROLE_3D, 
+					                      "org.dawb.workbench.plotting.views.toolPageView.3D");
+		} catch (Exception e1) {
+			logger.error("Cannot open window tool!", e1);
+		}
 	}
 
 	@Override
