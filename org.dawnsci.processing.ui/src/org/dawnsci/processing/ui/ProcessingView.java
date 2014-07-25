d0b2023819636547d0bdd7d7da48bc537661bb4d
matthew.gerring <matthew.gerring@gmail.com>
2014-07-25 16:27:20 +0100

    Placeholder for processing UI plugin.

---
+++ org.dawnsci.processing.ui/src/org/dawnsci/processing/ui/ProcessingView.java
@@ -0,0 +1,26 @@
+package org.dawnsci.processing.ui;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * A view for constructing and executing a processing pipeline.
+ * 
+ * @author fcp94556
+ *
+ */
+public class ProcessingView extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
