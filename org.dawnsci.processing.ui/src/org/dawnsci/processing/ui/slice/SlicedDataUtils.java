b4c88cfac027e41c2909d3435a3d544d980bb55e
Jacob Filik <jacob.filik@diamond.ac.uk>
2014-11-07 15:36:59 +0000

    Null vals in AxesMeta now fine, if for meta names but no shapes

--- org.dawnsci.processing.ui/src/org/dawnsci/processing/ui/slice/SlicedDataUtils.java
+++ org.dawnsci.processing.ui/src/org/dawnsci/processing/ui/slice/SlicedDataUtils.java
@@ -41,19 +41,8 @@ public class SlicedDataUtils {
 			}
 			
 			if (lazyAx != null) axMeta.setAxis(key-1, new ILazyDataset[] {lazyAx});
-//			else axMeta.setAxis(key-1, new ILazyDataset[1]);
+			else axMeta.setAxis(key-1, new ILazyDataset[1]);
 		}
-		
-//		ILazyDataset[] axes = axMeta.getAxes();
-//		boolean allNull = true;
-//		for (ILazyDataset ld : axes) {
-//			if (ld != null ) {
-//				allNull = false;
-//				break;
-//			}
-//		}
-//		
-//		if (allNull) return null;
 		
 		return axMeta;
 	}
