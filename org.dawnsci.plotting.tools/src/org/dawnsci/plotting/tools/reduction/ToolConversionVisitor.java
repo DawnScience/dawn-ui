083f0001a320bbca13160641c9b845468030158e
matthew.gerring <matthew.gerring@gmail.com>
2014-11-19 14:13:35 +0000

    Fix to axes being sometimes nD

--- org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/reduction/ToolConversionVisitor.java
+++ org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/reduction/ToolConversionVisitor.java
@@ -8,6 +8,7 @@
  */
 package org.dawnsci.plotting.tools.reduction;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ncsa.hdf.hdf5lib.exceptions.HDF5FunctionArgumentException;
@@ -18,6 +19,9 @@ import org.dawb.common.ui.plot.tools.IDataReductionToolPage;
 import org.dawb.common.ui.plot.tools.IDataReductionToolPage.DataReductionInfo;
 import org.dawb.common.ui.plot.tools.IDataReductionToolPage.DataReductionSlice;
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.api.dataset.ILazyDataset;
+import org.eclipse.dawnsci.analysis.api.metadata.AxesMetadata;
+import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 import org.eclipse.dawnsci.hdf5.HierarchicalDataFactory;
 import org.eclipse.dawnsci.hdf5.IHierarchicalDataFile;
 import org.eclipse.dawnsci.hdf5.Nexus;
@@ -62,7 +66,7 @@ class ToolConversionVisitor implements IConversionVisitor {
 		
 		String grp = createGroupIfRequired(context);
 		DataReductionSlice bean = new DataReductionSlice(output, grp, slice, object, context.getSelectedSlice(), context.getSelectedShape(), context.getMonitor());
-		bean.setAxes(nexusAxes);
+		bean.setAxes(getAxes(context, slice));
 		bean.setExpandedDatasetNames(getExpandedDatasets());
 		DataReductionInfo  info = tool.export(bean);
 		if (info.getStatus().isOK()) object = info.getUserData();
@@ -70,6 +74,61 @@ class ToolConversionVisitor implements IConversionVisitor {
 		if (context.getMonitor()!=null) context.getMonitor().worked(1);
 	}
 	
+	/**
+	 * Deals with getting correct axes for slice
+	 * @param context
+	 * @return
+	 * @throws Exception 
+	 */
+	private List<IDataset> getAxes(IConversionContext context, IDataset slice) throws Exception {
+		
+		if (nexusAxes==null) return null;
+
+		// If all the nexusAxes are 1D then we do what we used to
+		// NOTE Might be able to ignore this step one day
+		boolean all1D = true;
+		for (IDataset i : nexusAxes) {
+			if (i.getRank()!=1) all1D = false;
+		}
+		if (all1D) return nexusAxes; // No need to slice them.
+		
+		// TODO Might be able to use these meta-data things but not sure how to relate these to the slice
+		// We know the axis name but this information cannot be looked up in AxesMetadata
+		// therefore AxesMetadata is a bit useless in thin context? Might need to change it
+		final List<AxesMetadata> adata = slice.getMetadata(AxesMetadata.class);
+		
+		final List<IDataset> ret = new ArrayList<IDataset>(nexusAxes.size());
+		
+		// This sucks, sorry.
+		for (IDataset i : nexusAxes) {
+			if (i.getRank()>1) {
+				
+			    // FIXME Yuckiness warning: we have the full path to the 
+				// axis but the AxesMetadata has sets named badly (arguable...)
+				try {
+					// We need to slice em 
+					String name = slice.getName();
+					name = name.substring(name.lastIndexOf("/")+1);
+					
+					// Search for axes by name.
+                    AXIS_LOOP: for (AxesMetadata amd : adata) {
+						for (ILazyDataset axis : amd.getAxes()) {
+							if (axis.getName().contains("'"+name+"'")) {
+								i = axis.getSlice();
+								break AXIS_LOOP;
+							}
+						}
+					}
+					
+				} catch (Exception ne) {
+					i = ((Dataset)i).mean(0);
+				}
+			}
+			ret.add(i);
+		}
+		return ret;
+	}
+
 	private String  currentH5Path;     
 
 	private String createGroupIfRequired(IConversionContext context) throws Exception {
