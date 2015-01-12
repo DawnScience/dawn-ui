a9f4068b52efc375bc08eee0437406d1fb17dfff
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-01-12 18:04:39 +0000

    Adds controls for background fitting. Changes general layout of UI (2 columns).

--- org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/fitting/PeakPrepopulateTool.java
+++ org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/fitting/PeakPrepopulateTool.java
@@ -18,11 +18,15 @@ import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
@@ -32,12 +36,20 @@ import uk.ac.diamond.scisoft.analysis.fitting.functions.Add;
 
 public class PeakPrepopulateTool extends Dialog {
 	
-	private Combo peakTypeCombo;
+	//Tool common UI elements
 	private Composite dialogContainer;
+	
+	//Peak finding UI elements
+	private Combo peakTypeCombo;
 	private Text nrPeaksTxtBox;
+	private Button findPeaksButton;
+	
+	//Background finding UI elements
+	private Combo bkgTypeCombo;
+	private Button fitInitialBkgButton;
+	
 	private Integer nrPeaks = null;
 	private Dataset[] roiLimits;
-	
 	private Map<String, Class <? extends APeak>> peakFnMap = new TreeMap<String, Class <? extends APeak>>();
 	private String[] availPeakTypes;
 	
@@ -65,20 +77,24 @@ public class PeakPrepopulateTool extends Dialog {
 		//Create/get the base containers and set up the grid layout
 		Composite windowArea = (Composite) super.createDialogArea(parent);
 		dialogContainer = new Composite(windowArea, SWT.NONE);
-		GridLayout toolGridLayout = new GridLayout(2, false);
 		dialogContainer.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		dialogContainer.setLayout(toolGridLayout);
+		dialogContainer.setLayout(new GridLayout(2, false));
+		
+		//Create a peak finding area on the left of the window
+		Group peakFindingSpace = new Group(dialogContainer,SWT.NONE);
+		peakFindingSpace.setText("Find Peaks");
+		peakFindingSpace.setLayoutData(new GridData(SWT.FILL, SWT.FILL,true, true));
+		peakFindingSpace.setLayout(new GridLayout(2, false));
+		
+//		Composite peakFindContainer = new Composite(peakFindingSpace, SWT.NONE);
+//		peakFindContainer.setLayoutData(new GridData(SWT.FILL,SWT.FILL, true, true));
+//		peakFindContainer.setLayout(new GridLayout(2, false));
 		
 		//Number of peaks text field
-		GridData nrPeaksGridData = new GridData();
-		nrPeaksGridData.grabExcessHorizontalSpace = true;
-		nrPeaksGridData.horizontalAlignment = GridData.FILL;
-		
-		Label nrPeaksTxtLbl = new Label(dialogContainer, SWT.None);
+		Label nrPeaksTxtLbl = new Label(peakFindingSpace, SWT.None);
 		nrPeaksTxtLbl.setText("Number of Peaks:");
-		
-		nrPeaksTxtBox = new Text(dialogContainer, SWT.BORDER); 
-		nrPeaksTxtBox.setLayoutData(nrPeaksGridData);
+		nrPeaksTxtBox = new Text(peakFindingSpace, SWT.BORDER); 
+		nrPeaksTxtBox.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
 		//This limits the input for the text box to +ve integers
 		final IntegerDecorator nrPeaksIDec = new IntegerDecorator(nrPeaksTxtBox);
 		nrPeaksIDec.setMinimum(0);
@@ -102,17 +118,59 @@ public class PeakPrepopulateTool extends Dialog {
 		});
 		
 		//Profile type combo box
-		GridData peakTypeGridData = new GridData();
-		peakTypeGridData.grabExcessHorizontalSpace = true;
-		peakTypeGridData.horizontalAlignment = GridData.FILL;
-		
-		Label peakTypeCmbLbl = new Label(dialogContainer, SWT.None);
+		Label peakTypeCmbLbl = new Label(peakFindingSpace, SWT.None);
 		peakTypeCmbLbl.setText("Peak Function Type:");
-		
-		peakTypeCombo = new Combo(dialogContainer, SWT.READ_ONLY);
+		peakTypeCombo = new Combo(peakFindingSpace, SWT.READ_ONLY);
 		setAvailPeakFunctions();
 		setDefaultPeakFunction();
-		peakTypeCombo.setLayoutData(peakTypeGridData);
+		peakTypeCombo.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
+		
+		//Button to call initial peak finding
+		findPeaksButton = new Button(peakFindingSpace, SWT.PUSH);
+		GridData findPeaksButtonLayout = new GridData(SWT.CENTER, SWT.CENTER, true, true);
+		findPeaksButtonLayout.horizontalSpan = 2;
+		findPeaksButton.setLayoutData(findPeaksButtonLayout);
+		findPeaksButton.setText("Find Peaks");
+		findPeaksButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				findInitialPeaks();
+			}
+		});
+		
+		//Create a background finding space on the right of the window
+		Group bkgFindingSpace = new Group(dialogContainer,SWT.NONE);
+		bkgFindingSpace.setText("Fit Background");
+		GridLayout bkgGridLayout = new GridLayout(2, false);
+		bkgFindingSpace.setLayoutData(new GridData(SWT.FILL, SWT.FILL,true, true));
+		bkgFindingSpace.setLayout(bkgGridLayout);
+		
+		//Background type combo box
+		Composite bkgLabelCombo = new Composite(bkgFindingSpace, SWT.NONE);
+		bkgLabelCombo.setLayoutData(new GridData(SWT.FILL,SWT.FILL, true, true));
+		bkgLabelCombo.setLayout(new GridLayout(1, false));
+		Label bkgTypeCmbLbl = new Label(bkgLabelCombo, SWT.NONE);
+		bkgTypeCmbLbl.setText("Background Function Type:");
+		bkgTypeCombo = new Combo(bkgLabelCombo, SWT.READ_ONLY);
+		//TODO Define options here...
+		bkgTypeCombo.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
+		
+		//Background function parameters
+		Label bkgFnParamsPlaceHolder = new Label(bkgFindingSpace, SWT.NONE);
+		bkgFnParamsPlaceHolder.setText("Function parameters go here");
+		
+		//Button to call background fitting
+		fitInitialBkgButton = new Button(bkgFindingSpace, SWT.PUSH);
+		GridData fitInitialBkgButtonGridData = new GridData(SWT.CENTER, SWT.CENTER, true, true);
+		fitInitialBkgButtonGridData.horizontalSpan = 2;
+		fitInitialBkgButton.setLayoutData(fitInitialBkgButtonGridData);
+		fitInitialBkgButton.setText("Fit Background");
+		fitInitialBkgButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("Fitting background...");
+			}
+		});
 		
 		return windowArea;
 	}
@@ -120,7 +178,6 @@ public class PeakPrepopulateTool extends Dialog {
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
 		//Create Close & Find Peaks buttons.
-		createButton(parent, IDialogConstants.PROCEED_ID, "Find Peaks", true);
 		createButton(parent, IDialogConstants.CLOSE_ID, IDialogConstants.CLOSE_LABEL, false);
 	}
 	
