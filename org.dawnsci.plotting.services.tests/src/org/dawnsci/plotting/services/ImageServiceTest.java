6a5f144975c537d75377a94a49a3da4ced99175f
Sabina <sabinaroze@gmail.com>
2015-02-16 11:55:34 +0000

    Tests for log values

--- org.dawnsci.plotting.services.tests/src/org/dawnsci/plotting/services/ImageServiceTest.java
+++ org.dawnsci.plotting.services.tests/src/org/dawnsci/plotting/services/ImageServiceTest.java
@@ -3,24 +3,28 @@ package org.dawnsci.plotting.services;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.commons.lang.ArrayUtils;
 import org.eclipse.dawnsci.analysis.dataset.impl.AbstractDataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 import org.eclipse.dawnsci.plotting.api.histogram.ImageServiceBean;
 import org.junit.Test;
 
 public class ImageServiceTest {
+	
+	final static double EPSILON = 0.01;
 
 	@Test
 	public void testLogOffset() {
 
+		// Test dataset of 10 doubles [0.0,..9.0]
 		ImageServiceBean imageServiceBean = new ImageServiceBean();
 		Dataset image = AbstractDataset.arange(10, AbstractDataset.FLOAT64);
 		imageServiceBean.setImage(image);
 		imageServiceBean.setLogColorScale(true);
+		
+		// Offset should be dataset min value -1
 		assertEquals(-1, imageServiceBean.getLogOffset(), 0.0);
 
 	}
@@ -28,73 +32,47 @@ public class ImageServiceTest {
 	@Test
 	public void testPlot() {
 		
-		ImageServiceBean imageServiceBean = new ImageServiceBean();
-		Dataset image = AbstractDataset.arange(10, AbstractDataset.FLOAT64);
-		imageServiceBean.setImage(image);
+		// Test dataset of 4 doubles [0.0,..3.0]
+		Dataset image = AbstractDataset.arange(4, AbstractDataset.FLOAT64);
 		ImageService imageService = new ImageService();
+				
+		// The expected results, to check against
+		double[] expectedResultNoLogging = {0.0, 1.0, 2.0, 3.0};
+		double[] expectedResultWithLogging = {Math.log10(1.0), Math.log10(2.0), Math.log10(3.0), Math.log10(4.0)};
 		
-		double epsilon = 0.01;
+		Map<Boolean, double[]> expectedResults = new HashMap<Boolean, double[]>();
+		expectedResults.put(false, expectedResultNoLogging);
+		expectedResults.put(true, expectedResultWithLogging);		
 		
-		
-		double expectedResultNoLogging[] = {0.0,
-				1.0,
-				2.0,
-				3.0,
-				4.0,
-				5.0,
-				6.0,
-				7.0,
-				8.0,
-				9.0
-		};
-		
-		double expectedResultWithLogging[] = {Math.log10(1.0), 
-				Math.log10(2.0),
-				Math.log10(3.0),
-				Math.log10(4.0),
-				Math.log10(5.0),
-				Math.log10(6.0),
-				Math.log10(7.0),
-				Math.log10(8.0),
-				Math.log10(9.0),
-				Math.log10(10.0),
-		};
-
-		
-		// Off by default
+		// Checking the initial state - off by default
+		ImageServiceBean imageServiceBean = new ImageServiceBean();
+		imageServiceBean.setImage(image);
 		double[] imageVals = (double[])image.getBuffer();
-		assertArrayEquals(expectedResultNoLogging, imageVals, epsilon);
+		assertArrayEquals(expectedResultNoLogging, imageVals, EPSILON);
 		
 		Dataset initialResult = imageService.getImageLoggedData(imageServiceBean);
 		double actualInitialResult[] = (double[])initialResult.getBuffer();
-		assertArrayEquals(expectedResultNoLogging, actualInitialResult, epsilon);
-
+		assertArrayEquals(expectedResultNoLogging, actualInitialResult, EPSILON);
 		
-		// Check on
-		ImageServiceBean imageServiceBean1 = new ImageServiceBean();
-		imageServiceBean1.setImage(image);
-		imageServiceBean1.setLogColorScale(true);
-		double[] imageVals1 = (double[])image.getBuffer();
-		assertArrayEquals(expectedResultNoLogging, imageVals1, epsilon);
-		assertEquals(-1, imageServiceBean1.getLogOffset(), 0.0);
-		Dataset resultLoggingOn = imageService.getImageLoggedData(imageServiceBean1);
-		double actualResultWithLogging[] = (double[])resultLoggingOn.getBuffer();
-		assertArrayEquals(expectedResultWithLogging, actualResultWithLogging, epsilon);
-		
-		
-		// Check off
-		ImageServiceBean imageServiceBean2 = new ImageServiceBean();
-		imageServiceBean2.setImage(image);
-		imageServiceBean2.setLogColorScale(false);
-		
-		double[] imageVals2 = (double[])image.getBuffer();
-		assertArrayEquals(expectedResultNoLogging, imageVals2, epsilon);
-		
-		assertEquals(0.0, imageServiceBean2.getLogOffset(), 0.0);
-		Dataset resultLoggingOff = imageService.getImageLoggedData(imageServiceBean2);
-		double actualResultNoLogging[] = (double[])resultLoggingOff.getBuffer();
-		assertArrayEquals(expectedResultNoLogging, actualResultNoLogging, epsilon);
-	
-
+		// Loop for toggling on/off the logging
+		for (int i = 0; i < 10; i++){
+			
+			// We need a new bean per iteration
+			ImageServiceBean imageServiceBean1 = new ImageServiceBean();
+			imageServiceBean1.setImage(image);
+			
+			// Toggle logging on/off
+			boolean toggle = i%2==0;
+			imageServiceBean1.setImage(image);
+			imageServiceBean1.setLogColorScale(toggle);
+			
+			// We expect the offset to be -1 if logging on, else 0
+			assertEquals(toggle? -1 : 0, imageServiceBean1.getLogOffset(), 0.0);
+			Dataset resultD = imageService.getImageLoggedData(imageServiceBean1);
+			double result[] = (double[])resultD.getBuffer();
+			
+			// Check the result against our expected result array
+			assertArrayEquals(expectedResults.get(toggle), result, EPSILON);			
+		}
 	}
 }
