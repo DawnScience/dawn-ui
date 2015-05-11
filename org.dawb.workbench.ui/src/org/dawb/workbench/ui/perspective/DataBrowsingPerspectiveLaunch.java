2071b6f4d75b08bce9411edb75a9d084c1d3aa58
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-05-11 15:54:31 +0100

    Add tracking to processing, spectrum and data browsing with label of corresponding perspectives

--- org.dawb.workbench.ui/src/org/dawb/workbench/ui/perspective/DataBrowsingPerspectiveLaunch.java
+++ org.dawb.workbench.ui/src/org/dawb/workbench/ui/perspective/DataBrowsingPerspectiveLaunch.java
@@ -12,6 +12,8 @@ import org.dawb.workbench.ui.EventTrackerServiceLoader;
 import org.eclipse.dawnsci.analysis.api.EventTracker;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IPerspectiveDescriptor;
+import org.eclipse.ui.IPerspectiveRegistry;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PlatformUI;
@@ -29,10 +31,16 @@ public class DataBrowsingPerspectiveLaunch implements IWorkbenchWindowActionDele
 	public void run(IAction action) {
 		try {
 			PlatformUI.getWorkbench().showPerspective(DataBrowsingPerspective.ID,PlatformUI.getWorkbench().getActiveWorkbenchWindow());
-			
+
+			// track perspective launch with perspective name
+			IPerspectiveRegistry reg = PlatformUI.getWorkbench().getPerspectiveRegistry();
+			IPerspectiveDescriptor per = reg.findPerspectiveWithId(DataBrowsingPerspective.ID);
+			String perspectiveName = "NA";
+			if (per!= null)
+				perspectiveName = per.getLabel();
 			EventTracker tracker = EventTrackerServiceLoader.getService();
 			if (tracker != null)
-				tracker.track("Data_Browsing_perspective_launch");
+				tracker.track(perspectiveName);
 		} catch (WorkbenchException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
--- org.dawnsci.spectrum.ui/src/org/dawnsci/spectrum/ui/SpectrumPerspectiveLaunch.java
+++ org.dawnsci.spectrum.ui/src/org/dawnsci/spectrum/ui/SpectrumPerspectiveLaunch.java
@@ -8,12 +8,17 @@
  */
 package org.dawnsci.spectrum.ui;
 
+import org.eclipse.dawnsci.analysis.api.EventTracker;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IPerspectiveDescriptor;
+import org.eclipse.ui.IPerspectiveRegistry;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.WorkbenchException;
+
+import uk.ac.diamond.scisoft.analysis.rcp.EventTrackerServiceLoader;
 
 public class SpectrumPerspectiveLaunch implements
 		IWorkbenchWindowActionDelegate {
@@ -22,7 +27,19 @@ public class SpectrumPerspectiveLaunch implements
 	public void run(IAction action) {
 		try {
 			PlatformUI.getWorkbench().showPerspective(SpectrumPerspective.ID,PlatformUI.getWorkbench().getActiveWorkbenchWindow());
+
+			// track perspective launch with perspective name
+			IPerspectiveRegistry reg = PlatformUI.getWorkbench().getPerspectiveRegistry();
+			IPerspectiveDescriptor per = reg.findPerspectiveWithId(SpectrumPerspective.ID);
+			String perspectiveName = "NA";
+			if (per!= null)
+				perspectiveName = per.getLabel();
+			EventTracker tracker = EventTrackerServiceLoader.getService();
+			if (tracker != null)
+				tracker.track(perspectiveName);
 		} catch (WorkbenchException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
