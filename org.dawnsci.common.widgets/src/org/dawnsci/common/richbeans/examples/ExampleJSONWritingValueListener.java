a834cb133587b59f2efae32c06a77e43cd04e291
Matthew Gerring <matthew.gerring@diamond.ac.uk>
2015-06-30 13:43:01 +0100

    Added example with more beans.

--- org.dawnsci.common.widgets/src/org/dawnsci/common/richbeans/examples/ExampleJSONWritingValueListener.java
+++ org.dawnsci.common.widgets/src/org/dawnsci/common/richbeans/examples/ExampleJSONWritingValueListener.java
@@ -1,18 +1,25 @@
 package org.dawnsci.common.richbeans.examples;
 
+import java.lang.reflect.Method;
+
 import org.dawnsci.common.richbeans.beans.BeanController;
 import org.dawnsci.common.richbeans.event.ValueAdapter;
 import org.dawnsci.common.richbeans.event.ValueEvent;
-import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Control;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ExampleJSONWritingValueListener extends ValueAdapter {
 
 	private BeanController controller;
-	private Label value;
+	private Control value;
+	private int textLimit = -1;
 
-	public ExampleJSONWritingValueListener(BeanController controller, Label value) {
+	/**
+	 * @param controller
+	 * @param value A control with a setText(...) method.
+	 */
+	public ExampleJSONWritingValueListener(BeanController controller, Control value) {
 
 		// The name should be unique and is only hard-coded here for simplicity - see the javadoc for ValueAdapter.
 		this.name = "Example listener";
@@ -32,11 +39,28 @@ public class ExampleJSONWritingValueListener extends ValueAdapter {
 			// whatever at this stage.
 			ObjectMapper mapper = new ObjectMapper();
 			String json = mapper.writeValueAsString(controller.getBean());
-			value.setText(json);
-			value.getParent().layout();
+			
+			if (textLimit>0) {
+				json = json.substring(0, textLimit)+"...";
+			}
+			
+			// The control must have a setText method
+			Method m = value.getClass().getMethod("setText", String.class);
+			m.invoke(value, json);
+			
+			value.getParent().layout(new Control[]{value});
+			
 		} catch (Exception e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+	}
+
+	public int getTextLimit() {
+		return textLimit;
+	}
+
+	public void setTextLimit(int textLimit) {
+		this.textLimit = textLimit;
 	}
 }
