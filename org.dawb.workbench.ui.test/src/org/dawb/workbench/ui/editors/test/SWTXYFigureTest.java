20d1766f4d527ba19df23e23e3aae378074536e0
Matthew Gerring <matthew.gerring@diamond.ac.uk>
2015-11-19 10:52:35 +0000

    Added test

---
+++ org.dawb.workbench.ui.test/src/org/dawb/workbench/ui/editors/test/SWTXYFigureTest.java
@@ -0,0 +1,16 @@
+package org.dawb.workbench.ui.editors.test;
+
+import org.junit.Test;
+
+public class SWTXYFigureTest {
+	
+	@Test
+	public void testFigure() throws Exception {
+		
+		// Create a Display and Shell
+		
+		// Create Figure Canvas
+		
+		// Make a plotting system on it
+	}
+}
