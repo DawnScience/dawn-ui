5ac5fefb98593883d133168e92c3c71dea636582
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-11-20 13:13:07 +0000

    Correctly set layout for IFigure container

--- org.dawb.workbench.ui.test/src/org/dawb/workbench/ui/editors/test/SWTXYFigureTest.java
+++ org.dawb.workbench.ui.test/src/org/dawb/workbench/ui/editors/test/SWTXYFigureTest.java
@@ -1,22 +1,20 @@
 package org.dawb.workbench.ui.editors.test;
 
-import java.util.Collection;
-
+import org.dawb.common.ui.ServiceLoader;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.api.io.IDataHolder;
+import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
 import org.eclipse.dawnsci.plotting.api.IPlottingSystem;
 import org.eclipse.dawnsci.plotting.api.PlotType;
 import org.eclipse.dawnsci.plotting.api.PlottingFactory;
-import org.eclipse.dawnsci.plotting.api.trace.IImageTrace;
-import org.eclipse.dawnsci.plotting.api.trace.ITrace;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.GridData;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.LightweightSystem;
-import org.eclipse.draw2d.XYLayout;
 import org.eclipse.richbeans.widgets.util.SWTUtils;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -31,14 +29,15 @@ public class SWTXYFigureTest {
 		// Create a Display and Shell
 		Display display = new Display();
 		Shell shell = new Shell(display);
-		shell.setLayout(new GridLayout(1, false));
+		shell.setLayout(new FillLayout());
 		shell.setText("Test draw2d figure");
 		
 		// Create Figure Canvas
 		Figure container = new Figure();
 		container.setFont(shell.getFont());
-		XYLayout layout = new XYLayout();
-		container.setLayoutManager(layout);
+		org.eclipse.draw2d.GridLayout grid = new org.eclipse.draw2d.GridLayout();
+		grid.setConstraint(container, new GridData(SWT.FILL, SWT.FILL, true, true));
+		container.setLayoutManager(grid);
 		// Create a canvas to display the root figure
 		Canvas canvas = new Canvas(shell, SWT.DOUBLE_BUFFERED);
 		canvas.setBackground(ColorConstants.white);
@@ -53,17 +52,13 @@ public class SWTXYFigureTest {
 		path = path.substring("reference:file:".length());
 		if (path.startsWith("/C:"))
 		path = path.substring(1);
+		IDataHolder holder = ServiceLoader.getLoaderService().getData(path, new IMonitor.Stub());
 
 		IPlottingSystem<IFigure> system = PlottingFactory.createPlottingSystem();
 		system.createPlotPart(root, "Figure Plot", null, PlotType.XY, null);
 
 		// plot an image
-		final Collection<ITrace> traces = system.getTraces(IImageTrace.class);
-		IImageTrace imt = (IImageTrace) traces.iterator().next();
-		IDataset data = imt.getData();
-		System.out.println("Setting image data...");
-		system.clear();
-		imt = (IImageTrace) system.createPlot2D(data, null, null);
+		system.createPlot2D(holder.getDataset(0), null, null);
 		
 		SWTUtils.showCenteredShell(shell);
 		
