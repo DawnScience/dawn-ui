a4e577d578f4dd8e0f3abe97b3713256246a3858
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-11-26 18:01:39 +0000

    Add new colour maps

---
+++ org.dawnsci.plotting.histogram/src/org/dawnsci/plotting/histogram/functions/classes/PlasmaRedTransferFunction.java
@@ -0,0 +1,19 @@
+package org.dawnsci.plotting.histogram.functions.classes;
+
+import org.dawnsci.plotting.histogram.data.HistogramData;
+
+public class PlasmaRedTransferFunction extends AbstractTransferFunction {
+
+	@Override
+	public double getPoint(double value) {
+		double div = Double.valueOf(1.0) / 255;
+		double val = value / div;
+		double rounded = Math.round(val);
+		for (int i = 0; i < 256; i ++) {
+			if (i == rounded)
+				return HistogramData.PLASMA[i][0];
+		}
+		return 0;
+	}
+
+}
