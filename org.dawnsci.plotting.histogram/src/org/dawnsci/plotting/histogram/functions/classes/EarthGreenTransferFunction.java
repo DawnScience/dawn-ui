f4b77bebe257476abbeeff96a362192792ed9cf6
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-11-30 16:12:11 +0000

    Add Earth and CRMap colour maps

---
+++ org.dawnsci.plotting.histogram/src/org/dawnsci/plotting/histogram/functions/classes/EarthGreenTransferFunction.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2015 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.dawnsci.plotting.histogram.functions.classes;
+
+import org.dawnsci.plotting.histogram.data.HistogramData;
+
+public class EarthGreenTransferFunction extends AbstractTransferFunction {
+
+	@Override
+	public double getPoint(double value) {
+		for (int i = 0; i < HistogramData.EARTH_GREEN.length; i++) {
+			if (i > 0 && value <= HistogramData.EARTH_GREEN[i][0]) {
+				return HistogramData.interpolatedY(
+						new double[] { HistogramData.EARTH_GREEN[i - 1][0], HistogramData.EARTH_GREEN[i - 1][1] },
+						new double[] { HistogramData.EARTH_GREEN[i][0], HistogramData.EARTH_GREEN[i][1] }, value);
+			}
+		}
+		return 0;
+	}
+}
