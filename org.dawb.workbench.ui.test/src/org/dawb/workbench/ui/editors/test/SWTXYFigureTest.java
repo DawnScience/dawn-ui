f99fd0303dc089f4aef9bb02e2494da22b1dd949
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-12-08 13:58:38 +0000

    Update test with createGrayScaleMPGJDataset method

--- org.dawb.workbench.ui.test/src/org/dawb/workbench/ui/editors/test/SWTXYFigureTest.java
+++ org.dawb.workbench.ui.test/src/org/dawb/workbench/ui/editors/test/SWTXYFigureTest.java
@@ -1,15 +1,19 @@
 package org.dawb.workbench.ui.editors.test;
 
-import org.dawb.common.ui.ServiceLoader;
+import java.net.URL;
+
+import org.dawb.common.services.ServiceManager;
 import org.dawb.common.ui.widgets.ActionBarWrapper;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.dawnsci.analysis.api.io.IDataHolder;
-import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
+import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.api.dataset.IRemoteDataset;
+import org.eclipse.dawnsci.analysis.api.io.IRemoteDatasetService;
 import org.eclipse.dawnsci.plotting.api.EmptyWorkbenchPart;
 import org.eclipse.dawnsci.plotting.api.IPlottingSystem;
 import org.eclipse.dawnsci.plotting.api.PlotType;
 import org.eclipse.dawnsci.plotting.api.PlottingFactory;
 import org.eclipse.dawnsci.plotting.api.tool.IToolPageSystem;
+import org.eclipse.dawnsci.plotting.api.trace.IImageTrace;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.Figure;
 import org.eclipse.draw2d.IFigure;
@@ -55,6 +59,9 @@ public class SWTXYFigureTest {
 		//create the plotting system
 		IPlottingSystem<IFigure> system = PlottingFactory.createPlottingSystem();
 		system.createPlotPart(root, "Figure Plot", wrapper, PlotType.XY, (IWorkbenchPart)new EmptyWorkbenchPart<IFigure>(system));
+		// Add a composite to display tools on
+		Composite toolComp = new Composite(shell, SWT.NONE);
+		((IToolPageSystem)system.getAdapter(IToolPageSystem.class)).setToolComposite(toolComp);
 
 		// load the data
 		final Bundle bun  = Platform.getBundle("org.dawb.workbench.ui.test");
@@ -62,15 +69,25 @@ public class SWTXYFigureTest {
 		path = path.substring("reference:file:".length());
 		if (path.startsWith("/C:"))
 		path = path.substring(1);
-		IDataHolder holder = ServiceLoader.getLoaderService().getData(path, new IMonitor.Stub());
+		// load data stream
+		IRemoteDatasetService remote = (IRemoteDatasetService) ServiceManager.getService(IRemoteDatasetService.class);
+		IRemoteDataset data = remote.createGrayScaleMJPGDataset(new URL("http://bl11i-di-serv-01:8082/ALCAM2.MJPG.mjpg"), 100, 10);
+		data.connect();
+
+		// set plot auto scale off
+		system.setRescale(false);
 
 		// plot an image
-		system.createPlot2D(holder.getDataset(0), null, null);
+		try {
+			system.updatePlot2D((IDataset) data, null, null);
+			// set palette
+			IImageTrace image = (IImageTrace)system.getTraces().iterator().next();
+			image.setPalette("Nipy Spectral");
 
-		// Add a composite to display tools on
-		Composite toolComp = new Composite(shell, SWT.NONE);
-		((IToolPageSystem)system.getAdapter(IToolPageSystem.class)).setToolComposite(toolComp);
-
-		SWTUtils.showCenteredShell(shell);
+			SWTUtils.showCenteredShell(shell);
+		} finally {
+			data.disconnect();
+		}
+		
 	}
 }
