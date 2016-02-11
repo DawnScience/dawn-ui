c68d00f40678ae2c186688ca436b592bbe9df154
Peter Chang <peter.chang@diamond.ac.uk>
2016-02-11 15:05:04 +0000

    Add unit tests to check what happens when bean or its JSON representation changes

--- uk.ac.diamond.scisoft.applications/test/uk/ac/diamond/scisoft/applications/MillerSpaceMapperBeanTest.java
+++ uk.ac.diamond.scisoft.applications/test/uk/ac/diamond/scisoft/applications/MillerSpaceMapperBeanTest.java
@@ -41,7 +41,6 @@ public class MillerSpaceMapperBeanTest {
 		}
 	}
 
-
 	@Test
 	public void createTestJSON() {
 		MillerSpaceMapperBean orig = MillerSpaceMapper.createI16MapperBean();
@@ -63,4 +62,53 @@ public class MillerSpaceMapperBeanTest {
 			e1.printStackTrace();
 			Assert.fail("Could not write file");
 		}
-	}}
+	}
+
+	@Test
+	public void loadIncompleteJSON() {
+		MillerSpaceMapperBean orig = MillerSpaceMapper.createI16MapperBean();
+		orig.setInputs("/scratch/images/i16/562926.nxs", "/scratch/images/i16/562927.nxs", "/scratch/images/i16/562928.nxs");
+		orig.setOutput("/scratch/tmp/562926.h5");
+		orig.setSplitterName("inverse");
+		orig.setSplitterParameter(0.5);
+		orig.setScaleFactor(2);
+		orig.setReduceToNonZero(true);
+		orig.setMillerStep(0.002);
+
+		ObjectMapper mapper = new ObjectMapper();
+		String testName = "test-files/i16-short.json";
+		File f = null;
+		try {
+			f = new File(testName);
+			MillerSpaceMapperBean copy = mapper.readValue(f, MillerSpaceMapperBean.class);
+			Assert.assertEquals(orig, copy);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			Assert.fail("Could not read file");
+		}
+	}
+
+	@Test
+	public void loadExtraItemsJSON() {
+		MillerSpaceMapperBean orig = MillerSpaceMapper.createI16MapperBean();
+		orig.setInputs("/scratch/images/i16/562926.nxs", "/scratch/images/i16/562927.nxs", "/scratch/images/i16/562928.nxs");
+		orig.setOutput("/scratch/tmp/562926.h5");
+		orig.setSplitterName("inverse");
+		orig.setSplitterParameter(0.5);
+		orig.setScaleFactor(2);
+		orig.setReduceToNonZero(true);
+		orig.setMillerStep(0.002);
+
+		ObjectMapper mapper = new ObjectMapper();
+		String testName = "test-files/i16-extra.json";
+		File f = null;
+		try {
+			f = new File(testName);
+			mapper.readValue(f, MillerSpaceMapperBean.class);
+			Assert.fail("Should not have been able to read file");
+		} catch (IOException e1) {
+			// should throw exception
+		}
+	}
+}
+
