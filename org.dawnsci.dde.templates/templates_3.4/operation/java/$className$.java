7bc8bbcf582896be53fdf2ddf59876aa6d3981c0
Jacob Filik <jacob.filik@diamond.ac.uk>
2016-03-14 11:24:29 +0000

    Improve Operation Template

--- org.dawnsci.dde.templates/templates_3.4/operation/java/$className$.java
+++ org.dawnsci.dde.templates/templates_3.4/operation/java/$className$.java
@@ -1,15 +1,19 @@
 package $packageName$;
 
+import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
+import org.eclipse.dawnsci.analysis.api.processing.OperationException;
+import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+import uk.ac.diamond.scisoft.analysis.processing.operations.EmptyModel;
 
-import uk.ac.diamond.scisoft.analysis.processing.operations.AbstractMathsOperation;
-import uk.ac.diamond.scisoft.analysis.processing.operations.ValueModel;
+public class $className$ extends AbstractOperation<EmptyModel, OperationData> {
 
-public class $className$ extends AbstractMathsOperation<ValueModel, OperationData> {
+	@Override
+	protected OperationData process(IDataset input, IMonitor monitor) throws OperationException {
 
-	protected IDataset operation(IDataset a, Object value) {
-		return a; // TODO: Operate on "a" and "value"
+		return new OperationData(input);
 	}
 
 	@Override
@@ -18,8 +22,15 @@ public class $className$ extends AbstractMathsOperation<ValueModel, OperationDat
 	}
 
 	@Override
-    public String getName() {
-		return "$operationName$";
+	public OperationRank getInputRank() {
+		return OperationRank.ANY;
 	}
+
+	@Override
+	public OperationRank getOutputRank() {
+		return OperationRank.SAME;
+	}
+
+	
 
 }
