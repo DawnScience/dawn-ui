488b1a4bb37dec3e2768d686c880fa1bf6a11609
Joel Ogden <joel.ogden@diamond.ac.uk>
2016-04-19 10:52:31 +0100

    adding classes when reorganising

---
+++ org.dawnsci.plotting.javafx/src/org/dawnsci/plotting/javafx/axis/objects/TickGroup.java
@@ -0,0 +1,204 @@
+package org.dawnsci.plotting.javafx.axis.objects;
+
+import javafx.geometry.Point3D;
+import javafx.scene.Group;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontSmoothingType;
+import javafx.scene.text.Text;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
+
+import org.dawnsci.plotting.javafx.objects.Line;
+import org.dawnsci.plotting.javafx.tools.Vector3DUtil;
+
+public class TickGroup extends Group
+{
+	
+	private Line line;
+	private Group textPane;
+	private Text textLabel;
+		
+	private Rotate labelCorrectionRotate;
+ 	 	
+ 	// this transforms
+	private Translate offset;
+	private Translate textOffset;
+	private Translate pivotPoint_TextPane;
+	private Translate translate_TextPane;
+	private Rotate rotate;
+	
+	private double textSize;
+	
+	public TickGroup(double length, Point3D direction, Point3D offset, String label, double textSize, Rotate gridRotate)
+	{		
+		super();
+		this.textSize = textSize;
+		this.offset = new Translate(offset.getX(), offset.getY(), offset.getZ());
+		this.line = new Line(length, new Rotate(), new Point3D(0,0,0));
+		
+		this.rotate = new Rotate();
+		this.textPane = new Group();
+		this.pivotPoint_TextPane = new Translate(0,0,0);
+		this.translate_TextPane = new Translate(0,0,0);
+		this.textOffset = new Translate(0,0,0);
+		
+		labelCorrectionRotate  = new Rotate();
+		
+		if (label != null)
+		{
+			
+			textLabel = createTextLabel(label);
+			
+			textLabel.setFontSmoothingType(FontSmoothingType.LCD);
+			textPane.getChildren().add(textLabel);
+			
+			setTextTransforms();
+			
+			textPane.getTransforms().addAll(
+					textOffset,
+					pivotPoint_TextPane,
+					translate_TextPane,
+					labelCorrectionRotate);
+			
+			
+			textPane.getTransforms().addAll();
+			
+		}
+		
+		
+		double angle = new Point3D(0, 1, 0).angle(direction);
+		Point3D rotateVector = new Point3D(0, 1, 0).crossProduct(direction);
+		
+		this.rotate = new Rotate(angle, 0, offset.getX(), 0, rotateVector);
+		
+		this.getChildren().addAll(this.line, this.textPane);
+		this.getTransforms().addAll(this.offset, this.rotate);
+				
+		this.localToSceneTransformProperty().addListener((obs, oldT, newT) -> {
+						
+			Rotate axisAngleRotate = Vector3DUtil.matrixToRotate(newT);
+			
+			try {
+				axisAngleRotate = (Rotate) axisAngleRotate.createInverse();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+            labelCorrectionRotate.setAxis(axisAngleRotate.getAxis());
+            labelCorrectionRotate.setAngle(axisAngleRotate.getAngle());
+            
+        });
+	}
+	
+	private void setTextTransforms()
+	{
+		double width = textLabel.getBoundsInLocal().getWidth();
+		double height= textLabel.getBoundsInLocal().getHeight();
+		double depth = textLabel.getBoundsInLocal().getDepth();	// should be zero as it is only a plane
+		
+		// Don't even ask why this is the mid point of the text... I don't understand myself.
+		Point3D midPoint = new Point3D((width/2), -(height*0.65)/2, depth/2);
+		
+		labelCorrectionRotate.setPivotX(midPoint.getX());
+		labelCorrectionRotate.setPivotY(midPoint.getY());
+		labelCorrectionRotate.setPivotZ(midPoint.getZ());
+		
+		Rotate rotate = new Rotate(-90,new Point3D(0, 0, 1));
+		rotate.setPivotX(width);
+		rotate.setPivotY(-height/4);
+		
+		pivotPoint_TextPane.setX(-midPoint.getX());
+		pivotPoint_TextPane.setY(-midPoint.getY());
+		pivotPoint_TextPane.setZ(-midPoint.getZ());
+		
+		double maxOffset = Vector3DUtil.getMaximumValue(midPoint);
+
+		this.translate_TextPane.setY(-maxOffset);
+		this.translate_TextPane.setZ(-maxOffset);
+		
+	}
+	
+	// create text label for the grid axis
+	private Text createTextLabel(String text) 
+	{
+		// create the text to return
+		Text returnText = new Text(text);
+		returnText.setText(text);
+		returnText.setFont(new Font(textSize));
+		
+		return returnText;
+	}
+		
+	public void setRotate(Rotate newRotate)
+	{
+		this.rotate = newRotate;
+	}
+	
+	public void setTextOffset(Point3D translate)
+	{
+		if (this.textOffset != null)
+		{
+			this.textOffset.setX(translate.getX());
+			this.textOffset.setY(translate.getY());
+			this.textOffset.setZ(translate.getZ());
+			
+			if (translate.getY() > 0)
+			{
+				this.translate_TextPane.setY(Math.abs(this.translate_TextPane.getY()) *  1);
+			}
+			else
+			{
+				this.translate_TextPane.setY(Math.abs(this.translate_TextPane.getY()) * -1);;
+			}
+		}
+	}
+	
+	
+	public void gridMoved(Point3D translate)
+	{
+		if (translate.getZ() > 0)
+		{
+			this.translate_TextPane.setZ(Math.abs(this.translate_TextPane.getZ()) *  1);
+		}
+		else
+		{
+			this.translate_TextPane.setZ(Math.abs(this.translate_TextPane.getZ()) * -1);
+		}
+	}
+	
+	public void setHeightExtended(double x)
+	{
+		this.line.setHeightExtended(x);
+	}
+	
+	public void resetOffset()
+	{
+		this.line.editOffset(new Translate( 0, 0 ,0));
+	}
+	
+	public void setMaterial(PhongMaterial phongMaterial)
+	{
+		this.line.setMaterial(phongMaterial);
+	}
+	
+	public void rewriteLabel(String newLabel)
+	{
+		this.textLabel.setText(newLabel);
+		setTextTransforms();
+	}
+	
+	public boolean getTextState()
+	{
+		if (this.textLabel == null)
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	public Translate getOffset()
+	{
+		return this.offset;
+	}
+}
---
+++ org.dawnsci.plotting.javafx/src/org/dawnsci/plotting/javafx/trace/volume/VolumeRender.java
@@ -0,0 +1,248 @@
+package org.dawnsci.plotting.javafx.trace.volume;
+
+import javafx.geometry.Point2D;
+import javafx.geometry.Point3D;
+import javafx.scene.DepthTest;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.transform.Rotate;
+
+import org.dawnsci.plotting.histogram.service.PaletteService;
+import org.dawnsci.plotting.javafx.tools.Vector3DUtil;
+import org.eclipse.dawnsci.analysis.api.dataset.ILazyDataset;
+import org.eclipse.dawnsci.plotting.api.histogram.functions.FunctionContainer;
+
+public class VolumeRender extends Group
+{
+	@SuppressWarnings("unused")
+	private ILazyDataset lazySlice;
+	
+	private double maxValue;
+	private double minValue;
+
+	private double maxCulling;
+	private double minCulling;
+	
+	private double opacity;
+	private double intensity;
+	
+	private Group xygroup;
+	private Group zygroup;
+	private Group zxgroup;
+	
+	/**
+	 * Create a volume render from a dataset.
+	 * @param size - the size of the volume
+	 * @param dataset - the dataset to be rendered
+	 * @param intensityValue - the intensity of each values colour
+	 */
+	public VolumeRender()
+	{
+		super();
+		
+		xygroup = new Group();
+		zygroup = new Group();
+		zxgroup = new Group();
+	}
+	
+	/**
+	 * Create a volume render from buffered image planes
+	 * @param xyPlanes - list of xy images
+	 * @param yzPlanes - list of yz images
+	 * @param zxPlanes - list of xz images
+	 * @param xyzObjectSize - the size of the volume render (int[3])
+	 */
+	
+	/*
+	 * privates 
+	 */
+	private void initialise()
+	{
+		zygroup.getTransforms().addAll(
+				new Rotate(120, new Point3D(1, 1, 1)));
+		
+		zxgroup.getTransforms().addAll(
+				new Rotate(-120, new Point3D(1, 1, 1)));
+		
+		xygroup.setDepthTest(DepthTest.DISABLE);
+		zygroup.setDepthTest(DepthTest.DISABLE);
+		zxgroup.setDepthTest(DepthTest.DISABLE);
+		
+		this.getChildren().addAll(xygroup, zygroup, zxgroup);
+		
+		
+	}
+	
+	private Group createPlanesFromDataSlice(
+			final int[]XYZSize, 
+			final PaletteService paletteService, 
+			final ILazyDataset lazySlice) 
+	{
+		Group outputGroup = new Group();
+				
+//		FunctionContainer functionContainer = paletteService.getFunctionContainer("Viridis (blue-green-yellow)");
+		FunctionContainer functionContainer = paletteService.getFunctionContainer("Plasma (blue-red-yellow)");
+//		FunctionContainer functionContainer = paletteService.getFunctionContainer("Gray Scale");
+		
+		functionContainer.setInverseRed(true);
+		functionContainer.setInverseGreen(true);
+		functionContainer.setInverseBlue(true);
+		
+		LayeredImageTexture textureData = new LayeredImageTexture(
+				lazySlice,
+				maxValue,
+				minValue,
+				maxCulling,
+				minCulling,
+				intensity,
+				functionContainer);
+		
+		LayeredPlaneMesh newPlane = new LayeredPlaneMesh(
+				new Point3D(XYZSize[0], XYZSize[1], XYZSize[2]),
+				new Point2D(lazySlice.getShape()[0], lazySlice.getShape()[1]),
+				textureData,
+				new Point3D(0, 0, 1));
+		
+		double xOffset = (double)((XYZSize[0] / (double)lazySlice.getShape()[0]) / 2);
+		double yOffset = (double)((XYZSize[1] / (double)lazySlice.getShape()[1]) / 2);
+				
+		newPlane.setTranslateX(-xOffset);
+		newPlane.setTranslateY(-yOffset);
+		
+		newPlane.setMaxOpacity(opacity);
+		newPlane.setOpacity_Material(1);
+		
+		outputGroup.getChildren().add(newPlane);
+		
+		outputGroup.localToSceneTransformProperty().addListener((obs, oldT, newT) -> {
+			
+			Rotate worldRotate = Vector3DUtil.matrixToRotate(newT);
+			
+			Point3D zVector = new Point3D(0, 0, 1);
+			try 
+			{
+				zVector = worldRotate.createInverse().transform(zVector);
+			} 
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+			
+			double zAngle = zVector.angle( new Point3D(0, 0, 1));
+						
+			double opacity = Math.abs(Math.cos(Math.toRadians(zAngle)));
+			
+			if (opacity < 0.3)
+			{
+				opacity *= (opacity*2);
+				if (opacity < 0.05)
+					opacity = 0;
+			}
+				
+			newPlane.setOpacity_Material(Math.abs(opacity));
+			newPlane.setColour(new Color(opacity, opacity, opacity, 1));
+			
+        });
+		
+		return outputGroup;
+	}
+		
+	private void setGroupColour(Color colour, Group group)
+	{
+		for (Node n : group.getChildren())
+		{
+			if (n instanceof LayeredPlaneMesh)
+			{
+				((LayeredPlaneMesh)n).setColour(colour);
+			}
+		}
+	}
+	
+	private void setGroupOpacity(double opacity, Group group)
+	{
+		
+		for (Node n : group.getChildren())
+		{
+			if (n instanceof LayeredPlaneMesh)
+			{
+				((LayeredPlaneMesh)n).setOpacity_Material(opacity);
+			}
+		}
+	}
+	
+	/* 
+	 * publics
+	 * 
+	 */
+	/**
+	 * 
+	 * @param size
+	 * @param dataset
+	 * @param intensity
+	 * @param opacity
+	 * @param paletteService
+	 */
+	public void compute(
+			final int[] size, 
+			final ILazyDataset dataset, 
+			final double intensity, 
+			final double opacity,
+			final PaletteService paletteService,
+			final double[] minMaxValue,
+			final double[] minMaxCulling)
+	{
+
+		this.minValue = minMaxValue[0];
+		this.maxValue = minMaxValue[1];
+
+		this.minCulling = minMaxCulling[0];
+		this.maxCulling = minMaxCulling[1];
+				
+		this.intensity = intensity;
+		this.opacity =  Math.pow(opacity, 3);
+			
+		System.out.println(this.opacity);
+		
+		xygroup.getChildren().clear(); 
+		zygroup.getChildren().clear(); 
+		zxgroup.getChildren().clear(); 
+				
+		// generate the planes
+		xygroup = createPlanesFromDataSlice(new int[]{size[0], size[1], size[2]}, paletteService, dataset);
+		zygroup = createPlanesFromDataSlice(new int[]{size[1], size[2], size[0]}, paletteService, dataset.getTransposedView(1,2,0).getSlice());
+		zxgroup = createPlanesFromDataSlice(new int[]{size[2], size[0], size[1]}, paletteService, dataset.getTransposedView(2,0,1).getSlice());
+		
+		this.setTranslateX((size[0] / dataset.getShape()[0])/2);
+		this.setTranslateY((size[1] / dataset.getShape()[1])/2);
+		this.setTranslateZ((size[2] / dataset.getShape()[2])/2);
+				
+		initialise();
+				
+	}
+	
+	/**
+	 * sets the colour of the volume. IGNORES THE OPACITY.
+	 * @param colour - the new colour
+	 */
+	public void setColour(Color colour)
+	{
+		setGroupColour(colour, xygroup);
+		setGroupColour(colour, zygroup);
+		setGroupColour(colour, zxgroup);
+	}
+	
+	/**
+	 * sets the opacity of the volume.
+	 * @param opacity - the new opacity
+	 */
+	public void setOpacity_Matrial(double opacity)
+	{
+		setGroupOpacity(opacity, xygroup);
+		setGroupOpacity(opacity, zygroup);
+		setGroupOpacity(opacity, zxgroup);
+	}
+	
+}
+
+
