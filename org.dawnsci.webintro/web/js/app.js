b676c135fdec8040272ac7f2e7a338891c9943d9
David Taylor <david@taylorhq.com>
2016-07-12 11:16:47 +0100

    Started work on templating

--- org.dawnsci.webintro/web/js/app.js
+++ org.dawnsci.webintro/web/js/app.js
@@ -3,6 +3,8 @@ function javaReady(){
 }
 
 function updateData(){
+	
+	
 	$( "#title span" ).first().show( "slow", function showNext() {
 		$( this ).next( "#title span" ).show( "slow", showNext );
 	});
@@ -10,6 +12,11 @@ function updateData(){
 	data = $.parseJSON(java.getIntroJSON());
 		
 	data.pages.forEach(function(entry){
+//		var source   = $("#entry-template").html();
+//		var template = Handlebars.compile(source);
+//		$("#entry-template").replaceWith();
+//		console.log(template);
+		
 		$("#tabList").append(
 		"<li role='presentation'><a href='#"+entry.page_id+"' aria-controls='"+entry.page_id+"' role='tab' data-toggle='tab'>"+entry.name+"</a></li>");
 		$("#tabContent").append(
