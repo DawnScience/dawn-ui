752b4cfede0a7ec3bfeb1473f3a4ba6ddcf328a7
Ed Nicklin <Ed.Nicklin@diamond.ac.uk>
2016-08-05 10:20:51 +0100

    Stitching overlap for Reflectivity a step closer.
    Works for multiple curves.Correction factors not good. Neeed Exponential fitting curve.

--- org.dawnsci.spectrum.ui/src/org/dawnsci/spectrum/ui/processing/StitchingOverlapProcess.java
+++ org.dawnsci.spectrum.ui/src/org/dawnsci/spectrum/ui/processing/StitchingOverlapProcess.java
@@ -2,14 +2,20 @@ package org.dawnsci.spectrum.ui.processing;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 import org.dawnsci.spectrum.ui.file.IContain1DData;
 import org.dawnsci.spectrum.ui.utils.Contain1DDataImpl;
 import org.eclipse.january.dataset.Dataset;
+import org.eclipse.january.dataset.DatasetFactory;
 import org.eclipse.january.dataset.DatasetUtils;
 import org.eclipse.january.dataset.IDataset;
+import org.eclipse.january.dataset.Maths;
+
+import uk.ac.diamond.scisoft.analysis.fitting.Fitter;
+import uk.ac.diamond.scisoft.analysis.fitting.functions.Polynomial;
 
 public class StitchingOverlapProcess extends AbstractProcess {
 
@@ -21,54 +27,193 @@ public class StitchingOverlapProcess extends AbstractProcess {
 		IDataset[] xArray = new IDataset[list.size()];
 		IDataset[] yArray = new IDataset[list.size()];
 		
-		int k =0;
+		IDataset[] xArrayCorrected = new IDataset[list.size()];
+		IDataset[] yArrayCorrected = new IDataset[list.size()];
+
+		IDataset[] xArrayCorrectedTrunc = new IDataset[1+2*list.size()];
+		IDataset[] yArrayCorrectedTrunc = new IDataset[1+2*list.size()];
 		
-		while (itr.hasNext()){
+		
+		int k=0;
+		
+		for(k =0;k<list.size();k++){
 			xArray[k] =list.get(k).getxDataset();
 			yArray[k] =list.get(k).getyDatasets().get(0);
-			k++;
+
+			xArrayCorrected[k] =list.get(k).getxDataset();
+			yArrayCorrected[k] =list.get(k).getyDatasets().get(0);
 		}
 		
-		double[][] maxMinArray = new double[list.size()][];
+		double[][] maxMinArray = new double[list.size()][2];
+		
 		
 		for (k=0; k<list.size();k++){
 			
-			maxMinArray[k][0] = (double) xArray[k].max(true, true);
-			maxMinArray[k][1] = (double) xArray[k].min(true, true);
+			maxMinArray[k][0] = (double) xArray[k].max(null);
+			maxMinArray[k][1] = (double) xArray[k].min(null);
 			
 		}
 		
 		
 		
-		ArrayList<Double> overlapLower = new ArrayList<Double>();
-		ArrayList<Double> overlapHigher = new ArrayList<Double>();
 		
-		for (k=0; k<list.size();k++){
+		
+		double[] correctionRatioArray = new double[list.size()];
+		correctionRatioArray[0]=1;
+		
+		
+		for (k=0; k<list.size()-1;k++){
 			
-			if (xArray[k+1] != null){
-				if (maxMinArray[k][0]<maxMinArray[k+1][1]){
+			ArrayList<Integer> overlapLower = new ArrayList<Integer>();
+			ArrayList<Integer> overlapHigher = new ArrayList<Integer>();
+			
+			
+			//if (xArray[k+1] != null){
+				if (maxMinArray[k][0]>maxMinArray[k+1][1]){
 					for(int l=0; l<xArray[k].getSize();l++){
-						if (xArray[k].getDouble(l)<maxMinArray[k+1][1]){
-							overlapLower.add((double) l);
+						if (xArray[k].getDouble(l)>maxMinArray[k+1][1]){
+							overlapLower.add(l);
 						}
-					for(int l=0; l<xArray[k+1].getSize();l++){
-						if (xArray[k+1].getDouble(l)<maxMinArray[k+1][0]){
-							overlapHigher.add((double) l);
+					}
+					for(int m=0; m<xArray[k+1].getSize();m++){
+						if (xArray[k+1].getDouble(m)<maxMinArray[k][0]){
+							overlapHigher.add(m);
 						}
-					}	
-					
-					
-				}
-		
+					}
 				
-		Dataset x = DatasetUtils.concatenate(xArray, 0);
-		Dataset y = DatasetUtils.concatenate(yArray, 0);
+				
+
+					
+					Dataset[] xLowerDataset =new Dataset[1];
+					Dataset yLowerDataset =null;
+					Dataset[] xHigherDataset =new Dataset[1];
+					Dataset yHigherDataset =null;
+					
+					ArrayList<Double> xLowerList =new ArrayList<>();
+					ArrayList<Double> yLowerList =new ArrayList<>();
+					ArrayList<Double> xHigherList =new ArrayList<>();
+					ArrayList<Double> yHigherList =new ArrayList<>();
+					
+//					int n=0;
+					
+					for (int l=0; l<overlapLower.size(); l++){
+						xLowerList.add(xArray[k].getDouble(overlapLower.get(l)));
+						yLowerList.add(yArray[k].getDouble(overlapLower.get(l)));
+						xLowerDataset[0] = DatasetFactory.createFromObject(xLowerList);
+						yLowerDataset = DatasetFactory.createFromObject(yLowerList);
+					}
+					
+					for (int l=0; l<overlapHigher.size(); l++){
+						xHigherList.add(xArray[k+1].getDouble(overlapHigher.get(l)));
+						yHigherList.add(yArray[k+1].getDouble(overlapHigher.get(l)));
+						xHigherDataset[0] = DatasetFactory.createFromObject(xHigherList);
+						yHigherDataset = DatasetFactory.createFromObject(yHigherList);
+
+					}
+					
+					
+					
+//					Polynomial polyFitLower = Fitter.polyFit(xLowerDataset, yLowerDataset, 1e-5,4);
+//					Polynomial polyFitHigher = Fitter.polyFit(xHigherDataset, yHigherDataset, 1e-5,4);
+					
+//					Dataset calculatedValuesHigher = polyFitHigher.calculateValues(xLowerDataset);
+//					Dataset calculatedValuesLower = polyFitLower.calculateValues(xLowerDataset);
+					
+					Dataset correctionsRatioDataset = Maths.divide(yLowerDataset.sum(), 
+							yHigherDataset.sum());
+					
+					
+					double correctionRatio = ((double) correctionsRatioDataset.sum())/((double) correctionsRatioDataset.getSize());
+					
+					correctionRatioArray[k+1] = correctionRatio;
+					
+					yArrayCorrected[k+1] = Maths.multiply((Maths.multiply(yArray[k+1],correctionRatio)), correctionRatioArray[k]);
+					
+					
+				//	}
+				}
+			}
+					
+			
+//		Dataset x = DatasetUtils.concatenate(xArray, 0);
+//		Dataset y = DatasetUtils.concatenate(yArray, 0);
+////////////////////////////////////////////////////		
+
+
+		
+		double[][] maxMinArrayCorrected = new double[list.size()][2];
 		
 		
+		for (k=0; k<list.size();k++){
+			
+			maxMinArrayCorrected[k][0] = (double) xArrayCorrected[k].max(null);
+			maxMinArrayCorrected[k][1] = (double) xArrayCorrected[k].min(null);
+			
+		}
 		
+		
+
+		
+		IDataset[] xArrayCorrTrunc = new IDataset[list.size()];
+		IDataset[] yArrayCorrTrunc = new IDataset[list.size()]; 
+		
+		//new IDataset[1+2*list.size()];
+		
+		//int r= 0;
+	
+		
+		for (k=0; k<list.size()-1;k++){
+			
+			ArrayList<Integer> overlapLowerCorr = new ArrayList<Integer>();
+			ArrayList<Integer> overlapHigherCorr = new ArrayList<Integer>();
+			
+				if (maxMinArrayCorrected[k][0]>maxMinArrayCorrected[k+1][1]){
+					for(int l=0; l<xArrayCorrected[k].getSize();l++){
+						if (xArrayCorrected[k].getDouble(l)>=maxMinArrayCorrected[k+1][1]){
+							overlapLowerCorr.add(l);
+						}
+					}
+					for(int m=0; m<xArrayCorrected[k+1].getSize();m++){
+						if (xArrayCorrected[k+1].getDouble(m)<maxMinArrayCorrected[k][0]){
+							overlapHigherCorr.add(m);
+						}
+					}
+	
+					IDataset xtemp = DatasetFactory.ones(xArrayCorrected[k].getSize()-overlapLowerCorr.size());
+					IDataset ytemp = DatasetFactory.ones(xArrayCorrected[k].getSize()-overlapLowerCorr.size());
+					
+					int bottomOfLower = Collections.min(overlapLowerCorr);
+					
+				for(int c = 0; c < bottomOfLower;c++){
+					
+					xtemp.set(xArrayCorrected[k].getObject(c), c);
+					ytemp.set(yArrayCorrected[k].getObject(c), c);
+				}
+				xArrayCorrTrunc[k] = xtemp;
+				yArrayCorrTrunc[k] = ytemp;
+
+				
+			}
+		}
+
+		xArrayCorrTrunc[list.size()-1] = xArrayCorrected[list.size()-1];
+		yArrayCorrTrunc[list.size()-1] = yArrayCorrected[list.size()-1];
+				
+				
+				
+				
+		
+//////////////////////////////////////////////////////////////		
+		
+		
+		Dataset xCorrectedTrunc = DatasetUtils.concatenate(xArrayCorrTrunc, 0);
+		Dataset yCorrectedTrunc = DatasetUtils.concatenate(yArrayCorrTrunc, 0);
+		
+		xCorrectedTrunc.setName("x");
+		yCorrectedTrunc.setName("y");
 		
 		List<IContain1DData> output = new ArrayList<IContain1DData>();
-		output.add(new Contain1DDataImpl(x, Arrays.asList(new IDataset[]{y}), list.get(0).getName() + getAppendingName(), list.get(0).getLongName() + getAppendingName()));
+		output.add(new Contain1DDataImpl(xCorrectedTrunc, Arrays.asList(new IDataset[]{yCorrectedTrunc}), list.get(0).getName() + getAppendingName(), list.get(0).getLongName() + getAppendingName()));
 		
 		return output;
 	}
