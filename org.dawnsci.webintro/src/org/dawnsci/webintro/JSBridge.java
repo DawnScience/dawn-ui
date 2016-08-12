647c82980bc7076d5c97dca319112b9185ccad36
David Taylor <david@taylorhq.com>
2016-08-12 11:01:49 +0100

    Fixed JSON dependencies

--- org.dawnsci.webintro/src/org/dawnsci/webintro/JSBridge.java
+++ org.dawnsci.webintro/src/org/dawnsci/webintro/JSBridge.java
@@ -29,18 +29,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
-import javax.json.JsonWriter;
+
 /**
  * 
  * Class which provides extension point information to the JavaScript running inside the JavaFX WebView.
