c94f0ce66557b59ac493e2e28fd30abc75ef6e2a
Ed Nicklin <Ed.Nicklin@diamond.ac.uk>
2016-08-16 16:47:48 +0100

    Corrected reflectivity tweek.

--- org.dawnsci.spectrum.ui/src/org/dawnsci/spectrum/ui/ReflectivityUI/ReflectivityDialog.java
+++ org.dawnsci.spectrum.ui/src/org/dawnsci/spectrum/ui/ReflectivityUI/ReflectivityDialog.java
@@ -23,91 +23,57 @@ import org.eclipse.swt.widgets.Shell;
 import uk.ac.diamond.scisoft.analysis.io.LoaderFactory;
 
 public class ReflectivityDialog extends Dialog {
-	
+
 	private String[] filepaths;
 	private String title;
 	private List<IContain1DData> output;
 	private List<IContain1DData> list;
-	
+
 
 	public ReflectivityDialog(Shell parentShell, String[] datFilenames,List<IContain1DData> input) {
-	super(parentShell);
-	this.filepaths = datFilenames;
-	this.list = input;
-	
+		super(parentShell);
+		this.filepaths = datFilenames;
+		this.list = input;
+
 	}
-	
-	
-	
+
 	@Override
-	  protected Control createDialogArea(Composite parent) {
-	    final Composite container = (Composite) super.createDialogArea(parent);
-	    GridLayout gridLayout = new GridLayout(3, false);
-	    container.setLayout(gridLayout);			
+	protected Control createDialogArea(Composite parent) {
+		final Composite container = (Composite) super.createDialogArea(parent);
+		GridLayout gridLayout = new GridLayout(3, false);
+		container.setLayout(gridLayout);			
 
 		ArrayList<ILazyDataset> arrayILDy = new ArrayList<ILazyDataset>();
 		ArrayList<ILazyDataset> arrayILDx = new ArrayList<ILazyDataset>();
-		
-		
+
+
 		int k=0;
-				
-				for(k =0;k<list.size();k++){
-					arrayILDx.add(list.get(k).getxDataset());
-					arrayILDy.add(list.get(k).getyDatasets().get(0));
-		
 
-//		
+		for(k =0;k<list.size();k++){
+			arrayILDx.add(list.get(k).getxDataset());
+			arrayILDy.add(list.get(k).getyDatasets().get(0));
+		}
+
 		ReflectivityUIModel model = new ReflectivityUIModel();
-		
-		
-		
-		//model.setData(aggDat);
-		
-		
-		model.setFilepaths(filepaths);
-	    title = filepaths[0];
 
-		
-	    ReflectivityCurves customComposite = new ReflectivityCurves(container, SWT.NONE, arrayILDy,arrayILDx,filepaths,title, model);
-	    customComposite.setLayout(new GridLayout());
-	    customComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-	    
-	    StitchedReflectivityCurves stitchedCurves = new StitchedReflectivityCurves(container, 
-	    		SWT.NONE, arrayILDy, arrayILDx,"Overlap Test", model);
-	    stitchedCurves.setLayout(new GridLayout());
-	    stitchedCurves.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-	    
-	    output  = stitchedCurves.getOutput();
-	    
-//	    try {
-//			PaddingClass padField = new PaddingClass(container, SWT.NONE);
-//			padField.setLayout(new GridLayout());
-//			padField.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	    
-//	    
-//	    
-//	    try {
-//			PlotSystem2Composite customComposite2 = new PlotSystem2Composite(container, SWT.NONE, 
-//					aggDat, model);
-//		    customComposite2.setLayout(new GridLayout());
-//		    customComposite2.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-//		    PlotSystem3Composite customComposite3 = new PlotSystem3Composite(container, SWT.NONE, 
-//		    		aggDat, model);
-//		    customComposite3.setLayout(new GridLayout());
-//		    customComposite3.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-//		    
-//	    } catch (Exception e2) {
-//			// TODO Auto-generated catch block
-//			e2.printStackTrace();
-//		}
-		
+		model.setFilepaths(filepaths);
+		title = filepaths[0];
+
+
+		ReflectivityCurves customComposite = new ReflectivityCurves(container, SWT.NONE, arrayILDy,arrayILDx,filepaths,title, model);
+		customComposite.setLayout(new GridLayout());
+		customComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+		StitchedReflectivityCurves stitchedCurves = new StitchedReflectivityCurves(container, 
+				SWT.NONE, arrayILDy, arrayILDx,"Overlap Test", model);
+		stitchedCurves.setLayout(new GridLayout());
+		stitchedCurves.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+		output  = stitchedCurves.getOutput();
+
 		return container;
 	}
-	
+
 	@Override
 	protected void configureShell(Shell newShell) {
 		super.configureShell(newShell);
@@ -118,16 +84,16 @@ public class ReflectivityDialog extends Dialog {
 	protected Point getInitialSize() {
 		return new Point(800, 600);
 	}
-	
+
 	@Override
-	  protected boolean isResizable() {
-	    return true;
-	  }
-	
+	protected boolean isResizable() {
+		return true;
+	}
+
 	public List<IContain1DData> getResult(){		
 		return output;
 	}
-	
-	
+
+
 }
 
