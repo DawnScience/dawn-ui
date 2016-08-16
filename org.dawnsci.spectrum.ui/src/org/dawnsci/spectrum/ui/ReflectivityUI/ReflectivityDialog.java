4ed7a41d0965498604dcf31c2eb838c406312a9f
Ed Nicklin <Ed.Nicklin@diamond.ac.uk>
2016-08-16 16:32:27 +0100

    Reflectovty Dialogue tweek

--- org.dawnsci.spectrum.ui/src/org/dawnsci/spectrum/ui/ReflectivityUI/ReflectivityDialog.java
+++ org.dawnsci.spectrum.ui/src/org/dawnsci/spectrum/ui/ReflectivityUI/ReflectivityDialog.java
@@ -29,11 +29,6 @@ public class ReflectivityDialog extends Dialog {
 	private List<IContain1DData> output;
 	private List<IContain1DData> list;
 	
-//	public ReflectivityDialog(Shell parentShell, String[] datFilenames) {
-//		super(parentShell);
-//		this.filepaths = datFilenames;
-//
-//	}
 
 	public ReflectivityDialog(Shell parentShell, String[] datFilenames,List<IContain1DData> input) {
 	super(parentShell);
@@ -54,52 +49,13 @@ public class ReflectivityDialog extends Dialog {
 		ArrayList<ILazyDataset> arrayILDx = new ArrayList<ILazyDataset>();
 		
 		
-		IDataset[] xArray = new IDataset[list.size()];
-		IDataset[] yArray = new IDataset[list.size()];
-		
-//		for (String fpath : filepaths){
-//			try {
-//				IDataHolder dh1 =LoaderFactory.getData(fpath);
-//				ILazyDataset ild =dh1.getLazyDataset("/entry/auxiliary/3-Final extraction for reflectivity/data"); 
-//				ILazyDataset ild1 =dh1.getLazyDataset("/entry/auxiliary/3-Final extraction for reflectivity/qsdcd"); 
-//				
-//				//DatasetUtils.
-//				arrayILDy.add(ild);
-//				arrayILDx.add(ild1);
-//				
-//				//{
-//			} catch (Exception e1) {
-//				// TODO Auto-generated catch block
-//				e1.printStackTrace();
-//			}
-//		}
-		
 		int k=0;
 				
 				for(k =0;k<list.size();k++){
 					arrayILDx.add(list.get(k).getxDataset());
 					arrayILDy.add(list.get(k).getyDatasets().get(0));
 		
-//					xArrayCorrected[k] =list.get(k).getxDataset();
-//					yArrayCorrected[k] =list.get(k).getyDatasets().get(0);
-				}
-			
-		
-		
-		
-//		auxiliary/3-Final extraction for reflectivity/data
-		
-		
-//		ILazyDataset[] shouldntneedthisarray = new ILazyDataset[arrayILD.size()];
-//		
-//		Iterator<ILazyDataset> itr =arrayILD.iterator();
-//		int i=0;
-//		while (itr.hasNext()){
-//			shouldntneedthisarray[i] = itr.next();
-//			i++;
-//		}
-//		
-//		final AggregateDataset aggDat = new AggregateDataset(false, shouldntneedthisarray);
+
 //		
 		ReflectivityUIModel model = new ReflectivityUIModel();
 		
@@ -110,17 +66,7 @@ public class ReflectivityDialog extends Dialog {
 		
 		model.setFilepaths(filepaths);
 	    title = filepaths[0];
-//		try {
-//			DropDownMenuTestComposite dropDown = new DropDownMenuTestComposite(container, SWT.NONE);
-//			dropDown.setLayout(new GridLayout(2,true));
-//			dropDown.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-//			test0 = dropDown.getFitDirection();
-//			test1 = dropDown.getFitPower();
-//		    
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+
 		
 	    ReflectivityCurves customComposite = new ReflectivityCurves(container, SWT.NONE, arrayILDy,arrayILDx,filepaths,title, model);
 	    customComposite.setLayout(new GridLayout());
