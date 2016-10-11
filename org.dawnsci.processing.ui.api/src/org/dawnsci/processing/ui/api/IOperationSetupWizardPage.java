884eaada66a9fd0c1464c303273f75bf382e8288
Jacob Filik <jacob.filik@diamond.ac.uk>
2016-10-11 16:07:48 +0100

    ui api package for Operation setup wizards and service

---
+++ org.dawnsci.processing.ui.api/src/org/dawnsci/processing/ui/api/IOperationSetupWizardPage.java
@@ -0,0 +1,9 @@
+package org.dawnsci.processing.ui.api;
+
+import org.eclipse.jface.wizard.IWizardPage;
+
+public interface IOperationSetupWizardPage extends IWizardPage {
+	
+	public void setOperation(IOperationSetupWizardPage operation);
+
+}
---
+++ org.dawnsci.processing.ui.api/src/org/dawnsci/processing/ui/api/IOperationUIService.java
@@ -0,0 +1,7 @@
+package org.dawnsci.processing.ui.api;
+
+public interface IOperationUIService {
+
+	public IOperationSetupWizardPage getWizardPage(String operation_id);
+	
+}
