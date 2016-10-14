d36ed577591c1eac609e0eb23bc13f4ff7577b3d
Jacob Filik <jacob.filik@diamond.ac.uk>
2016-10-14 12:02:54 +0100

    Initial commit of surface scattering ui

---
+++ org.dawnsci.surfacescatter.ui/src/org/dawnsci/surfacescatter/ui/TestDialog.java
@@ -0,0 +1,12 @@
+package org.dawnsci.surfacescatter.ui;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class TestDialog extends Dialog {
+
+	protected TestDialog(Shell parentShell) {
+		super(parentShell);
+	}
+
+}
