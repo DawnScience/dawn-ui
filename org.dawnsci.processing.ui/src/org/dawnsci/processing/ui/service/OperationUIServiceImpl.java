e13e7c01d159af93b87b21b1f937cd9d688fe63a
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-10-19 15:49:59 +0100

    OperationUIServiceImpl: implement getWizardPage
    untested

--- org.dawnsci.processing.ui/src/org/dawnsci/processing/ui/service/OperationUIServiceImpl.java
+++ org.dawnsci.processing.ui/src/org/dawnsci/processing/ui/service/OperationUIServiceImpl.java
@@ -1,12 +1,17 @@
 package org.dawnsci.processing.ui.service;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.dawnsci.processing.ui.ServiceHolder;
 import org.dawnsci.processing.ui.api.IOperationSetupWizardPage;
 import org.dawnsci.processing.ui.api.IOperationUIService;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +23,7 @@ public class OperationUIServiceImpl implements IOperationUIService {
 
 	private final static Logger logger = LoggerFactory.getLogger(OperationUIServiceImpl.class);
 
-	private Map<String, IOperationSetupWizardPage> operationSetupWizardPages;
+	private Map<String, Class<? extends IOperationSetupWizardPage>> operationSetupWizardPages;
 
 	public OperationUIServiceImpl() {
 		// Intentionally do nothing -> OSGI...
@@ -27,7 +32,61 @@ public class OperationUIServiceImpl implements IOperationUIService {
 	@Override
 	public IOperationSetupWizardPage getWizardPage(String operation_id) {
 		checkOperationSetupWizardPages();
-		return operationSetupWizardPages.get(operation_id);
+		
+		// The hash only contains the Class wizardPage belongs to.
+		// We need to construct it here.
+		// Try the 3 arg constructor first, followed by the 1 arg constructor.
+		Class <? extends IOperationSetupWizardPage> klazz = operationSetupWizardPages.get(operation_id);
+		if (klazz == null)
+			return null;
+	
+		Constructor<?> constructor = null;
+		
+		try {
+			constructor = klazz.getConstructor(String.class, String.class, ImageDescriptor.class);
+			//
+			String name = operation_id, description = null;
+			ImageDescriptor image = null;
+			try {
+				name = ServiceHolder.getOperationService().getName(operation_id);
+			} catch (Exception e) {
+				// do nothing
+			}
+			try {
+				description = ServiceHolder.getOperationService().getDescription(operation_id);
+			} catch (Exception e) {
+				// do nothing
+			}
+			try {
+				return (IOperationSetupWizardPage) constructor.newInstance(name, description, image);
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+					| InvocationTargetException e) {
+				logger.error("Could not instantiate IOperationSetupWizardPage for {}", klazz.getName());
+				logger.error("Stacktrace: ", e);
+				return null;
+			}
+		} catch (NoSuchMethodException | SecurityException e) {
+			try {
+				constructor = klazz.getConstructor(String.class);
+				String name = operation_id;
+				try {
+					name = ServiceHolder.getOperationService().getName(operation_id);
+				} catch (Exception e2) {
+					// do nothing
+				}
+				try {
+					return (IOperationSetupWizardPage) constructor.newInstance(name);
+				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+						| InvocationTargetException e2) {
+					logger.error("Could not instantiate IOperationSetupWizardPage for {}", klazz.getName());
+					logger.error("Stacktrace: ", e2);
+					return null;
+				}
+			} catch (NoSuchMethodException | SecurityException e1) {
+				logger.error("Could not find a suitable constructor for {}!", klazz.getName());
+				return null;
+			}
+		}
 	}
 
 	private synchronized void checkOperationSetupWizardPages() {
@@ -36,17 +95,23 @@ public class OperationUIServiceImpl implements IOperationUIService {
 
 		operationSetupWizardPages = new HashMap<>();
 
-		/*// look for the extension points
+		// look for the extension points
 		IConfigurationElement[] eles = Platform.getExtensionRegistry().getConfigurationElementsFor("org.eclipse.dawnsci.analysis.api.operationsetupwizardpage");
 		for (IConfigurationElement e : eles) {
 			if (!e.getName().equals("operationsetupwizardpage")) continue;
-			final String id = e.getAttribute("id");
+			final String klazz = e.getAttribute("class");
 			final String operationid = e.getAttribute("operationid");
-			String old = operationSetupWizardPages.put(operationid, id);
-			if (old != null) {
-				logger.warn("OperationDialog has been changed from {} to {} for operation {}", old, id, operationid);
+			Class<? extends IOperationSetupWizardPage> old = null;
+			try {
+				old = operationSetupWizardPages.put(operationid, ((IOperationSetupWizardPage) e.createExecutableExtension("class")).getClass());
+			} catch (CoreException e1) {
+				logger.error("Exception in createExecutableExtension", e1);
+				continue;
 			}
-		}*/
+			if (old != null) {
+				logger.warn("IOperationSetupWizardPage has been changed from {} to {} for operation {}", old.getName(), klazz, operationid);
+			}
+		}
 	}
 
 }
