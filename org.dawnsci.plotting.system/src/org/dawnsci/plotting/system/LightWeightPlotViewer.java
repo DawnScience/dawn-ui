bfb9721cc905b4b8949689cccc476be94ee25ac7
Peter Chang <peter.chang@diamond.ac.uk>
2016-11-09 11:44:40 +0000

    Fix bug in plotting more than one line in data browsing
    This is caused by the y axes becoming out of synchronization when they are removed on resetting the plotting system. This replaces the hack of not removing them at all which causes problems for other uses of the plotting system

--- org.dawnsci.plotting.system/src/org/dawnsci/plotting/system/LightWeightPlotViewer.java
+++ org.dawnsci.plotting.system/src/org/dawnsci/plotting/system/LightWeightPlotViewer.java
@@ -923,7 +923,12 @@ public class LightWeightPlotViewer<T> extends AbstractPlottingViewer<T> implemen
 			if (y==null) continue;
 			// set yaxis name
 			String dataName = dataNames!=null ? dataNames.get(i) : null;
-			dataName = dataName == null ? ((y.getName() != null && !y.getName().isEmpty()) ? y.getName() : YAXIS_DEFAULT_NAME) : dataName;
+			if (dataName == null) {
+				dataName = y.getName();
+			}
+			if (dataName == null || dataName.isEmpty()) {
+				dataName = YAXIS_DEFAULT_NAME;
+			}
 			yAxis.setTitle(dataName);
 			final LineTrace trace = new LineTrace(getName(y,rootName));
 			LineTraceImpl wrapper = new LineTraceImpl(system, trace);
@@ -1084,12 +1089,11 @@ public class LightWeightPlotViewer<T> extends AbstractPlottingViewer<T> implemen
 		xyGraph.setSelectedYAxis((IAxis) xyGraph.primaryYAxis);
 		for (Axis axis : xyGraph.getAxisList()) {
 			if (axis!=xyGraph.primaryXAxis && axis!=xyGraph.primaryYAxis) {
-				axis.setVisible(false);
-				//removeAxis((IAxis) axis);
+				axis.setVisible(false); 
+				removeAxis((IAxis) axis);
 			}
 		}
 	}
-
 
 	@Override
 	public IAnnotation createAnnotation(final String name) throws Exception {
