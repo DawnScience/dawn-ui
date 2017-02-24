0abfd36e8c634fa0146c80645384c234209ec833
Dean <dean.ottewell@diamond.ac.uk>
2017-02-24 17:17:26 +0000

    Removed components not needed to spawn peak finder + background fitting as this might fit better in a seperate setup

--- org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/fitting/PeakPrepopulateTool.java
+++ org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/fitting/PeakPrepopulateTool.java
@@ -37,7 +37,8 @@ import uk.ac.diamond.scisoft.analysis.fitting.functions.Add;
 import uk.ac.diamond.scisoft.analysis.fitting.functions.FunctionFactory;
 
 /**
- * 
+ * TODO: adjust to spawn peak finder widget
+ * @edits Dean P. Ottewell
  *
  */
 public class PeakPrepopulateTool extends Dialog {
@@ -62,7 +63,6 @@ public class PeakPrepopulateTool extends Dialog {
 	private FunctionFittingTool parentFittingTool;
 	
 	private FindInitialPeaksJob findStartingPeaksJob;
-	private FitBackgroundJob fitBackgroundJob;
 	
 	private Add pkCompFunction = null;
 	private IFunction bkgFunction = null;
@@ -84,6 +84,10 @@ public class PeakPrepopulateTool extends Dialog {
 	
 	@Override
 	protected Control createDialogArea(Composite parent) {
+		
+		
+		
+		
 		//Create/get the base containers and set up the grid layout
 		Composite windowArea = (Composite) super.createDialogArea(parent);
 		dialogContainer = new Composite(windowArea, SWT.NONE);
@@ -96,103 +100,21 @@ public class PeakPrepopulateTool extends Dialog {
 		peakFindingSpace.setLayoutData(new GridData(SWT.FILL, SWT.FILL,true, true));
 		peakFindingSpace.setLayout(new GridLayout(2, false));
 		
-//		Composite peakFindContainer = new Composite(peakFindingSpace, SWT.NONE);
-//		peakFindContainer.setLayoutData(new GridData(SWT.FILL,SWT.FILL, true, true));
-//		peakFindContainer.setLayout(new GridLayout(2, false));
 		
-		//Number of peaks text field
-		Label nrPeaksTxtLbl = new Label(peakFindingSpace, SWT.None);
-		nrPeaksTxtLbl.setText("Number of Peaks:");
-		nrPeaksTxtBox = new Text(peakFindingSpace, SWT.BORDER); 
-		nrPeaksTxtBox.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
-		//This limits the input for the text box to +ve integers
-		final IntegerDecorator nrPeaksIDec = new IntegerDecorator(nrPeaksTxtBox);
-		nrPeaksIDec.setMinimum(0);
-		
-		//When the value in the text box is changed and valid, convert to an Integer and enable the find peaks button
-		nrPeaksTxtBox.addModifyListener(new ModifyListener() {
-			@Override
-			public void modifyText(ModifyEvent e) {
-				if (!nrPeaksIDec.isError()) {
-					findPeaksButton.setEnabled(true);
-					try {
-						nrPeaks = Integer.parseInt(nrPeaksTxtBox.getText());
-					} catch (NumberFormatException nfe) {
-						// Move on.
-					}
-				} else {
-					findPeaksButton.setEnabled(false);
-				}
-				
-			}
-		});
-		
-		//Profile type combo box
-		Label peakTypeCmbLbl = new Label(peakFindingSpace, SWT.None);
-		peakTypeCmbLbl.setText("Peak Function Type:");
-		peakTypeCombo = new Combo(peakFindingSpace, SWT.READ_ONLY);
-		setAvailPeakFunctions();
-		setDefaultPeakFunction();
-		peakTypeCombo.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
 		
 		//Button to call initial peak finding
-		findPeaksButton = new Button(peakFindingSpace, SWT.PUSH);
-		GridData findPeaksButtonLayout = new GridData(SWT.CENTER, SWT.CENTER, true, true);
-		findPeaksButtonLayout.horizontalSpan = 2;
-		findPeaksButton.setLayoutData(findPeaksButtonLayout);
-		findPeaksButton.setText("Find Peaks");
-		findPeaksButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				findInitialPeaks();
-			}
-		});
-		
-		/*
-		 * This lot does what it's supposed to, but because Peak Finding is 
-		 * flakey it's hard to tell whether is does what it's supposed to or not.
-		 * 
-		 * 2015-03-13
-		 * Commented out so it doesn't get included in the release.
-		 * 
-		 * 
-		//Create a background finding space on the right of the window
-		Group bkgFindingSpace = new Group(dialogContainer,SWT.NONE);
-		bkgFindingSpace.setText("Fit Background");
-		GridLayout bkgGridLayout = new GridLayout(2, false);
-		bkgFindingSpace.setLayoutData(new GridData(SWT.FILL, SWT.FILL,true, true));
-		bkgFindingSpace.setLayout(bkgGridLayout);
-		
-		//Background type combo box
-		Composite bkgLabelCombo = new Composite(bkgFindingSpace, SWT.NONE);
-		bkgLabelCombo.setLayoutData(new GridData(SWT.FILL,SWT.FILL, true, true));
-		bkgLabelCombo.setLayout(new GridLayout(1, false));
-		Label bkgTypeCmbLbl = new Label(bkgLabelCombo, SWT.NONE);
-		bkgTypeCmbLbl.setText("Background Function Type:");
-		bkgTypeCombo = new Combo(bkgLabelCombo, SWT.READ_ONLY);
-		setAvailBkgFunctions();
-		setDefaultBkgFunction();
-		bkgTypeCombo.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
-		
-		//Background function parameters - this should allow configuration of the function, e.g. polynomial order
-//		Label bkgFnParamsPlaceHolder = new Label(bkgFindingSpace, SWT.NONE);
-//		bkgFnParamsPlaceHolder.setText("Function parameters go here");
-		
-		//Button to call background fitting
-		fitInitialBkgButton = new Button(bkgFindingSpace, SWT.PUSH);
-		GridData fitInitialBkgButtonGridData = new GridData(SWT.CENTER, SWT.CENTER, true, true);
-		fitInitialBkgButtonGridData.horizontalSpan = 2;
-		fitInitialBkgButton.setLayoutData(fitInitialBkgButtonGridData);
-		fitInitialBkgButton.setText("Fit Background");
-		fitInitialBkgButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				System.out.println("Fitting background...");
-				fitBackground();
-			}
-		});
-		 * 
-		 */
+//		findPeaksButton = new Button(peakFindingSpace, SWT.PUSH);
+//		GridData findPeaksButtonLayout = new GridData(SWT.CENTER, SWT.CENTER, true, true);
+//		findPeaksButtonLayout.horizontalSpan = 2;
+//		findPeaksButton.setLayoutData(findPeaksButtonLayout);
+//		findPeaksButton.setText("Find Peaks");
+//		findPeaksButton.addSelectionListener(new SelectionAdapter() {
+//			@Override
+//			public void widgetSelected(SelectionEvent e) {
+//				findInitialPeaks();
+//			}
+//		});
+
 		return windowArea;
 	}
 	
@@ -212,24 +134,8 @@ public class PeakPrepopulateTool extends Dialog {
 			close();
 		}
 	}
-	/**
-	 * Gets the list of available peak names and their classes from FunctionFactory
-	 * and populates combo box with them
-	 */
-	private void setAvailPeakFunctions() {
-		populateCombo(peakTypeCombo, FunctionFactory.getPeakFunctionNames());
-	}
 	
-	/**
-	 * Sets default peak profile to Pseudo-Voigt (if available)
-	 */
-	private void setDefaultPeakFunction() {
-		//TODO FIXME This should use the preferences in DAWN, maybe through FittingUtils?
-		int i = peakTypeCombo.indexOf("Pseudo-Voigt");
-		if (i >= 0) {
-			peakTypeCombo.select(i);
-		}
-	}
+	
 	
 	/**
 	 * Gets the currently selected peak profile type in the combo box
@@ -248,40 +154,6 @@ public class PeakPrepopulateTool extends Dialog {
 		for (String s : items) {
 			combo.add(s);
 		}
-	}
-
-	/**
-	 * Gets the list of available function names and their classes from FunctionFactory
-	 * and populates combo box with them
-	 */
-	private void setAvailBkgFunctions() {
-		populateCombo(bkgTypeCombo, FunctionFactory.getFunctionNames());
-	}
-	
-	/**
-	 * Sets default background to Linear (if available)
-	 */
-	private void setDefaultBkgFunction() {
-		//TODO FIXME This should use the preferences in DAWN, maybe through FittingUtils?
-		int i = bkgTypeCombo.indexOf("Linear");
-		if (i >= 0) {
-			bkgTypeCombo.select(i);
-		}
-	}
-	
-	/**
-	 * Gets the currently selected background function type in the combo box
-	 * @return peak function class
-	 */	
-	private IFunction getBackgroundFunction(){
-		String selectedBkgFuncName = bkgTypeCombo.getText();
-		IFunction selectedBackground = null;
-		try {
-			selectedBackground = FunctionFactory.getFunction(selectedBkgFuncName);
-		} catch (Exception ne) {
-			logger.error("Could not access selected function type.", ne);
-		}
-		return selectedBackground;
 	}
 	
 	/**
@@ -337,26 +209,6 @@ public class PeakPrepopulateTool extends Dialog {
 		});
 	}
 	
-	private void fitBackground() {
-		if (fitBackgroundJob == null) {
-			fitBackgroundJob = new FitBackgroundJob("Fit Background");
-		}
-
-		fitBackgroundJob.setData(roiLimits);
-		fitBackgroundJob.setPeakCompoundFunction(pkCompFunction);
-		fitBackgroundJob.setBkgFunction(getBackgroundFunction());
-
-		fitBackgroundJob.schedule();
-
-		fitBackgroundJob.addJobChangeListener(new JobChangeAdapter() {
-			@Override
-			public void done(IJobChangeEvent event) {
-				updateCompFunction(null, bkgFunction);
-				// TODO this wants updating to use something more generic
-				parentFittingTool.setInitialPeaks(compFunction);
-			}
-		});
-	}
 	
 	//**********************************
 	
@@ -389,52 +241,7 @@ public class PeakPrepopulateTool extends Dialog {
 			return Status.OK_STATUS;
 		}
 	}
-		
-		/**
-		 * Job to find background of a dataset, removing the peaks from the 
-		 * background first.
-		 */
-	private class FitBackgroundJob extends FittingJob {
 	
-		public FitBackgroundJob(String name) {
-			super(name);
-		}
-		
-		Add peakCompFunction = null;
-		IFunction fitBkgFunction;
-		
-		public void setBkgFunction(IFunction bkgInFunction) {
-			fitBkgFunction = bkgInFunction;
-		}
-		
-		public void setPeakCompoundFunction(Add peakFn) {
-			peakCompFunction = peakFn;
-		}
-		
-		protected IStatus run(IProgressMonitor monitor) {
-			//1 Calculate existing peak function
-			Dataset peakDifference;
-			if (peakCompFunction != null) {
-				Dataset peakCompValues = peakCompFunction.calculateValues(x);
-			//2 Subtract peak data from observed
-			peakDifference = Maths.subtract(y, peakCompValues);
-			} else {
-				peakDifference = y;
-			}
-			//4 Fit subtracted data to given function.
-			try {
-				//IFunction bkgFunctionCopy = bkgFitFunction.copy();
-				//TODO Need to be able to determine which fitter
-				Fitter.geneticFit(new Dataset[]{x}, peakDifference, fitBkgFunction);
-				bkgFunction = fitBkgFunction;
-			}
-			catch (Exception e) {
-				//this covers an exception of the fit routine.
-				logger.error("Background fitting encountered an error", e);
-				return Status.CANCEL_STATUS;
-			}
-		
-			return Status.OK_STATUS;
-		}
-	}
+	
+	
 }
