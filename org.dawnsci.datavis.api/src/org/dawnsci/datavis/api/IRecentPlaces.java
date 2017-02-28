1ecd9710d1e853b81ecac487f72efdbed250db09
Jacob Filik <jacob.filik@diamond.ac.uk>
2017-02-28 16:11:29 +0000

    Add api plugin

---
+++ org.dawnsci.datavis.api/src/org/dawnsci/datavis/api/IRecentPlaces.java
@@ -0,0 +1,11 @@
+package org.dawnsci.datavis.api;
+
+import java.util.List;
+
+public interface IRecentPlaces {
+
+	public void addPlace(String path);
+	
+	public List<String> getRecentPlaces();
+	
+}
