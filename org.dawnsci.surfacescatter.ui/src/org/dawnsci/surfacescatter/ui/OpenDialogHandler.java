753295d90d947c8bc84b4b3ba90c0ae253fb0f02
Jacob Filik <jacob.filik@diamond.ac.uk>
2017-03-17 13:04:40 +0000

    Add to tools menu

--- org.dawnsci.surfacescatter.ui/src/org/dawnsci/surfacescatter/ui/OpenDialogHandler.java
+++ org.dawnsci.surfacescatter.ui/src/org/dawnsci/surfacescatter/ui/OpenDialogHandler.java
@@ -12,65 +12,23 @@ public class OpenDialogHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
-		if (selection instanceof StructuredSelection) {
-//			StructuredSelection ss = (StructuredSelection)selection;
-//			
-//			List<String> selectedFile = new ArrayList<String>();
-			
-//			Iterator it = ss.iterator();
-//			
-//			while (it.hasNext()) {
-//				Object ob = it.next();
-//				if (ob instanceof IContain1DData) {
-//					selectedFile.add(((IContain1DData)ob).getLongName());
-//				}
-//			}
-//			
-//			String[] sf = new String[selectedFile.size()];
-//			
-//			for (int i = 0; i<selectedFile.size(); i++){
-//				sf[i]=selectedFile.get(i);
-//			}
-//			
-//			if (!selectedFile.isEmpty()) {
-//				String string = selectedFile.get(0);
-//				MessageBox box = new MessageBox(Display.getCurrent().getActiveShell());
-//				box.setMessage(string);
-//				box.open();
-//			}
-			
-			Shell s =PlatformUI.getWorkbench().getWorkbenchWindows()[0].getShell();
-			
-//			ExampleDialog ed =new ExampleDialog(s, sf);
-//			ed.open();
-			
-//			SurfaceScatterPresenter ssp = new SurfaceScatterPresenter(s, sf); 
-			
-//			PresenterInitialSetup pis = new PresenterInitialSetup(s, sf);
-//			pis.open();
-			
-			
-			SurfaceScatterPresenter ssp = new SurfaceScatterPresenter();
 
-			
-			ssp.setImageFolderPath(null);
-			
-			SurfaceScatterViewStart ssvs = new SurfaceScatterViewStart(s, 
-					   null, 
-					   ssp.getNumberOfImages(), 
-					   ssp.getImage(0),
-					   ssp,
-					   null);
-//					   datFolderPath);
-			
-			ssp.setSsvs(ssvs);
-			
-			ssvs.open();
-			
-			
-		}
+		Shell s =PlatformUI.getWorkbench().getWorkbenchWindows()[0].getShell();
 
+		SurfaceScatterPresenter ssp = new SurfaceScatterPresenter();
+
+		ssp.setImageFolderPath(null);
+
+		SurfaceScatterViewStart ssvs = new SurfaceScatterViewStart(s, 
+				null, 
+				ssp.getNumberOfImages(), 
+				ssp.getImage(0),
+				ssp,
+				null);
+
+		ssp.setSsvs(ssvs);
+		ssvs.open();
+		
 		return null;
 	}
 
