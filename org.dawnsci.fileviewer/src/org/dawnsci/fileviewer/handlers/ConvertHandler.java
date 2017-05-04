82ecbbf66ba106b7048ab59a586f5d47592b2cce
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-05-04 08:56:31 +0100

    ConvertHandler: support FileTableContent in selections

--- org.dawnsci.fileviewer/src/org/dawnsci/fileviewer/handlers/ConvertHandler.java
+++ org.dawnsci.fileviewer/src/org/dawnsci/fileviewer/handlers/ConvertHandler.java
@@ -1,10 +1,14 @@
 
 package org.dawnsci.fileviewer.handlers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.dawnsci.conversion.ui.ConvertWizard;
 import org.dawnsci.fileviewer.FileViewer;
+import org.dawnsci.fileviewer.table.FileTableContent;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
@@ -25,7 +29,17 @@ public class ConvertHandler {
 	public void execute() {
 		ConvertWizard cwizard = new ConvertWizard();
 		IStructuredSelection sel = fileviewer.getSelections();
-		cwizard.setSelectionOverride(sel.toList());
+		@SuppressWarnings("rawtypes")
+		List selFiles;
+		if (sel.getFirstElement() instanceof FileTableContent) {
+			selFiles = new ArrayList<>();
+			for (Object selElement : sel.toArray()) {
+				selFiles.add(((FileTableContent)selElement).getFile());
+			}
+		} else {
+			selFiles = sel.toList();
+		}
+		cwizard.setSelectionOverride(selFiles);
 		WizardDialog dialog = new WizardDialog(Display.getDefault().getActiveShell(), cwizard);
 		dialog.setPageSize(new Point(400, 450));
 		dialog.create();
