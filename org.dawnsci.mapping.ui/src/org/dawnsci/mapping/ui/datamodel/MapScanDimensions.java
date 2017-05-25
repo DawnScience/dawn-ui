ce4d95215e15315249c676bae88b7a5e17069876
Jacob Filik <jacob.filik@diamond.ac.uk>
2017-05-25 11:45:59 +0100

    fix issue in MapScanDimensions

--- org.dawnsci.mapping.ui/src/org/dawnsci/mapping/ui/datamodel/MapScanDimensions.java
+++ org.dawnsci.mapping.ui/src/org/dawnsci/mapping/ui/datamodel/MapScanDimensions.java
@@ -66,7 +66,9 @@ public class MapScanDimensions {
 	public void updateNonXYScanSlice(int dim, int value) {
 		
 		for (int i = 0; i < nonXYScanDimensions.length; i++) {
-			if (dim == nonXYScanDimensions[i]); nonXYDimensionValues[i] = value;
+			if (dim == nonXYScanDimensions[i]) {
+				nonXYDimensionValues[i] = value;
+			}
 		}
 	}
 	
