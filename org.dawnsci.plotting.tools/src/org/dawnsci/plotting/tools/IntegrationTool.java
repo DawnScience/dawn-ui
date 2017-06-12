d88319345e73cd09040c7bc5fcf2b22ec5e92377
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-06-12 14:03:31 +0100

    IntegrationTool: works properly

--- org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/IntegrationTool.java
+++ org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/IntegrationTool.java
@@ -1,23 +1,26 @@
 package org.dawnsci.plotting.tools;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.dawnsci.analysis.api.roi.IROI;
-import org.eclipse.dawnsci.analysis.dataset.roi.XAxisBoxROI;
 import org.eclipse.dawnsci.plotting.api.region.IROIListener;
 import org.eclipse.dawnsci.plotting.api.region.IRegion;
-import org.eclipse.dawnsci.plotting.api.region.ROIEvent;
 import org.eclipse.dawnsci.plotting.api.region.IRegion.RegionType;
+import org.eclipse.dawnsci.plotting.api.region.ROIEvent;
 import org.eclipse.dawnsci.plotting.api.tool.AbstractToolPage;
 import org.eclipse.dawnsci.plotting.api.trace.ILineTrace;
 import org.eclipse.dawnsci.plotting.api.trace.ITrace;
 import org.eclipse.dawnsci.plotting.api.trace.ITraceListener;
 import org.eclipse.dawnsci.plotting.api.trace.TraceEvent;
 import org.eclipse.dawnsci.plotting.api.trace.TraceWillPlotEvent;
+import org.eclipse.january.dataset.DatasetUtils;
+import org.eclipse.january.dataset.DoubleDataset;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -37,7 +40,7 @@ public class IntegrationTool extends AbstractToolPage implements IROIListener, I
 	private double upperBound;
 
 	private IRegion region;
-	//private TableViewer integrationResultsTable;
+	private TableViewer integrationResultsTable;
 	
 	
 	
@@ -61,8 +64,6 @@ public class IntegrationTool extends AbstractToolPage implements IROIListener, I
 		IROI roi = evt.getROI();
 		double[] point = roi.getBounds().getPoint();
 		double[] endPoint = roi.getBounds().getEndPoint();
-		logger.debug("point: {}", Arrays.toString(point));
-		logger.debug("endPoint: {}", Arrays.toString(endPoint));
 		lowerBound = point[0];
 		upperBound = endPoint[0];
 		update();
@@ -70,19 +71,16 @@ public class IntegrationTool extends AbstractToolPage implements IROIListener, I
 	
 	@Override
 	public void roiDragged(ROIEvent evt) {
-		logger.debug("Entering roiDragged");
 		roiEventHandler(evt);
 	}
 
 	@Override
 	public void roiChanged(ROIEvent evt) {
-		logger.debug("Entering roiChanged");
 		roiEventHandler(evt);
 	}
 
 	@Override
 	public void roiSelected(ROIEvent evt) {
-		logger.debug("Entering roiSelected");
 		roiEventHandler(evt);
 	}
 
@@ -100,6 +98,49 @@ public class IntegrationTool extends AbstractToolPage implements IROIListener, I
 		tempLabel.setText("Upper bound");
 		upperBoundLabel = new Label(composite, SWT.NONE);
 		upperBoundLabel.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+	
+		integrationResultsTable = new TableViewer(composite);
+		integrationResultsTable.getTable().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+		integrationResultsTable.getTable().setHeaderVisible(true);
+		integrationResultsTable.getTable().setLinesVisible(true);
+		integrationResultsTable.setContentProvider(new ArrayContentProvider());
+		
+		// add columns
+		final TableViewerColumn column1 = new TableViewerColumn(integrationResultsTable, SWT.NONE, 0);
+		column1.getColumn().setText("Trace");
+		column1.getColumn().setWidth(100);
+		column1.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				ILineTrace trace = (ILineTrace) element;
+				String name = trace.getDataName();
+				name = name.substring(name.lastIndexOf('/')+1);
+				return name;
+			}
+			
+			@Override
+			public String getToolTipText(Object element) {
+				ILineTrace trace = (ILineTrace) element;
+				return trace.getDataName();
+			}
+		});
+		
+		final TableViewerColumn column2 = new TableViewerColumn(integrationResultsTable, SWT.NONE, 1);
+		column2.getColumn().setText("ROI sum");
+		column2.getColumn().setWidth(100);
+		column2.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				ILineTrace trace = (ILineTrace) element;
+				DoubleDataset xData = DatasetUtils.cast(DoubleDataset.class, trace.getXData());
+				DoubleDataset yData = DatasetUtils.cast(DoubleDataset.class, trace.getYData());
+				int lowerBoundIndex = DatasetUtils.findIndexGreaterThanOrEqualTo(xData, lowerBound);
+				int upperBoundIndex = DatasetUtils.findIndexGreaterThanOrEqualTo(xData, upperBound);
+				return String.format("%g", yData.getSlice(new int[]{lowerBoundIndex}, new int[]{upperBoundIndex + 1}, null).sum());
+			}
+		});
+		
+		
 		
 		super.createControl(parent);
 	}
@@ -114,7 +155,7 @@ public class IntegrationTool extends AbstractToolPage implements IROIListener, I
 			//region.setROI(new XAxisBoxROI(ptx, pty, width, height, angle));
 			region.addROIListener(this);
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.error("Could not create region", e);
 		} 
 		update();
 		super.activate();
@@ -135,6 +176,9 @@ public class IntegrationTool extends AbstractToolPage implements IROIListener, I
 	private synchronized void update() {
 		lowerBoundLabel.setText(String.format("%g", lowerBound));
 		upperBoundLabel.setText(String.format("%g", upperBound));
+		
+		Collection<ITrace> traces = getPlottingSystem().getTraces(ILineTrace.class);
+		integrationResultsTable.setInput(traces);
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -144,7 +188,7 @@ public class IntegrationTool extends AbstractToolPage implements IROIListener, I
 		if (!(evt.getSource() instanceof List<?>) && !(evt.getSource() instanceof ITrace)) {
 			return false;
 		}
-		List<ITrace> eventSource = new ArrayList<ITrace>();
+		List<ITrace> eventSource = new ArrayList<>();
 		if (evt.getSource() instanceof List<?>)
 			eventSource = (List<ITrace>)evt.getSource();
 		if (evt.getSource() instanceof ITrace) {
@@ -184,28 +228,21 @@ public class IntegrationTool extends AbstractToolPage implements IROIListener, I
 			
 	@Override
 	public void traceRemoved(TraceEvent evt) {
-		Collection<ITrace> traces = getPlottingSystem().getTraces(ILineTrace.class);
-	
-		if (traces == null || traces.isEmpty()) {
-			return;
-		}
 		update();
 	}
 			
 	@Override
 	public void tracesRemoved(TraceEvent evt) {
-		Collection<ITrace> traces = getPlottingSystem().getTraces(ILineTrace.class);
-		if (traces == null || traces.isEmpty()) {
-			return;
-		}
 		update();
 	}
 
 	@Override
 	public void traceCreated(TraceEvent evt) {
+		// ignore
 	}
 
 	@Override
 	public void traceWillPlot(TraceWillPlotEvent evt) {
+		// ignore
 	}
 }
