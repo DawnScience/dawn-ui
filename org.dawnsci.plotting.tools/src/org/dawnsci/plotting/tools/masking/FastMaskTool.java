a4a8eac270186fba292a1e86e18fe056fa524bb5
Jacob Filik <jacob.filik@diamond.ac.uk>
2017-07-18 12:20:25 +0100

    Improvements to fast masking tool ui

--- org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/masking/FastMaskTool.java
+++ org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/masking/FastMaskTool.java
@@ -1,9 +1,12 @@
 package org.dawnsci.plotting.tools.masking;
 
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.dawb.common.ui.util.EclipseUtils;
 import org.dawb.common.ui.wizard.persistence.PersistenceExportWizard;
@@ -23,18 +26,31 @@ import org.eclipse.dawnsci.plotting.api.region.RegionEvent;
 import org.eclipse.dawnsci.plotting.api.region.RegionUtils;
 import org.eclipse.dawnsci.plotting.api.tool.AbstractToolPage;
 import org.eclipse.dawnsci.plotting.api.trace.IImageTrace;
+import org.eclipse.january.dataset.BooleanDataset;
+import org.eclipse.january.dataset.Dataset;
+import org.eclipse.january.dataset.DatasetUtils;
 import org.eclipse.january.dataset.IDataset;
+import org.eclipse.january.dataset.IndexIterator;
 import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.TraverseEvent;
+import org.eclipse.swt.events.TraverseListener;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 public class FastMaskTool extends AbstractToolPage {
 
@@ -43,6 +59,8 @@ public class FastMaskTool extends AbstractToolPage {
 	private List<NamedRegionType> regionTypes;
 	private FastMaskJob job;
 	private boolean paintMode = false;
+	private AtomicReference<Double> lowerValue;
+	private AtomicReference<Double> upperValue;
 	
 	private IROIListener iroiListener;
 	
@@ -56,6 +74,8 @@ public class FastMaskTool extends AbstractToolPage {
 		regionTypes.add(new NamedRegionType("Y-Axis", RegionType.YAXIS));
 		regionTypes.add(new NamedRegionType("Ring", RegionType.RING));
 		regionTypes.add(new NamedRegionType("Circle", RegionType.CIRCLE));
+		lowerValue = new AtomicReference<Double>(null);
+		upperValue = new AtomicReference<Double>(null);
 	}
 
 	@Override
@@ -71,59 +91,26 @@ public class FastMaskTool extends AbstractToolPage {
 		control.setLayout(new GridLayout(1, false));
 		removeMargins(control);
 		
-		Combo combo = new Combo(control, SWT.READ_ONLY);
-		String[] array = regionTypes.stream().map(r -> r.name).toArray(String[]::new);
-		combo.setItems(array);
-		combo.select(0);
+		MaskRegionComposite maskRegionComposite = new MaskRegionComposite(control, SWT.None);
+		maskRegionComposite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 		
-		Button draw = new Button(control, SWT.PUSH);
-		draw.setText("Draw");
-		draw.addSelectionListener(new SelectionAdapter() {
-			
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				String text = combo.getText();
-				Optional<NamedRegionType> findFirst = regionTypes.stream().filter(r -> text.equals(r.name)).findFirst();
-				if (findFirst.isPresent()) {
-					NamedRegionType nrt = findFirst.get();
-					IPlottingSystem<?> system = getPlottingSystem();
-					try {
-						system.createRegion(RegionUtils.getUniqueName("MaskRegion", system, (String)null), nrt.regionType);
-					} catch (Exception e1) {
-						logger.error("Could not create region!",e1);
-					}
-				}
-				
-			}
-			
+		ThresholdMaskComposite thresholdMaskComposite = new ThresholdMaskComposite(control, SWT.None);
+		thresholdMaskComposite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		
+		Button b3b = new Button(control, SWT.PUSH);
+		b3b.setText("Invert");
+		b3b.addSelectionListener(new SelectionAdapter() {
 
-		});
-		
-		
-		
-		Button b = new Button(control, SWT.PUSH);
-		b.setText("Apply");
-		b.addSelectionListener(new SelectionAdapter() {
-			
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				IImageTrace imageTrace = getImageTrace();
-				if (imageTrace != null) {
-					IDataset data = imageTrace.getData();
-					if (buffer == null) buffer = new MaskCircularBuffer(data.getShape());
-					final Collection<IRegion> regions = getPlottingSystem().getRegions();
-					
-					Runnable r = () -> {for (IRegion re: regions) {
-						buffer.maskROI(re.getROI());
-						};
-					};
-						
-					job.setRunnable(r);
-					job.schedule();
-				}
+				if (buffer == null) return;
 				
+				Runnable r = () -> buffer.invert();
+				
+				job.setRunnable(r);
+				job.schedule();
 			}
-			
+
 
 		});
 		
@@ -181,46 +168,8 @@ public class FastMaskTool extends AbstractToolPage {
 
 		});
 		
-		Button b3b = new Button(control, SWT.PUSH);
-		b3b.setText("Invert");
-		b3b.addSelectionListener(new SelectionAdapter() {
-
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				if (buffer == null) return;
-				
-				Runnable r = () -> buffer.invert();
-				
-				job.setRunnable(r);
-				job.schedule();
-			}
-
-
-		});
 		
-		Button b4 = new Button(control, SWT.CHECK);
-		b4.setText("Paint");
-		b4.addSelectionListener(new SelectionAdapter() {
 
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				paintMode = b4.getSelection();
-
-					Collection<IRegion> regions = getPlottingSystem().getRegions();
-					IROIListener roiListener = getROIListener();
-					for (IRegion next : regions) {
-
-						if (paintMode) {
-							next.addROIListener(roiListener);
-						} else {
-							next.removeROIListener(iroiListener);
-						}
-					}
-				
-			}
-
-
-		});
 		
 		getPlottingSystem().addRegionListener(new IRegionListener() {
 			
@@ -350,12 +299,211 @@ public class FastMaskTool extends AbstractToolPage {
 			Runnable r = runnable;
 			r.run();
 			
-			Runnable rDisplay = () -> getImageTrace().setMask(buffer.getMask());
+			BooleanDataset mask = buffer.getMask();
+			
+			Double lower = lowerValue.get();
+			Double upper = upperValue.get();
+			
+			if (lower != null || upper != null) {
+				includeThreshold(mask, lower, upper, DatasetUtils.convertToDataset(getImageTrace().getData()));
+			}
+			
+			Runnable rDisplay = () -> getImageTrace().setMask(mask);
 			
 			Display.getDefault().syncExec(rDisplay);
 
 			return Status.OK_STATUS;
 		}
+		
+		private void includeThreshold(BooleanDataset mask, Double lower, Double upper, Dataset data) {
+			if (!Arrays.equals(mask.getShape(), data.getShape())) throw new IllegalArgumentException("must have same shape");
+			
+			
+			int count = 0;
+			
+			IndexIterator iterator = data.getIterator();
+			
+			while (iterator.hasNext()) {
+				double element = data.getElementDoubleAbs(iterator.index);
+				
+				if (lower != null && element < lower) {
+					mask.setAbs(count, false);
+				}
+				
+				if (upper != null && element > upper) {
+					mask.setAbs(count, false);
+				}
+				
+				count++;
+			}
+		}
+		
+	}
+	
+	private class ThresholdMaskComposite extends Composite {
+
+		public ThresholdMaskComposite(Composite parent, int style) {
+			super(parent, style);
+			
+			this.setLayout(new FillLayout());
+			Group group = new Group(this, SWT.NONE);
+			group.setLayout(new GridLayout(2, false));
+			group.setText("Threshold");
+			
+			
+			Label ll = new Label(group, SWT.NONE);
+			ll.setText("Lower");
+			Text lower = new Text(group,SWT.BORDER);
+			lower.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+			lower.addTraverseListener(new TraverseListener() {
+				
+				@Override
+				public void keyTraversed(TraverseEvent e) {
+					
+					update(lowerValue,e,lower.getText());
+
+				}
+			});
+			
+			Label lu = new Label(group, SWT.NONE);
+			lu.setText("Upper");
+			Text upper = new Text(group,SWT.BORDER);
+			upper.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+			upper.addTraverseListener(new TraverseListener() {
+				
+				@Override
+				public void keyTraversed(TraverseEvent e) {
+					
+					update(upperValue,e,upper.getText());
+
+				}
+			});
+		}
+		
+		private void update(AtomicReference<Double> value, TraverseEvent e, String text) {
+			if (e.detail != SWT.TRAVERSE_RETURN && e.detail != SWT.TRAVERSE_TAB_NEXT) return;
+			
+			if (buffer == null) {
+				IImageTrace imageTrace = getImageTrace();
+				if (imageTrace == null) return;
+				
+				IDataset data = imageTrace.getData();
+				buffer = new MaskCircularBuffer(data.getShape());
+				
+			}
+			
+			Double val = null;
+			
+			try{
+				val = Double.parseDouble(text);
+			} catch (Exception ex) {
+				//ignore
+			}
+			
+			value.set(val);
+			//Does nothing
+			Runnable r = () -> {};
+
+			job.setRunnable(r);
+			job.schedule();
+		}
+		
+	}
+	
+	private class MaskRegionComposite extends Composite {
+
+		public MaskRegionComposite(Composite parent, int style) {
+			super(parent, style);
+			
+			this.setLayout(new FillLayout());
+			Group group = new Group(this, SWT.NONE);
+			group.setLayout(new GridLayout(2, false));
+			group.setText("Draw Regions");
+			
+			Combo combo = new Combo(group, SWT.READ_ONLY);
+			combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+			String[] array = regionTypes.stream().map(r -> r.name).toArray(String[]::new);
+			combo.setItems(array);
+			combo.select(0);
+			
+			Button draw = new Button(group, SWT.PUSH);
+			draw.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false));
+			draw.setText("Draw");
+			draw.addSelectionListener(new SelectionAdapter() {
+				
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					String text = combo.getText();
+					Optional<NamedRegionType> findFirst = regionTypes.stream().filter(r -> text.equals(r.name)).findFirst();
+					if (findFirst.isPresent()) {
+						NamedRegionType nrt = findFirst.get();
+						IPlottingSystem<?> system = getPlottingSystem();
+						try {
+							system.createRegion(RegionUtils.getUniqueName("MaskRegion", system, (String)null), nrt.regionType);
+						} catch (Exception e1) {
+							logger.error("Could not create region!",e1);
+						}
+					}
+					
+				}
+				
+
+			});
+			
+			
+			
+			Button b = new Button(group, SWT.PUSH);
+			b.setText("Apply");
+			b.addSelectionListener(new SelectionAdapter() {
+				
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					IImageTrace imageTrace = getImageTrace();
+					if (imageTrace != null) {
+						IDataset data = imageTrace.getData();
+						if (buffer == null) buffer = new MaskCircularBuffer(data.getShape());
+						final Collection<IRegion> regions = getPlottingSystem().getRegions();
+						
+						Runnable r = () -> {for (IRegion re: regions) {
+							buffer.maskROI(re.getROI());
+							};
+						};
+							
+						job.setRunnable(r);
+						job.schedule();
+					}
+					
+				}
+				
+
+			});
+			
+			Button b4 = new Button(group, SWT.CHECK);
+			b4.setText("Paint on region drag");
+			b4.addSelectionListener(new SelectionAdapter() {
+
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					paintMode = b4.getSelection();
+
+						Collection<IRegion> regions = getPlottingSystem().getRegions();
+						IROIListener roiListener = getROIListener();
+						for (IRegion next : regions) {
+
+							if (paintMode) {
+								next.addROIListener(roiListener);
+							} else {
+								next.removeROIListener(iroiListener);
+							}
+						}
+					
+				}
+
+
+			});
+			
+		}
+		
 		
 	}
 }
