de270310b21a820aa600f7c170d9a9be100274c2
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-07-19 10:32:45 +0100

    FileDatasetFilterFactory: new static method createCombinedFileDatasetFilter
    Useful when you need a filter that can be constructed by OR-ing or AND-ing existing filters.

--- org.dawnsci.common.widgets/src/org/dawnsci/common/widgets/filedataset/FileDatasetFilterFactory.java
+++ org.dawnsci.common.widgets/src/org/dawnsci/common/widgets/filedataset/FileDatasetFilterFactory.java
@@ -6,6 +6,10 @@ public class FileDatasetFilterFactory {
 	private FileDatasetFilterFactory() {
 		
 	}
+
+	public enum CombineOperator{
+		AND, OR
+	}
 	
 	private static class FileDatasetFilterRank implements IFileDatasetFilter {
 		
@@ -34,5 +38,23 @@ public class FileDatasetFilterFactory {
 		if (rank < 0)
 			throw new IllegalArgumentException("rank must be greater than or equal to zero");
 		return new FileDatasetFilterRank(rank);
+	}
+	
+	public static IFileDatasetFilter createCombinedFileDatasetFilter(final CombineOperator operator, final IFileDatasetFilter... filters) {
+		return dataset -> {
+			for (IFileDatasetFilter filter : filters) {
+				// not a single test is allowed to fail!
+				if (operator == CombineOperator.AND && !filter.accept(dataset))
+					return false;
+				// a single successful test is sufficient!
+				else if (operator == CombineOperator.OR && filter.accept(dataset))
+					return true;
+			}
+			if (operator == CombineOperator.AND)
+				return true;
+			else if (operator == CombineOperator.OR)
+				return false;
+			return false; // this will never be reached but the compiler needs it anyway...
+		};
 	}
 }
