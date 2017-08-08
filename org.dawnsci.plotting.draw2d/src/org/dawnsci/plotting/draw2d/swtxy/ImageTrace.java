DawnScience/dawn-ui

commit a424517eb50b1dbe7c6f09a7c67f36c77c22091f

Stop weird axis behaviour in mapping

org.dawnsci.plotting.draw2d/src/org/dawnsci/plotting/draw2d/swtxy/ImageTrace.java
@@ -1139,16 +1139,21 @@ public class ImageTrace extends Figure implements IImageTrace, IAxisListener, IT
 		if (downsampleListeners!=null) downsampleListeners.clear();
 		downsampleListeners = null;
 		
-        clearAspect(xAxis);
-        clearAspect(yAxis);
-        
 		if (getParent()!=null) getParent().remove(this);
 		xAxis.removeListener(this);
 		yAxis.removeListener(this);
-		if (xAxis instanceof DAxis)((DAxis)xAxis).setTicksAtEnds(xTicksAtEnd);
-		if (yAxis instanceof DAxis)((DAxis)yAxis).setTicksAtEnds(yTicksAtEnd);
-		if (xAxis instanceof DAxis)((DAxis)xAxis).setTicksIndexBased(false);
-		if (yAxis instanceof DAxis)((DAxis)yAxis).setTicksIndexBased(false);
+		
+		Collection<ITrace> traces = plottingSystem.getTraces(IImageTrace.class);
+		
+		if (traces == null || traces.isEmpty()) {
+			clearAspect(xAxis);
+	        clearAspect(yAxis);
+			if (xAxis instanceof DAxis)((DAxis)xAxis).setTicksAtEnds(xTicksAtEnd);
+			if (yAxis instanceof DAxis)((DAxis)yAxis).setTicksAtEnds(yTicksAtEnd);
+			if (xAxis instanceof DAxis)((DAxis)xAxis).setTicksIndexBased(false);
+			if (yAxis instanceof DAxis)((DAxis)yAxis).setTicksIndexBased(false);
+		}
+		
 		axisRedrawActive = false;
 		if (imageServiceBean!=null) imageServiceBean.dispose();
 		

commit 2b4d86eb5cfa54ca3afc612962f6492fe8126821

Fix SCI-6005 which was caused by a424517
Getting traces from the plotting system will not give the correct number of image traces!

Move resetting axes code to separate method and call it only for the last image trace. Also refactor lots of common code.

org.dawnsci.plotting.draw2d/src/org/dawnsci/plotting/draw2d/swtxy/ImageTrace.java
@@ -155,21 +155,33 @@ public class ImageTrace extends Figure implements IImageTrace, IAxisListener, IT
 		xAxis.addListener(this);
 		yAxis.addListener(this);
 
-		xTicksAtEnd = ((DAxis)xAxis).hasTicksAtEnds();
-		((DAxis)xAxis).setTicksAtEnds(false);
-		yTicksAtEnd = yAxis.hasTicksAtEnds();
-		((DAxis)yAxis).setTicksAtEnds(false);
-		((DAxis)xAxis).setTicksIndexBased(true);
-		((DAxis)yAxis).setTicksIndexBased(true);
+		xTicksAtEnd = setupTicks(xAxis);
+		yTicksAtEnd = setupTicks(yAxis);
 
 		if (xAxis instanceof AspectAxis && yAxis instanceof AspectAxis) {
-			
-			AspectAxis x = (AspectAxis)xAxis;
-			AspectAxis y = (AspectAxis)yAxis;
+			AspectAxis x = (AspectAxis) xAxis;
+			AspectAxis y = (AspectAxis) yAxis;
 			x.setKeepAspectWith(y);
-			y.setKeepAspectWith(x);		
+			y.setKeepAspectWith(x);
 		}
-				
+	}
+
+	private boolean setupTicks(Axis a) {
+		boolean hasTicksAtEnd = a.hasTicksAtEnds();
+		if (xAxis instanceof DAxis) {
+			DAxis da = (DAxis) a;
+			da.setTicksAtEnds(false);
+			da.setTicksIndexBased(true);
+		}
+		return hasTicksAtEnd;
+	}
+
+	private void resetAxisTicks(Axis a, boolean hasTicksAtEnd) {
+		if (a instanceof DAxis) {
+			DAxis da = (DAxis) a;
+			da.setTicksAtEnds(hasTicksAtEnd);
+			da.setTicksIndexBased(false);
+		}
 	}
 
 	private IPreferenceStore store;
@@ -1142,18 +1154,7 @@ public class ImageTrace extends Figure implements IImageTrace, IAxisListener, IT
 		if (getParent()!=null) getParent().remove(this);
 		xAxis.removeListener(this);
 		yAxis.removeListener(this);
-		
-		Collection<ITrace> traces = plottingSystem.getTraces(IImageTrace.class);
-		
-		if (traces == null || traces.isEmpty()) {
-			clearAspect(xAxis);
-	        clearAspect(yAxis);
-			if (xAxis instanceof DAxis)((DAxis)xAxis).setTicksAtEnds(xTicksAtEnd);
-			if (yAxis instanceof DAxis)((DAxis)yAxis).setTicksAtEnds(yTicksAtEnd);
-			if (xAxis instanceof DAxis)((DAxis)xAxis).setTicksIndexBased(false);
-			if (yAxis instanceof DAxis)((DAxis)yAxis).setTicksIndexBased(false);
-		}
-		
+
 		axisRedrawActive = false;
 		if (imageServiceBean!=null) imageServiceBean.dispose();
 		
@@ -1171,17 +1172,11 @@ public class ImageTrace extends Figure implements IImageTrace, IAxisListener, IT
 		this.rgbDataset       = null;
 		this.fullMask         = null;
 	}
-	
+
+	@Override
 	public void dispose() {
 		remove();
-	}
-
-	private void clearAspect(Axis axis) {
-        if (axis instanceof AspectAxis ) {			
-			AspectAxis aaxis = (AspectAxis)axis;
-			aaxis.setKeepAspectWith(null);
-			aaxis.setMaximumRange(null);
-		}
+		resetAxes();
 	}
 
 	@Override
@@ -2165,5 +2160,23 @@ public class ImageTrace extends Figure implements IImageTrace, IAxisListener, IT
 		// TODO Auto-generated method stub
 		
 	}
-	
+
+	private void clearAspect(Axis axis) {
+		if (axis instanceof AspectAxis) {
+			AspectAxis aaxis = (AspectAxis) axis;
+			aaxis.setKeepAspectWith(null);
+			aaxis.setMaximumRange(null);
+		}
+	}
+
+	/**
+	 * Reset axes back to not keep aspect ratio and original ticks settings
+	 */
+	public void resetAxes() {
+		clearAspect(xAxis);
+		clearAspect(yAxis);
+
+		resetAxisTicks(xAxis, xTicksAtEnd);
+		resetAxisTicks(yAxis, yTicksAtEnd);
+	}
 }

org.dawnsci.plotting.draw2d/src/org/dawnsci/plotting/draw2d/swtxy/RegionArea.java
@@ -446,23 +446,17 @@ public class RegionArea extends PlotArea implements IPlotArea {
 	}
 
 	public void clearImageTraces() {
-		if (imageTraces==null) return;
-		for (ImageTrace trace : imageTraces.values()) {
-			trace.remove();
-			fireImageTraceRemoved(new TraceEvent(trace));
-		}
-		imageTraces.clear();
+		internalClearImageTraces();
 		revalidate();
 	}
 
-	
 	@Override
 	protected void layout() {
-		setFigureBounds(imageTraces);		
-		setFigureBounds(vectorTraces);			
-        super.layout();
+		setFigureBounds(imageTraces);
+		setFigureBounds(vectorTraces);
+		super.layout();
 	}
-		
+
     private void setFigureBounds(Map<String, ? extends Figure> traces) {
     	if (traces == null) return;
 	    final Rectangle clientArea = getClientArea();
@@ -757,20 +751,7 @@ public class RegionArea extends PlotArea implements IPlotArea {
 			traceList.clear();
 	    }
 		
-		if (imageTraces!=null) {
-			final Collection<ImageTrace> its = new HashSet<ImageTrace>(imageTraces.values());
-			for (ImageTrace trace : its) {
-				final ImageTrace gone = imageTraces.remove(trace.getName());
-				if (gone!=null){
-					trace.remove();
-					fireImageTraceRemoved(new TraceEvent(trace));
-				}
-			}
-
-			imageTraces.clear();
-
-		}
-		
+		internalClearImageTraces();
 		
 		// Catch all needed for fix to http://jira.diamond.ac.uk/browse/SCI-1318
 		try {
@@ -787,6 +768,27 @@ public class RegionArea extends PlotArea implements IPlotArea {
 
 	}
 
+	private void internalClearImageTraces() {
+		if (imageTraces==null || imageTraces.size() == 0) {
+			return;
+		}
+
+		final Collection<ImageTrace> its = new HashSet<ImageTrace>(imageTraces.values());
+		ImageTrace last = null;
+		for (ImageTrace trace : its) {
+			final ImageTrace gone = imageTraces.remove(trace.getName());
+			if (gone != null) {
+				trace.remove();
+				fireImageTraceRemoved(new TraceEvent(trace));
+				last = gone;
+			}
+		}
+
+		if (last != null) {
+			last.resetAxes();
+		}
+		imageTraces.clear();
+	}
 
 	public void setPaletteData(PaletteData data) {
 		if (imageTraces!=null) for (ImageTrace trace : imageTraces.values()) {