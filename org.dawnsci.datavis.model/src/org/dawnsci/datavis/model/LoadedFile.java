ab38efacdbc82615eb0b8d0f4a894c719dd9aa64
Jacob Filik <jacob.filik@diamond.ac.uk>
2017-09-28 13:49:48 +0100

    Dont rely on metadata being there

--- org.dawnsci.datavis.model/src/org/dawnsci/datavis/model/LoadedFile.java
+++ org.dawnsci.datavis.model/src/org/dawnsci/datavis/model/LoadedFile.java
@@ -110,16 +110,33 @@ public class LoadedFile implements IDataObject, IDataFilePackage {
 	
 	public Map<String, int[]> getDataShapes(){
 		
-		Map<String, int[]> ds = dataHolder.get().getMetadata().getDataShapes();
-		ds = new HashMap<>(ds);
-		for (String s : ds.keySet()) {
-			if (ds.get(s) == null) {
-				ILazyDataset lazyDataset = dataHolder.get().getLazyDataset(s);
-				if (lazyDataset != null) ds.put(s, lazyDataset.getShape());
+		IDataHolder dh = dataHolder.get();
+		
+		//use metadata if possible
+		if (dh.getMetadata() != null && dh.getMetadata().getDataShapes() != null) {
+			Map<String, int[]> ds = dataHolder.get().getMetadata().getDataShapes();
+			ds = new HashMap<>(ds);
+			for (String s : ds.keySet()) {
+				if (ds.get(s) == null) {
+					ILazyDataset lazyDataset = dataHolder.get().getLazyDataset(s);
+					if (lazyDataset != null) ds.put(s, lazyDataset.getShape());
+				}
 			}
+			
+			return ds;
+		} else {
+			String[] ds = dataHolder.get().getNames();
+			Map<String, int[]> dsmap = new HashMap<>();
+			for (String s : ds) {
+
+				ILazyDataset lazyDataset = dataHolder.get().getLazyDataset(s);
+				if (lazyDataset != null) dsmap.put(s, lazyDataset.getShape());
+
+			}
+
+			return dsmap;
 		}
 		
-		return ds;
 	}
 	
 	public boolean isSelected() {
