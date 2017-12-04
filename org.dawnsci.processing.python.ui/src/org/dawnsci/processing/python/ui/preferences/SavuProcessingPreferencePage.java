63a653cea362a9991a18eb98ddf3c2796f86e699
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-12-04 10:56:04 +0000

    SCI-6372: remove unused Savu PreferenceStore code

--- org.dawnsci.processing.python.ui/src/org/dawnsci/processing/python/ui/preferences/SavuProcessingPreferencePage.java
+++ org.dawnsci.processing.python.ui/src/org/dawnsci/processing/python/ui/preferences/SavuProcessingPreferencePage.java
@@ -2,23 +2,17 @@ package org.dawnsci.processing.python.ui.preferences;
 
 import java.io.FileNotFoundException;
 
-import org.dawnsci.processing.python.ui.Activator;
 import org.dawnsci.processing.python.ui.SavuWindow2;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
 public class SavuProcessingPreferencePage extends PreferencePage implements
 		IWorkbenchPreferencePage {
-	public SavuProcessingPreferencePage() {
-	}
 	
-	private Text remoteURITextBox;
-
 	@Override
 	protected Control createContents(Composite parent) {
 		Composite savu = null;
@@ -30,31 +24,10 @@ public class SavuProcessingPreferencePage extends PreferencePage implements
         
 		return savu;
 	}
-	
+
 	@Override
 	public void init(IWorkbench workbench) {
-		setPreferenceStore(Activator.getDefault().getPreferenceStore());
-	}
-	
-	@Override
-	public boolean performOk() {
-		String text = remoteURITextBox.getText();
-//		getPreferenceStore().setValue(ProcessingConstants.REMOTE_RUNNER_URI, text);
-//		getPreferenceStore().setValue(ProcessingConstants.FORCE_SERIES, forceSeries.getSelection());
-		
-		return super.performOk();
-	}
-	
-	@Override
-	protected void performDefaults() {
-		getPreferenceStore().setValue(
-				SavuProcessingConstants.REMOTE_RUNNER_URI, getPreferenceStore().getDefaultString(
-						SavuProcessingConstants.REMOTE_RUNNER_URI));
-		
-		getPreferenceStore().setValue(
-				SavuProcessingConstants.FORCE_SERIES, getPreferenceStore().getDefaultString(
-						SavuProcessingConstants.FORCE_SERIES));
-		
+		// no preferences for now
 	}
 
 }
