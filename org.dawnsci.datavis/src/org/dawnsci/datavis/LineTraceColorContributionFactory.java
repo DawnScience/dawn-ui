8ad90d6ede4a782d0632885627d30cb3e7e04f41
Jacob Filik <jacob.filik@diamond.ac.uk>
2018-01-17 11:16:22 +0000

    remove data vis menu item as test

--- org.dawnsci.datavis/src/org/dawnsci/datavis/LineTraceColorContributionFactory.java
+++ org.dawnsci.datavis/src/org/dawnsci/datavis/LineTraceColorContributionFactory.java
@@ -20,63 +20,63 @@ public class LineTraceColorContributionFactory extends ExtensionContributionFact
 		MenuManager search = new MenuManager("Line Plot Colors",
                 "org.dawnsci.datavis.linecolormenu");
 		
-		BundleContext bundleContext =
-                FrameworkUtil.
-                getBundle(this.getClass()).
-                getBundleContext();
-		
-		final IPlotController plotController = bundleContext.getService(bundleContext.getServiceReference(IPlotController.class));
-		final ITraceColorProviderService traceColourProvider = bundleContext.getService(bundleContext.getServiceReference(ITraceColorProviderService.class));
-		
-		
-		search.addMenuListener(new IMenuListener() {
-			
-			@Override
-			public void menuAboutToShow(IMenuManager manager) {
-				
-				search.removeAll();
-				
-				ITraceColourProvider current = plotController.getColorProvider();
-				
-				Action a = new Action("Default Colours"){
-					@Override
-					public void run() {
-						plotController.setColorProvider(null);
-					}
-				};
-				
-				if (current == null) {
-					a.setChecked(true);
-				}
-				
-				search.add(a);
-				
-				try {
-					
-				
-				
-				ITraceColourProvider[] pm = traceColourProvider.getColorProviders();
-				
-				for (ITraceColourProvider f : pm) {
-					
-					Action ac = new Action(f.getName()) {
-			        	
-			        	@Override
-			        	public void run() {
-			        		plotController.setColorProvider(f);
-			        		
-			        	}
-					};
-					
-					if (current == f) {
-						ac.setChecked(true);
-					}
-					
-					search.add(ac);
-				}
-			} catch (Exception e){};
-			}
-		});
+//		BundleContext bundleContext =
+//                FrameworkUtil.
+//                getBundle(this.getClass()).
+//                getBundleContext();
+//		
+//		final IPlotController plotController = bundleContext.getService(bundleContext.getServiceReference(IPlotController.class));
+//		final ITraceColorProviderService traceColourProvider = bundleContext.getService(bundleContext.getServiceReference(ITraceColorProviderService.class));
+//		
+//		
+//		search.addMenuListener(new IMenuListener() {
+//			
+//			@Override
+//			public void menuAboutToShow(IMenuManager manager) {
+//				
+//				search.removeAll();
+//				
+//				ITraceColourProvider current = plotController.getColorProvider();
+//				
+//				Action a = new Action("Default Colours"){
+//					@Override
+//					public void run() {
+//						plotController.setColorProvider(null);
+//					}
+//				};
+//				
+//				if (current == null) {
+//					a.setChecked(true);
+//				}
+//				
+//				search.add(a);
+//				
+//				try {
+//					
+//				
+//				
+//				ITraceColourProvider[] pm = traceColourProvider.getColorProviders();
+//				
+//				for (ITraceColourProvider f : pm) {
+//					
+//					Action ac = new Action(f.getName()) {
+//			        	
+//			        	@Override
+//			        	public void run() {
+//			        		plotController.setColorProvider(f);
+//			        		
+//			        	}
+//					};
+//					
+//					if (current == f) {
+//						ac.setChecked(true);
+//					}
+//					
+//					search.add(ac);
+//				}
+//			} catch (Exception e){};
+//			}
+//		});
 
 		search.add(new Action("") {
 
