e5f5accb58ac23aada439ce4c1d15aadf1b6c7c1
Peter Chang <peter.chang@diamond.ac.uk>
2018-02-07 16:11:00 +0000

    Preserve order when filtering datasets for signals

--- org.dawnsci.datavis.model/src/org/dawnsci/datavis/model/LoadedFile.java
+++ org.dawnsci.datavis.model/src/org/dawnsci/datavis/model/LoadedFile.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -49,7 +50,7 @@ public class LoadedFile implements IDataObject, IDataFilePackage {
 
 	public LoadedFile(IDataHolder dataHolder) {
 		this.dataHolder = new AtomicReference<IDataHolder>(dataHolder.clone());
-		this.signals = new HashSet<>();
+		this.signals = new LinkedHashSet<>();
 		dataOptions = new LinkedHashMap<>();
 		possibleLabels = new HashMap<>();
 		String[] names = null;
