625032218e505bb1861bf13637e52275a54f68dd
Peter Chang <peter.chang@diamond.ac.uk>
2018-05-09 10:03:45 +0100

    Update copyright notice

--- org.dawnsci.datavis.model/src/org/dawnsci/datavis/model/LabelValueMetadata.java
+++ org.dawnsci.datavis.model/src/org/dawnsci/datavis/model/LabelValueMetadata.java
@@ -1,3 +1,12 @@
+/*-
+ * Copyright (c) 2018 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.dawnsci.datavis.model;
 
 import org.eclipse.january.dataset.Dataset;
