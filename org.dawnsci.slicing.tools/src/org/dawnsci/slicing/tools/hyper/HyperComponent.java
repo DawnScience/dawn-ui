52734d5673b8c4dba609a594cc53553f3626333a
Jacob Filik <jacob.filik@diamond.ac.uk>
2018-06-20 16:25:24 +0100

    SCI-7430 Implement Hyper4D for arpes maps
    I'm sorry. This code and the HyperComponent in general are horrific for what they are doing.
    
    I have raised SCI-7482 to improve the framework, but I didnt realise the state of it until I had done this work.
    
    Change-Id: Ic427867d35a30614db6b8c8122008e5a69c32ac4

--- org.dawnsci.slicing.tools/src/org/dawnsci/slicing/tools/hyper/HyperComponent.java
+++ org.dawnsci.slicing.tools/src/org/dawnsci/slicing/tools/hyper/HyperComponent.java
@@ -10,16 +10,10 @@
 package org.dawnsci.slicing.tools.hyper;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 import org.dawb.common.ui.util.GridUtils;
 import org.dawb.common.ui.widgets.ActionBarWrapper;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.dawnsci.analysis.api.roi.IROI;
 import org.eclipse.dawnsci.plotting.api.IPlottingSystem;
 import org.eclipse.dawnsci.plotting.api.PlotType;
@@ -34,8 +28,6 @@ import org.eclipse.dawnsci.plotting.api.tool.IToolPageSystem;
 import org.eclipse.dawnsci.plotting.api.trace.ColorOption;
 import org.eclipse.dawnsci.plotting.api.trace.ILineTrace;
 import org.eclipse.dawnsci.plotting.api.trace.ITrace;
-import org.eclipse.dawnsci.plotting.api.trace.TraceUtils;
-import org.eclipse.dawnsci.slicing.api.util.ProgressMonitorWrapper;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.january.dataset.Dataset;
 import org.eclipse.january.dataset.DatasetFactory;
@@ -55,7 +47,6 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.Page;
@@ -82,9 +73,10 @@ public class HyperComponent {
 	private Composite mainComposite;
 	private IWorkbenchPart part;
 	private SashForm sashForm;
-	private final static Logger logger = LoggerFactory.getLogger(HyperComponent.class);
+	private static final Logger logger = LoggerFactory.getLogger(HyperComponent.class);
 	private List<IAction> leftActions;
 	private List<IAction> rightActions;
+	
 	
 	private static final String SS1 = "uk.ac.diamond.scisoft.analysis.rcp.views.HyperPlotView.reducerGroup1";
 	private static final String SS2 = "uk.ac.diamond.scisoft.analysis.rcp.views.HyperPlotView.reducerGroup2";
@@ -517,199 +509,4 @@ public class HyperComponent {
 		rightJob.profile(r,rb);
 	}
 	
-	private void updateImage(final IPlottingSystem<Composite> plot, final IDataset image, final List<IDataset> axes) {
-		
-		plot.updatePlot2D(image, axes, null);
-		
-	}
-	
-	private void updateTrace(final IPlottingSystem<Composite> plot, final IDataset axis, final IDataset data, final boolean update, final IRegion region) {
-
-		if (update) {
-			plot.updatePlot1D(axis,Arrays.asList(new IDataset[] {data}), null);
-			plot.repaint();	
-		} else {
-			final List<ITrace> traceOut = plot.createPlot1D(axis,Arrays.asList(new IDataset[] {data}), null);
-
-			Display.getDefault().syncExec(new Runnable() {
-				@Override
-				public void run() {
-					for (ITrace trace : traceOut) {
-						trace.setUserObject(region);
-						if (trace instanceof ILineTrace){
-							region.setRegionColor(((ILineTrace)trace).getTraceColor());
-							//use name listener to update color
-							String name = region.getName();
-							plot.renameRegion(region, name + " Color");
-							plot.renameRegion(region, name);
-						}
-					}
-				}
-			});
-		}
-	}
-	
-	private void updateTrace(final IPlottingSystem<Composite> plot, final IDataset axis, final IDataset data) {
-
-		Display.getDefault().syncExec(new Runnable() {
-			@Override
-			public void run() {
-
-				Collection<ITrace> traces = plot.getTraces(ILineTrace.class);
-
-				List<IDataset> datasets = convertFrom2DToListOf1D(axis, data);
-
-				if (traces == null || traces.isEmpty()) {
-					plot.createPlot1D(axis, datasets, null);
-					return;
-				}
-				int i = 0;
-				for (ITrace trace : traces) {
-					if (i < datasets.size()) ((ILineTrace)trace).setData(axis, datasets.get(i));
-					else plot.removeTrace(trace);
-					i++;
-				}
-
-				if (i >= datasets.size()) {
-					plot.repaint();
-					return;
-				}
-
-				List<IDataset> subdatasets = new ArrayList<IDataset>(datasets.size() - i);
-
-				for (; i < datasets.size(); ++i) {
-					subdatasets.add(datasets.get(i));
-				}
-
-				plot.createPlot1D(axis, subdatasets, null);
-				plot.repaint();
-			}
-		});
-	}
-	
-	private List<IDataset> convertFrom2DToListOf1D(IDataset axis, IDataset data) {
-		
-		int[] dataShape = data.getShape();
-		
-		List<IDataset> datasets = new ArrayList<IDataset>(dataShape[0]);
-		Slice[] slices = new Slice[2];
-		slices[0] = new Slice(0,1,1);
-		
-		for (int i = 0; i < dataShape[0]; i++) {
-			slices[0].setStart(i);
-			slices[0].setStop(i+1);
-			
-			IDataset out = data.getSlice(slices);
-			
-			out.setName("trace_" + i);
-			
-			datasets.add(out.squeeze());
-		}
-		
-		return datasets;
-		
-	}
-
-	private class HyperDelegateJob extends Job {
-		
-		private IRegion currentRegion;
-		private IROI currentROI;
-		private IPlottingSystem<Composite> plot;
-		private ILazyDataset data;
-		private List<IDataset> axes;
-		private int[] order;
-		private Slice[] slices;
-		private IDatasetROIReducer reducer;
-		
-		
-		public HyperDelegateJob(String name,
-				IPlottingSystem<Composite> plot,
-				ILazyDataset data,
-				List<IDataset> axes,
-				Slice[] slices,
-				int[] order,
-				IDatasetROIReducer reducer) {
-			
-			super(name);
-			this.plot = plot;
-			this.data = data;
-			this.axes = axes;
-			this.order = order;
-			this.slices = slices;
-			this.reducer = reducer;
-			setSystem(false);
-			setUser(false);
-		}
-		
-		public void profile(IRegion r, IROI rb) {
-			
-			cancel(); // Needed for large datasets but makes small ones look less responsive.
-			this.currentRegion = r;
-			this.currentROI    = rb;
-	        
-          	schedule();		
-		}
-		
-		public void updateData(ILazyDataset data,
-				List<IDataset> axes,
-				Slice[] slices,
-				int[] order){
-			this.data = data;
-			this.axes = axes;
-			this.order = order;
-			this.slices = slices;
-		}
-		
-		public IDatasetROIReducer getReducer() {
-			return reducer;
-		}
-
-		@Override
-		protected IStatus run(IProgressMonitor monitor) {
-			try {
-				if (monitor.isCanceled()) return Status.CANCEL_STATUS;
-				
-				IDataset output = this.reducer.reduce(data, axes, currentROI, slices, order, new ProgressMonitorWrapper(monitor));
-				if (output==null) return Status.CANCEL_STATUS;
-				
-				List<IDataset> outputAxes = this.reducer.getAxes();
-
-				if (!this.reducer.isOutput1D()) {
-					output.setName("Image");
-					updateImage(plot,output,outputAxes);
-				} else {
-					
-					IDataset axis = null;
-					
-					if (outputAxes != null && !outputAxes.isEmpty()) {
-						axis = outputAxes.get(0);
-					}
-					
-					if (output.getRank() == 1) {
-						Collection<ITrace> traces = plot.getTraces();
-						for (ITrace trace : traces) {
-							Object uo = trace.getUserObject();
-							if (uo == currentRegion) {
-								output.setName(trace.getName());
-								updateTrace(plot,axis,output,true,currentRegion);
-								return Status.OK_STATUS;
-							}
-						}
-
-						String name = TraceUtils.getUniqueTrace("trace", plot, (String[])null);
-						output.setName(name);
-						
-						updateTrace(plot,axis,output,false,currentRegion);
-					} else {
-						updateTrace(plot,axis,output);
-					}
-					
-				}
-
-				return Status.OK_STATUS;
-			} catch (Throwable ne) {
-				return Status.CANCEL_STATUS;
-			}
-		}
-	}
 }
---
+++ org.dawnsci.slicing.tools/src/org/dawnsci/slicing/tools/hyper/PlotModeHyper4D.java
@@ -0,0 +1,94 @@
+package org.dawnsci.slicing.tools.hyper;
+
+import org.dawnsci.datavis.api.ILazyPlotMode;
+import org.eclipse.dawnsci.plotting.api.IPlottingSystem;
+import org.eclipse.dawnsci.plotting.api.trace.ITrace;
+import org.eclipse.january.dataset.IDataset;
+import org.eclipse.january.dataset.ILazyDataset;
+import org.eclipse.january.dataset.SliceND;
+
+public class PlotModeHyper4D implements ILazyPlotMode {
+
+	private static final String[] options =  new String[]{"X2", "Y2", "X1", "Y1"};
+	private ILazyDataset view4d;
+	private int[] order = new int[]{0,1,2,3};
+	private SliceND slice;
+	
+	@Override
+	public String[] getOptions() {
+		return options;
+	}
+
+	@Override
+	public IDataset[] sliceForPlot(ILazyDataset lz, SliceND slice, Object[] options, IPlottingSystem<?> system)
+			throws Exception {
+		view4d = lz.getSliceView();
+		this.slice = slice;
+		int count = 0;
+		order = this.order.clone();
+		for (int j = 0; j <options.length; j++) {
+			if (options[j] != null) {
+				if (options[j].equals(PlotModeHyper4D.options[3])) {
+					order[0] = count++;
+				} else if (options[j].equals(PlotModeHyper4D.options[2])) {
+					order[1] = count++;
+				}else if (options[j].equals(PlotModeHyper4D.options[1])) {
+					order[2] = count++;
+				} else if (options[j].equals(PlotModeHyper4D.options[0])) {
+					order[3] = count++;
+				} else {
+					count++;
+				}
+			}
+		}
+	
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return "Hyper4d";
+	}
+
+	@Override
+	public boolean supportsMultiple() {
+		return false;
+	}
+
+	@Override
+	public int getMinimumRank() {
+		return 4;
+	}
+
+	@Override
+	public boolean isThisMode(ITrace trace) {
+		return trace instanceof IHyper4DTrace;
+	}
+
+	@Override
+	public int[] getDataDimensions(Object[] currentOptions) {
+		return null;
+	}
+
+	@Override
+	public void displayData(ITrace[] update, IPlottingSystem<?> system, Object userObject) throws Exception {
+		if (update != null && update.length > 0 && update[0] instanceof IHyperTrace) {
+			IHyper4DTrace h = (IHyper4DTrace)update[0];
+			h.setData(view4d, order, slice);
+			h.setUserObject(userObject);
+		} else {
+			IHyper4DTrace t = system.createTrace("Hyper Trace", IHyper4DTrace.class);
+
+			t.setData(view4d, order, slice);
+			t.setUserObject(userObject);
+			system.addTrace(t);
+		}
+		
+	}
+
+	@Override
+	public void displayData(IDataset[] data, ITrace[] update, IPlottingSystem<?> system, Object userObject)
+			throws Exception {
+	}
+
+}
