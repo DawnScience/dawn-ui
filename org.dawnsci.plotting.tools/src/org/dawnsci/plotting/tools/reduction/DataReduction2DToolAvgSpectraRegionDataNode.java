184aed9ff232014bf2ac33fbd2e16c1dcee548a6
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-07-23 13:24:40 +0100

    SCI-7632: DataReduction2DTool -> dramatically speedup average spectrum calculation
    Change-Id: If790a75271eb58ced818c6b1750c20f798ba1387

--- org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/reduction/DataReduction2DToolAvgSpectraRegionDataNode.java
+++ org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/reduction/DataReduction2DToolAvgSpectraRegionDataNode.java
@@ -3,7 +3,6 @@ package org.dawnsci.plotting.tools.reduction;
 import org.eclipse.dawnsci.plotting.api.region.IRegion;
 import org.eclipse.january.dataset.Dataset;
 import org.eclipse.january.dataset.DatasetFactory;
-import org.eclipse.january.dataset.DatasetUtils;
 
 class DataReduction2DToolAvgSpectraRegionDataNode extends DataReduction2DToolSpectraRegionDataNode {
 
@@ -13,15 +12,13 @@ class DataReduction2DToolAvgSpectraRegionDataNode extends DataReduction2DToolSpe
 	
 	@Override
 	public Dataset getDataset(Dataset fullData) {
-		Dataset result = DatasetFactory.zeros(fullData.getClass(), 0, fullData.getShapeRef()[1]);
-		for (DataReduction2DToolSpectrumDataNode node : this.getSpectra()) {
+		Dataset result = DatasetFactory.zeros(fullData.getClass(), 1, fullData.getShapeRef()[1]);
+		for (DataReduction2DToolSpectrumDataNode node : getSpectra()) {
 			int i = node.getIndex();
 			Dataset data = fullData.getSliceView(new int[]{i, 0}, new int[]{i + 1, fullData.getShape()[1]}, new int[]{1,1});
-			data.setShape(1, fullData.getShape()[1]);
-			result = DatasetUtils.append(result, data, 0);
+			result.iadd(data);
 		}
-		result = result.mean(0);
-		result.setShape(1, fullData.getShape()[1]);
+		result.idivide(getSpectra().size());
 		return result;
 	}
 	
