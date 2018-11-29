1b2d77bc53e2ce008f4a54454d6c0c97d036fe1f
Jacob Filik <jacob.filik@diamond.ac.uk>
2018-11-29 22:23:49 +0000

    Remove duplicate code, fixing histogram control appearing incorrectly
    Change-Id: I17a4b04213389efea4a8da4ca63c64d1a669715e

--- org.dawnsci.datavis.e4.addons/src/org/dawnsci/datavis/e4/addons/HistogramToolbarAddon.java
+++ org.dawnsci.datavis.e4.addons/src/org/dawnsci/datavis/e4/addons/HistogramToolbarAddon.java
@@ -1,96 +1,15 @@
 package org.dawnsci.datavis.e4.addons;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
+public class HistogramToolbarAddon extends AbstractDatavisAddon{
 
-import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.eclipse.e4.core.di.annotations.Optional;
-import org.eclipse.e4.ui.di.UIEventTopic;
-import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.model.application.ui.SideValue;
-import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
-import org.eclipse.e4.ui.model.application.ui.basic.MTrimBar;
-import org.eclipse.e4.ui.model.application.ui.basic.MTrimElement;
-import org.eclipse.e4.ui.model.application.ui.basic.MTrimmedWindow;
-import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
-import org.eclipse.e4.ui.model.application.ui.menu.MMenuFactory;
-import org.eclipse.e4.ui.model.application.ui.menu.MToolControl;
-import org.eclipse.e4.ui.workbench.UIEvents;
-import org.eclipse.e4.ui.workbench.UIEvents.EventTags;
-import org.osgi.service.event.Event;
-
-public class HistogramToolbarAddon {
-
-	@Inject
-	MApplication application;
-	
-	private MToolControl control;
-	
-    @PostConstruct
-    public void init(IEclipseContext context) {
-        // injected IEclipseContext comes from the application
-        
-        control = getMyControl();
-        
-        if (control == null) {
-			MTrimBar topTrimBar = getTopTrimBar();
-			control = MMenuFactory.INSTANCE.createToolControl();
-			control.setElementId(HistogramToolbarControl.ID);
-			control.setContributionURI(HistogramToolbarControl.CLASS_URI);
-			control.getTags().add("Draggable");
-			topTrimBar.getChildren().add(0, control);
-		}
-        control.setVisible(true);
-        
-    }
-    
-    
-    @Inject
-	@Optional
-	public void subscribeTopicSelectedElement(
-			@UIEventTopic(UIEvents.ElementContainer.TOPIC_SELECTEDELEMENT) Event event) {
-		
-		
-		Object newValue = event.getProperty(EventTags.NEW_VALUE);
-		
-		if (!(newValue instanceof MPerspective)) {
-			return;
-		}
-
-		MPerspective perspective = (MPerspective) newValue;
-		String id  = perspective.getElementId();
-		
-		if (id.equals("org.dawnsci.datavis.DataVisPerspective")) {
-			control.setVisible(true);
-		} else {
-			control.setVisible(false);
-		}
+	@Override
+	protected String getElementID() {
+		return HistogramToolbarControl.ID;
 	}
-    
-    private MToolControl getMyControl() {
-		MTrimBar topTrimBar = getTopTrimBar();
-		if (topTrimBar == null) {
-			return null;
-		}
-		for (MTrimElement trimElement : topTrimBar.getChildren()) {
-			if (HistogramToolbarControl.ID.equals(trimElement.getElementId())) {
-				return (MToolControl) trimElement;
-			}
-		}
-		return null;
+
+
+	@Override
+	protected String getURI() {
+		return HistogramToolbarControl.CLASS_URI;
 	}
-    
-    private MTrimBar getTopTrimBar() {
-		for (MWindow window : application.getChildren()) {
-			if (window instanceof MTrimmedWindow) {
-				for (MTrimBar trimBar : ((MTrimmedWindow) window).getTrimBars()) {
-					if (trimBar.getSide() == SideValue.TOP) {
-						return trimBar;
-					}
-				}
-			}
-		}
-		return null;
-	}
-    
 }
