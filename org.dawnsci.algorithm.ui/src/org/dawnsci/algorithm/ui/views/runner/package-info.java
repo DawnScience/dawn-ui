bed64bf6306da519f7cfa19bae9c7cdea183b968
Matthew Webber <matthew.webber@diamond.ac.uk>
2018-12-14 12:22:21 +0000

    DASCTEST-545 Remove org.dawnsci.algorithm.ui, imported by neither GDA nor Dawn
    Change-Id: Id267f2be5c81c55fa5d202e28ee22245aece482d

--- org.dawnsci.algorithm.ui/src/org/dawnsci/algorithm/ui/views/runner/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Package for view and associated classes for creating
- * custom views which run algorithms e.g. workflows.
- * 
- * @author Matthew Gerring
- */
-package org.dawnsci.algorithm.ui.views.runner;
