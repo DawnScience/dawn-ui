DawnScience/dawn-ui

commit 10b79390578df87b8f73922af9d4d2ddd181fa94

Delay axes initialization of traces until they are added
This allows traces to be created in a non-GUI thread.

Change-Id: Ib0f08a7bee3c0c8047234e6792705ad93e0202a7

org.dawnsci.plotting.system/src/org/dawnsci/plotting/system/LightWeightPlotViewer.java
@@ -963,12 +963,7 @@ public class LightWeightPlotViewer<T> extends AbstractPlottingViewer<T> implemen
 
 
 	protected IImageTrace createImageTrace(String traceName) {
-		final Axis xAxis = (Axis)getSelectedXAxis();
-		final Axis yAxis = (Axis)getSelectedYAxis();
-		xAxis.setLogScale(false);
-		yAxis.setLogScale(false);
-
-		final ImageTrace trace = xyGraph.createImageTrace(traceName, xAxis, yAxis, intensity);
+		final ImageTrace trace = xyGraph.createImageTrace(traceName, intensity);
 		trace.setPlottingSystem(system);
 		trace.addPaletteListener(paletteListener);
 		return trace;
@@ -996,6 +991,7 @@ public class LightWeightPlotViewer<T> extends AbstractPlottingViewer<T> implemen
 		}
 	}
 
+	@SuppressWarnings("unchecked")
 	public <U extends ITrace> U createTrace(String name, Class<? extends ITrace> clazz) {
 		if (ILineTrace.class.isAssignableFrom(clazz)) {
 			return (U) createLineTrace(name);
@@ -1011,30 +1007,18 @@ public class LightWeightPlotViewer<T> extends AbstractPlottingViewer<T> implemen
 	}
 
 	protected ILineTrace createLineTrace(String traceName) {
-		final AspectAxis xAxis = (AspectAxis)getSelectedXAxis();
-		xAxis.setLabelDataAndTitle(null);
-		final AspectAxis yAxis = (AspectAxis)getSelectedYAxis();
-		yAxis.setLabelDataAndTitle(null);
-
-		LightWeightDataProvider traceDataProvider = new LightWeightDataProvider();
-		final LineTrace   trace    = new LineTrace(traceName, xAxis, yAxis, traceDataProvider);
+		final LineTrace   trace    = new LineTrace(traceName);
 		final LineTraceImpl wrapper = new LineTraceImpl(getSystem(), trace);
 		return wrapper;
 	}
 
 	protected IVectorTrace createVectorTrace(String traceName) {
-		final Axis xAxis = (Axis)getSelectedXAxis();
-		final Axis yAxis = (Axis)getSelectedYAxis();
-
-		final VectorTrace trace    = new VectorTrace(traceName, xAxis, yAxis);
+		final VectorTrace trace    = new VectorTrace(traceName);
 		return trace;
 	}
 
 	protected IImageStackTrace createImageStackTrace(String traceName) {
-		final Axis xAxis = (Axis)getSelectedXAxis();
-		final Axis yAxis = (Axis)getSelectedYAxis();
-
-		final ImageStackTrace trace = xyGraph.createImageStackTrace(traceName, xAxis, yAxis, intensity);
+		final ImageStackTrace trace = xyGraph.createImageStackTrace(traceName, intensity);
 		trace.setPlottingSystem(system);
 		return trace;
 	}
@@ -1057,6 +1041,7 @@ public class LightWeightPlotViewer<T> extends AbstractPlottingViewer<T> implemen
 	 * @param traceMap, may be null
 	 * @return
 	 */
+	@Override
 	public List<ILineTrace> createLineTraces(final String                title, 
 			final IDataset              x, 
 			final List<? extends IDataset> ys,
@@ -1171,14 +1156,18 @@ public class LightWeightPlotViewer<T> extends AbstractPlottingViewer<T> implemen
 		system.fireWillPlot(evt);
 		if (!evt.doit) return false;
 
+		final AspectAxis xAxis = (AspectAxis) getSelectedXAxis();
+		final AspectAxis yAxis = (AspectAxis) getSelectedYAxis();
 		if (trace instanceof IImageTrace) {
+			xAxis.setLogScale(false);
+			yAxis.setLogScale(false);
+			trace.initialize(xAxis, yAxis);
 
-			final IImageTrace image = (IImageTrace)trace;
+			final IImageTrace image = (IImageTrace) trace;
 
 			boolean autoscale = image.getGlobalRange()!= null && !system.isRescale() ? false : true;
 
 			if (!image.setData(evt.getImage(), evt.getAxes(), autoscale)) return false; // But not plotted
-
 
 			xyGraph.addImageTrace((ImageTrace)image);
 			removeAdditionalAxes(); // Do not have others with images.
@@ -1191,7 +1180,7 @@ public class LightWeightPlotViewer<T> extends AbstractPlottingViewer<T> implemen
 
 			// If we are a stack, show the scale for iterating images.
 			if (image instanceof IImageStackTrace) {
-				IImageStackTrace stack = (IImageStackTrace)image;
+				IImageStackTrace stack = (IImageStackTrace) image;
 				if (stack.getStackSize()>1) {
 					setFolderScaleVisible(true);
 					folderScale.setRange(0, stack.getStackSize()-1);
@@ -1202,18 +1191,16 @@ public class LightWeightPlotViewer<T> extends AbstractPlottingViewer<T> implemen
 			} else {
 				setFolderScaleVisible(false);
 			}
-
 		} else if (trace instanceof IVectorTrace) {
-
+			trace.initialize(xAxis, yAxis);
 			final IVectorTrace vector = (IVectorTrace)trace;
 			xyGraph.addVectorTrace((VectorTrace)vector);
 			vector.setVisible(true);
-
 		} else {
-
-			final AspectAxis xAxis = (AspectAxis)getSelectedXAxis();
-			final AspectAxis yAxis = (AspectAxis)getSelectedYAxis();
-			xyGraph.addTrace(((LineTraceImpl)trace).getTrace(), xAxis, yAxis, true);
+			xAxis.setLabelDataAndTitle(null);
+			yAxis.setLabelDataAndTitle(null);
+			trace.initialize(xAxis, yAxis);
+			xyGraph.addTrace(((LineTraceImpl) trace).getTrace(), xAxis, yAxis, true);
 			intensity.setVisible(false);
 		}
 		redraw();
@@ -1222,16 +1209,16 @@ public class LightWeightPlotViewer<T> extends AbstractPlottingViewer<T> implemen
 	}
 
 	public void removeTrace(ITrace trace) {
-
 		if (trace instanceof LineTraceImpl) {
-			xyGraph.removeTrace(((LineTraceImpl)trace).getTrace());
+			xyGraph.removeTrace(((LineTraceImpl) trace).getTrace());
 		} else if (trace instanceof ImageTrace) {
 			((ImageTrace)trace).removePaletteListener(paletteListener);
-			xyGraph.removeImageTrace((ImageTrace)trace);
+			xyGraph.removeImageTrace((ImageTrace) trace);
 		}else if (trace instanceof VectorTrace) {
-			xyGraph.removeVectorTrace((VectorTrace)trace);
+			xyGraph.removeVectorTrace((VectorTrace) trace);
 		}
-		redraw();		
+
+		redraw();
 	}
 
 	public void setShowLegend(boolean b) {

org.dawnsci.plotting.system/src/org/dawnsci/plotting/system/LineTraceImpl.java
@@ -54,15 +54,25 @@ public class LineTraceImpl implements ILineTrace, ITraceListener{
 		if (trace instanceof ITraceContainer) {
 			((ITraceContainer)trace).setTrace(this);
 		}
-		IDataProvider prov = trace.getDataProvider();
-		if (prov!=null && prov.hasErrors()) {
-			trace.setErrorBarEnabled(getPreferenceStore().getBoolean(PlottingConstants.GLOBAL_SHOW_ERROR_BARS));
-			trace.setErrorBarColor(ColorConstants.red);
-		}
 		
 		trace.addListener(this);
 	}
-	
+
+	@Override
+	public void initialize(IAxis... axes) {
+		Axis xAxis = (Axis) axes[0];
+		Axis yAxis = (Axis) axes[0];
+		IDataProvider prov = trace.getDataProvider();
+		if (prov == null) {
+			prov = new LightWeightDataProvider();
+		}
+		if (prov.hasErrors()) {
+			trace.setErrorBarEnabled(getPreferenceStore().getBoolean(PlottingConstants.GLOBAL_SHOW_ERROR_BARS));
+			trace.setErrorBarColor(ColorConstants.red);
+		}
+		trace.init(xAxis, yAxis, prov);
+	}
+
 	private IPreferenceStore getPreferenceStore() {
 		return new ScopedPreferenceStore(InstanceScope.INSTANCE, "org.dawnsci.plotting");
 	}

org.dawnsci.datavis.model.test/src/org/dawnsci/datavis/model/test/MockImageTrace.java
@@ -1,10 +1,9 @@
 package org.dawnsci.datavis.model.test;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
-
 import java.util.List;
 
 import org.eclipse.dawnsci.analysis.api.roi.IROI;
+import org.eclipse.dawnsci.plotting.api.axis.IAxis;
 import org.eclipse.dawnsci.plotting.api.histogram.HistogramBound;
 import org.eclipse.dawnsci.plotting.api.histogram.ImageServiceBean;
 import org.eclipse.dawnsci.plotting.api.histogram.ImageServiceBean.HistoType;
@@ -27,7 +26,11 @@ public class MockImageTrace implements IImageTrace {
 	public MockImageTrace(String name) {
 		this.name = name;
 	}
-	
+
+	@Override
+	public void initialize(IAxis... axes) {
+	}
+
 	@Override
 	public PaletteData getPaletteData() {
 		// TODO Auto-generated method stub

org.dawnsci.datavis.model.test/src/org/dawnsci/datavis/model/test/MockLineTrace.java
@@ -14,7 +14,11 @@ public class MockLineTrace implements ILineTrace {
 	public MockLineTrace(String name) {
 		this.name = name;
 	}
-	
+
+	@Override
+	public void initialize(IAxis... axes) {
+	}
+
 	@Override
 	public String getDataName() {
 		// TODO Auto-generated method stub

org.dawnsci.jzy3d/src/org/dawnsci/jzy3d/Abstract2DJZY3DTrace.java
@@ -3,6 +3,7 @@ package org.dawnsci.jzy3d;
 import java.util.Arrays;
 import java.util.List;
 
+import org.eclipse.dawnsci.plotting.api.axis.IAxis;
 import org.eclipse.dawnsci.plotting.api.histogram.IImageService;
 import org.eclipse.dawnsci.plotting.api.histogram.IPaletteService;
 import org.eclipse.january.dataset.DatasetUtils;
@@ -27,6 +28,10 @@ public abstract class Abstract2DJZY3DTrace extends AbstractColorMapTrace {
 	private IDataset yAxis;
 	
 	protected ColorMapper colorMapper;
+
+	@Override
+	public void initialize(IAxis... axes) {
+	}
 
 	public void setData(IDataset data, IDataset[] axes) {
 		

org.dawnsci.plotting.draw2d/src/org/dawnsci/plotting/draw2d/swtxy/ImageStackTrace.java
@@ -22,7 +22,6 @@ import org.eclipse.january.DatasetException;
 import org.eclipse.january.dataset.IDataset;
 import org.eclipse.january.dataset.ILazyDataset;
 import org.eclipse.nebula.visualization.widgets.figureparts.ColorMapRamp;
-import org.eclipse.nebula.visualization.xygraph.figures.Axis;
 import org.eclipse.swt.widgets.Display;
 
 public class ImageStackTrace extends ImageTrace implements IImageStackTrace {
@@ -32,10 +31,8 @@ public class ImageStackTrace extends ImageTrace implements IImageStackTrace {
     private StackJob     stackJob;
     
 	public ImageStackTrace(String       name, 
-			               Axis         xAxis, 
-			               Axis         yAxis,
 			               ColorMapRamp intensityScale) {
-		super(name, xAxis, yAxis, intensityScale);
+		super(name, intensityScale);
 		this.stackJob = new StackJob();
 	}
 

org.dawnsci.plotting.draw2d/src/org/dawnsci/plotting/draw2d/swtxy/ImageTrace.java
@@ -29,6 +29,7 @@ import org.eclipse.dawnsci.analysis.dataset.roi.PolygonalROI;
 import org.eclipse.dawnsci.analysis.dataset.roi.PolylineROI;
 import org.eclipse.dawnsci.analysis.dataset.roi.RectangularROI;
 import org.eclipse.dawnsci.plotting.api.IPlottingSystem;
+import org.eclipse.dawnsci.plotting.api.axis.IAxis;
 import org.eclipse.dawnsci.plotting.api.histogram.HistogramBound;
 import org.eclipse.dawnsci.plotting.api.histogram.IImageService;
 import org.eclipse.dawnsci.plotting.api.histogram.IPaletteService;
@@ -135,13 +136,9 @@ public class ImageTrace extends Figure implements IImageTrace, IAxisListener, IT
 	private double[] globalRange;
 		
 	public ImageTrace(final String name, 
-			          final Axis xAxis, 
-			          final Axis yAxis,
 			          final ColorMapRamp intensityScale) {
 		
 		this.name  = name;
-		this.xAxis = xAxis;
-		this.yAxis = yAxis;
 		this.intensityScale = intensityScale;
 
 		this.service = ServiceHolder.getImageService();
@@ -149,6 +146,13 @@ public class ImageTrace extends Figure implements IImageTrace, IAxisListener, IT
 		setPaletteName(getPreferenceStore().getString(BasePlottingConstants.COLOUR_SCHEME));
 		
 		downsampleType = DownsampleType.forLabel(getPreferenceStore().getString(BasePlottingConstants.DOWNSAMPLE_PREF));
+
+	}
+
+	@Override
+	public void initialize(IAxis... axes) {
+		this.xAxis = axes[0] instanceof Axis ? (Axis) axes[0] : null;
+		this.yAxis = axes[1] instanceof Axis ? (Axis) axes[1] : null;
 
 		xAxis.addListener(this);
 		yAxis.addListener(this);

org.dawnsci.plotting.draw2d/src/org/dawnsci/plotting/draw2d/swtxy/RegionArea.java
@@ -53,7 +53,6 @@ import org.eclipse.jface.action.IStatusLineManager;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.nebula.visualization.widgets.figureparts.ColorMapRamp;
 import org.eclipse.nebula.visualization.xygraph.figures.Annotation;
-import org.eclipse.nebula.visualization.xygraph.figures.Axis;
 import org.eclipse.nebula.visualization.xygraph.figures.IXYGraph;
 import org.eclipse.nebula.visualization.xygraph.figures.PlotArea;
 import org.eclipse.nebula.visualization.xygraph.figures.Trace;
@@ -350,19 +349,19 @@ public class RegionArea extends PlotArea implements IPlotArea {
 	}
 	
 
-	public ImageTrace createImageTrace(String name, Axis xAxis, Axis yAxis, ColorMapRamp intensity) {
+	public ImageTrace createImageTrace(String name, ColorMapRamp intensity) {
         
-		final ImageTrace trace = new ImageTrace(name, xAxis, yAxis, intensity);
+		final ImageTrace trace = new ImageTrace(name, intensity);
 		fireImageTraceCreated(new TraceEvent(trace));
 		
 		return trace;
 	}
 
-	public ImageStackTrace createImageStackTrace(String name, Axis xAxis, Axis yAxis, ColorMapRamp intensity) {
+	public ImageStackTrace createImageStackTrace(String name, ColorMapRamp intensity) {
 		
         if (imageTraces.containsKey(name)) throw new RuntimeException("There is an image called '"+name+"' already plotted!");
         
-		final ImageStackTrace trace = new ImageStackTrace(name, xAxis, yAxis, intensity);
+		final ImageStackTrace trace = new ImageStackTrace(name, intensity);
 		fireImageTraceCreated(new TraceEvent(trace));
 		
 		return trace;

commit 403e22c0833ccbb05e32788dd6d279f58ed79b07

Fix line plotting broken by commit 10b79390578df87b8f73922af9d4d2ddd181fa94
Also add back error bar preference behaviour and rationalize preference store access

Change-Id: Ia51e43ced9f60c5d3064680718433bfdaa1a9fa8

org.dawnsci.plotting.system/src/org/dawnsci/plotting/system/LightWeightPlotViewer.java
@@ -1100,6 +1100,10 @@ public class LightWeightPlotViewer<T> extends AbstractPlottingViewer<T> implemen
 			LightWeightDataProvider traceDataProvider = new LightWeightDataProvider(evt.getXData(), evt.getYData());
 
 			//create the trace
+			if (traceDataProvider.hasErrors()) {
+				trace.setErrorBarEnabled(system.getPreferenceStore().getBoolean(PlottingConstants.GLOBAL_SHOW_ERROR_BARS));
+				trace.setErrorBarColor(ColorConstants.red);
+			}
 			trace.init(xAxis, yAxis, traceDataProvider);
 
 			if (y.getName()!=null && !"".equals(y.getName())) {

org.dawnsci.plotting.system/src/org/dawnsci/plotting/system/LineTraceImpl.java
@@ -10,7 +10,6 @@ package org.dawnsci.plotting.system;
 
 import org.dawnsci.plotting.AbstractPlottingSystem;
 import org.dawnsci.plotting.draw2d.swtxy.LineTrace;
-import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.dawnsci.plotting.api.axis.IAxis;
 import org.eclipse.dawnsci.plotting.api.preferences.PlottingConstants;
 import org.eclipse.dawnsci.plotting.api.trace.ILineTrace;
@@ -21,14 +20,12 @@ import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.january.dataset.DatasetFactory;
 import org.eclipse.january.dataset.IDataset;
 import org.eclipse.january.dataset.IntegerDataset;
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.nebula.visualization.xygraph.dataprovider.IDataProvider;
 import org.eclipse.nebula.visualization.xygraph.figures.Axis;
 import org.eclipse.nebula.visualization.xygraph.figures.ITraceListener;
 import org.eclipse.nebula.visualization.xygraph.figures.Trace;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.preferences.ScopedPreferenceStore;
 
 /**
  * This class only wraps line traces, images have their own IImageTrace implementor.
@@ -61,23 +58,18 @@ public class LineTraceImpl implements ILineTrace, ITraceListener{
 	@Override
 	public void initialize(IAxis... axes) {
 		Axis xAxis = (Axis) axes[0];
-		Axis yAxis = (Axis) axes[0];
+		Axis yAxis = (Axis) axes[1];
 		IDataProvider prov = trace.getDataProvider();
 		if (prov == null) {
 			prov = new LightWeightDataProvider();
 		}
 		if (prov.hasErrors()) {
-			trace.setErrorBarEnabled(getPreferenceStore().getBoolean(PlottingConstants.GLOBAL_SHOW_ERROR_BARS));
+			trace.setErrorBarEnabled(sys.getPreferenceStore().getBoolean(PlottingConstants.GLOBAL_SHOW_ERROR_BARS));
 			trace.setErrorBarColor(ColorConstants.red);
 		}
 		trace.init(xAxis, yAxis, prov);
 	}
 
-	private IPreferenceStore getPreferenceStore() {
-		return new ScopedPreferenceStore(InstanceScope.INSTANCE, "org.dawnsci.plotting");
-	}
-
-	
 	public IDataProvider getDataProvider() {
 		return trace.getDataProvider();
 	}
@@ -416,10 +408,10 @@ public class LineTraceImpl implements ILineTrace, ITraceListener{
 			trace.setDataProvider(prov);
 			
 			if (xData.hasErrors() && !errorBarExplicitySet) {
-				trace.setErrorBarEnabled(getPreferenceStore().getBoolean(PlottingConstants.GLOBAL_SHOW_ERROR_BARS));
+				trace.setErrorBarEnabled(sys.getPreferenceStore().getBoolean(PlottingConstants.GLOBAL_SHOW_ERROR_BARS));
 			}
 			if (yData.hasErrors() && !errorBarExplicitySet) {
-				trace.setErrorBarEnabled(getPreferenceStore().getBoolean(PlottingConstants.GLOBAL_SHOW_ERROR_BARS));
+				trace.setErrorBarEnabled(sys.getPreferenceStore().getBoolean(PlottingConstants.GLOBAL_SHOW_ERROR_BARS));
 			}
 			
 			if (sys!=null) try {

org.dawnsci.plotting.system/src/org/dawnsci/plotting/system/PlottingSystemImpl.java
@@ -790,7 +790,7 @@ public class PlottingSystemImpl<T> extends AbstractPlottingSystem<T> {
 	}
 
 	private IPreferenceStore store;
-	private IPreferenceStore getPreferenceStore() {
+	public IPreferenceStore getPreferenceStore() {
 		if (store!=null) return store;
 		store = new ScopedPreferenceStore(InstanceScope.INSTANCE, "org.dawnsci.plotting");
 		return store;

org.dawnsci.plotting/src/org/dawnsci/plotting/AbstractPlottingSystem.java
@@ -55,6 +55,7 @@ import org.eclipse.dawnsci.plotting.api.trace.IWaterfallTrace;
 import org.eclipse.dawnsci.plotting.api.trace.TraceEvent;
 import org.eclipse.dawnsci.plotting.api.trace.TraceWillPlotEvent;
 import org.eclipse.draw2d.Figure;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.jface.viewers.ISelectionProvider;
@@ -126,6 +127,8 @@ public abstract class AbstractPlottingSystem<T> implements IPlottingSystem<T>, I
 	public AbstractPlottingSystem() {
 		this.actionBarManager = createActionBarManager();
 	}
+
+	abstract public IPreferenceStore getPreferenceStore();
 
 	public void setPointControls(Text pointControls) {
 		this.pointControls = pointControls;