baf34a677f02a58f623d2326f1584225a74583aa
Jacob Filik <jacob.filik@diamond.ac.uk>
2019-10-08 13:48:44 +0100

    Add json plugin to feature to fix processing
    also added some debug statements
    
    Change-Id: Ia291baea1a9f08ced1b42ce48648f898ae3f713e

--- org.dawnsci.commandserver.processing/src/org/dawnsci/commandserver/processing/OperationApplication.java
+++ org.dawnsci.commandserver.processing/src/org/dawnsci/commandserver/processing/OperationApplication.java
@@ -6,9 +6,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.dawnsci.commandserver.processing.process.OperationExecution;
+import org.eclipse.dawnsci.analysis.api.persistence.IMarshallerService;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
-import org.eclipse.scanning.api.event.IEventConnectorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import uk.ac.diamond.scisoft.analysis.processing.bean.OperationBean;
 
@@ -24,6 +26,8 @@ import uk.ac.diamond.scisoft.analysis.processing.bean.OperationBean;
  */
 public class OperationApplication implements IApplication {
 
+	private final static Logger logger = LoggerFactory.getLogger(OperationApplication.class);
+	
 	private OperationExecution runner;
 
 	/**
@@ -32,6 +36,8 @@ public class OperationApplication implements IApplication {
 	 */
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
+		
+		logger.debug("Starting Processing Application");
 		
 		// Parse out the configuration.
 		final Map      args          = context.getArguments();
@@ -45,9 +51,15 @@ public class OperationApplication implements IApplication {
 			}
 		}
 		
-		this.runner = new OperationExecution();
-		runner.run(createOperationBean(conf));
-		return runner;
+		try {
+			this.runner = new OperationExecution();
+			runner.run(createOperationBean(conf));
+		} catch (Exception e) {
+			logger.error("Error occured in processing:", e);
+			return new Integer(1);
+		}
+		
+		return IApplication.EXIT_OK;
 	}
 
 	private OperationBean createOperationBean(Map<String, String> conf) throws Exception {
@@ -55,7 +67,13 @@ public class OperationApplication implements IApplication {
 		
 		String json = new String(Files.readAllBytes(new File(path).toPath()));
 		
-		OperationBean bean = Activator.getService(IEventConnectorService.class).unmarshal(json, OperationBean.class);
+		IMarshallerService service = Activator.getService(IMarshallerService.class);
+		
+		if (service == null) {
+			throw new Exception("Could not get services required to deserialize bean");
+		}
+		
+		OperationBean bean = service.unmarshal(json, OperationBean.class);
 		if (conf.containsKey("publisheruri")){
 			bean.setPublisherURI(conf.get("publisheruri"));
 		}
