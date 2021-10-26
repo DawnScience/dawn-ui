385bed632995c550f7942ba56ccaae29dd0c5efc
Jacob Filik <jacob.filik@diamond.ac.uk>
2021-10-26 08:41:35 +0100

    Try slicing 3d blocks into memory
    Beamline would like it to be a bit more responsive
    
    Change-Id: I836465d6148863434270a88ab7276d793ee51c7f

--- org.dawnsci.multidimensional.ui/src/org/dawnsci/multidimensional/ui/arpes/PlotModeArpesSlice.java
+++ org.dawnsci.multidimensional.ui/src/org/dawnsci/multidimensional/ui/arpes/PlotModeArpesSlice.java
@@ -4,9 +4,16 @@ import org.dawnsci.multidimensional.ui.hyper.IHyperTrace;
 import org.dawnsci.multidimensional.ui.hyper.PlotModeHyper;
 import org.eclipse.dawnsci.plotting.api.IPlottingSystem;
 import org.eclipse.dawnsci.plotting.api.trace.ITrace;
+import org.eclipse.january.dataset.IDataset;
+import org.eclipse.january.dataset.ILazyDataset;
+import org.eclipse.january.dataset.SliceND;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PlotModeArpesSlice extends PlotModeHyper {
 
+	private static final Logger logger = LoggerFactory.getLogger(PlotModeArpesSlice.class);
+	
 	private static final String[] options =  new String[]{"Z", "Y", "X"};
 	
 	@Override
@@ -18,6 +25,24 @@ public class PlotModeArpesSlice extends PlotModeHyper {
 	public String getName() {
 		return "ARPES Slicing";
 	}
+	
+	@Override
+	public IDataset[] sliceForPlot(ILazyDataset lz, SliceND slice, Object[] options, IPlottingSystem<?> system) throws Exception {
+		IDataset[] output = super.sliceForPlot(lz, slice, options, system);
+		
+		//if data has 3 dimensions try and slice it into memory
+		//to improve responsiveness of UI
+		if (view3d.getRank() == 3) {
+			try {
+				view3d = view3d.getSlice();
+			} catch (Exception e) {
+				logger.debug("Could not read data into memory, using lazy version", e);
+			}
+		}
+		
+		return output;
+	}
+		
 
 	@Override
 	public void displayData(ITrace[] update, IPlottingSystem<?> system, Object userObject) throws Exception {
